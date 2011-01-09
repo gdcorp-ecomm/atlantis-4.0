75d5e2b05a2f30812631ce99d389a07b7ac83129
Michael Micco <mmicco@godaddy.com>
2011-01-09 19:30:57 +0000

    Framework 4 engine and interface
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C169696

---
+++ Atlantis.Framework.Engine/Atlantis.Framework.Interface/IProviderContainer.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Atlantis.Framework.Interface
+{
+  public interface IProviderContainer
+  {
+    void RegisterProvider<TProviderInterface, TProvider>() where TProviderInterface : class where TProvider : ProviderBase;
+    TProviderInterface Resolve<TProviderInterface>() where TProviderInterface : class;
+  }
+}

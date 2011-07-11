61c56c832713e436f979c685e4c02dc2f3252d0c
Kent Searle <ksearle@godaddy.com>
2011-07-11 22:55:19 +0000

    Project# 66399  - ( 3-Medium ) Miscellaneous .NET 4.0 Atlantis Framework Item conversions
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C205153

---
+++ Atlantis.Framework.GetCertifiedDomainCount/Atlantis.Framework.GetCertifiedDomainCount.Interface/GetCertifiedDomainCountRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GetCertifiedDomainCount.Interface
+{
+  public class GetCertifiedDomainCountRequestData : RequestData
+  {
+    public GetCertifiedDomainCountRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderIo,
+                                  string pathway,
+                                  int pageCount)
+      : base(shopperId, sourceUrl, orderIo, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(ShopperID);
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", "");
+    }
+  }
+}

83c3c992a21c2f52a3db092d68248c19c6f469e8
Kent Searle <ksearle@godaddy.com>
2011-07-19 22:01:19 +0000

    Project# 66399  - ( 3-Medium ) Miscellaneous .NET 4.0 Atlantis Framework Item conversions
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C206445

---
+++ Atlantis.Framework.LogOfferClick/Atlantis.Framework.LogOfferClick.Interface/LogOfferClickRequestData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.LogOfferClick.Interface
+{
+  public class LogOfferClickRequestData : RequestData
+  {
+    #region Properties
+    private string _fbiOfferID;
+    public string FbiOfferID
+    {
+      get { return _fbiOfferID; }
+      set { _fbiOfferID = value; }
+    }
+
+    private string _visitGuid;
+    public string VisitGuid
+    {
+      get { return _visitGuid; }
+      set { _visitGuid = value; }
+    }
+
+    private DateTime _clickDate;
+    public DateTime ClickDate
+    {
+      get { return _clickDate; }
+      set { _clickDate = value; }
+    }
+
+    private short _applicationID;
+    public short ApplicationID
+    {
+      get { return _applicationID; }
+      set { _applicationID = value; }
+    }
+
+    #endregion
+    
+    public LogOfferClickRequestData(
+      string shopperID, string sourceURL, string orderID, string pathway,
+      int pageCount, string fbiOfferID, string visitGuid, DateTime clickDate, short applicationID)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      _fbiOfferID = fbiOfferID;
+      _visitGuid = visitGuid;
+      _clickDate = clickDate;
+      _applicationID = applicationID;
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("LogOfferClick is not a cacheable request.");
+    }
+  }
+}

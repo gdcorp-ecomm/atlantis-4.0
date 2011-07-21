f87013b0befb51247b18331cc94112268e3fd6aa
Kent Searle <ksearle@godaddy.com>
2011-07-21 21:58:49 +0000

    Project# 66399  - ( 3-Medium ) MYA-Redesign - .NET 4.0 Atlantis Framework Item conversions
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C206850

---
+++ Atlantis.Framework.MyaCurrentProductGroups/Atlantis.Framework.MyaCurrentProductGroups.Interface/MyaCurrentProductGroupsRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.MyaCurrentProductGroups.Interface
+{
+  public class MyaCurrentProductGroupsRequestData : RequestData
+  {
+    public int PrivateLabelId { get; set; }
+
+    public MyaCurrentProductGroupsRequestData(string shopperId, string sourceUrl, string orderId, string pathway, int pageCount, int privateLabelId)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(3);
+      PrivateLabelId = privateLabelId;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("MyaCurrentProductGroups is not a cacheable request.");
+    }
+
+    public override string ToXML()
+    {
+      XDocument xmlDoc = new XDocument(new XElement("MyaCurrentProductGroupsRequestData", new XElement("ShopperId", ShopperID), new XElement("PrivateLabelId", PrivateLabelId.ToString())));
+      return xmlDoc.ToString();
+    }
+  }
+}

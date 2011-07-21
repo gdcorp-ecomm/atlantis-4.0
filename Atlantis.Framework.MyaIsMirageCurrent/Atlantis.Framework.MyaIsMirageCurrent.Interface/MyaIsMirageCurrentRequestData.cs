d3c24a85916410a596d0149e31cc036aeaac6775
Kent Searle <ksearle@godaddy.com>
2011-07-21 22:07:09 +0000

    Project# 66399  - ( 3-Medium ) MYA-Redesign - .NET 4.0 Atlantis Framework Item conversions
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C206854

---
+++ Atlantis.Framework.MyaIsMirageCurrent/Atlantis.Framework.MyaIsMirageCurrent.Interface/MyaIsMirageCurrentRequestData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.MyaIsMirageCurrent.Interface
+{
+  public class MyaIsMirageCurrentRequestData : RequestData
+  {
+    public MyaIsMirageCurrentRequestData(string shopperId, string sourceUrl, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(3);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("MyaIsMirageCurrent is not a cacheable request.");
+    }
+
+    public override string ToXML()
+    {
+      XDocument xmlDoc = new XDocument(new XElement("MyaIsMirageCurrentRequestData", new XElement("ShopperId", ShopperID)));
+      return xmlDoc.ToString();
+    }
+  }
+}

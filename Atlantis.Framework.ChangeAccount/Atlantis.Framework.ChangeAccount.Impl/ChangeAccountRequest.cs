51fdf8a12adac8d95554e17929c81f3807465e2a
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-07-28 16:44:32 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C207857

---
+++ Atlantis.Framework.ChangeAccount/Atlantis.Framework.ChangeAccount.Impl/ChangeAccountRequest.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+using Atlantis.Framework.ChangeAccount.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ChangeAccount.Impl
+{
+  public class ChangeAccountRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData returnResponse;
+      var requestData = (ChangeAccountRequestData)oRequestData;
+
+      if (requestData.ChangeAccountRequests != null)
+      {
+        returnResponse = BatchRequestHandler(oRequestData, oConfig);
+      }
+      else
+      {
+        returnResponse = SingleRequestHandler(oRequestData, oConfig);
+      }
+
+      return returnResponse;
+    }
+
+    public IResponseData BatchRequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      var responseData = new ChangeAccountResponseData { IsSuccess = true };
+      var requestData = (ChangeAccountRequestData)oRequestData;
+      var configuration = (WsConfigElement)oConfig;
+
+      using (var manager = new BonsaiManager.Service())
+      {
+        manager.Url = configuration.WSURL;
+        manager.Timeout = (int) requestData.RequestTimeout.TotalMilliseconds;
+
+        foreach (var requestObject in requestData.ChangeAccountRequests)
+        {
+          var responseObject = new ChangeAccountResponseObject
+                                 {
+                                   RenewalPFID = requestObject.RenewalPFID,
+                                   ResourceID = requestObject.ResourceID,
+                                 };
+          try
+          {
+            int resultCode;
+
+            int basketCode = manager.ChangeAccountRequest(requestObject.ResourceID, requestObject.ResourceType,
+                                                          requestObject.IDType, requestObject.AccountChangeXML,
+                                                          requestObject.RenewalPFID,
+                                                          requestObject.RenewalPeriods, requestObject.ItemRequestXML,
+                                                          out resultCode);
+            if (resultCode < 0 || basketCode < 0)
+            {
+              string data = string.Format("Result Code: {0}, Basket Result Code {1}, ResourceID: {2},"
+                                          + " ResourceType: {3}, IDType: {4}, AccountChangeXML: {5}, RenewalPFID: {6},"
+                                          + " RenewalPeriods: {7}, ItemRequestXML: {8}",
+                                          resultCode, basketCode, requestObject.ResourceID,
+                                          requestObject.ResourceType, requestObject.IDType,
+                                          requestObject.AccountChangeXML,
+                                          requestObject.RenewalPFID, requestObject.RenewalPeriods,
+                                          requestObject.ItemRequestXML);
+              responseObject.AtlException = new AtlantisException(requestData,
+                                                                  "ChangeAccountRequest.RequestHandler",
+                                                                  "Failure changing account", data);
+              responseObject.IsSuccess = false;
+              responseData.IsSuccess = false;
+            }
+            else
+            {
+              responseObject.BasketResultCode = basketCode;
+              responseObject.IsSuccess = true;
+            }
+          }
+          catch (Exception ex)
+          {
+            string data = string.Format("ResourceID: {0},"
+                                        + " ResourceType: {1}, IDType: {2}, AccountChangeXML: {3}, RenewalPFID: {4},"
+                                        + " RenewalPeriods: {5}, ItemRequestXML: {6}",
+                                        requestObject.ResourceID, requestObject.ResourceType, requestObject.IDType,
+                                        requestObject.AccountChangeXML, requestObject.RenewalPFID,
+                                        requestObject.RenewalPeriods, requestObject.ItemRequestXML);
+            responseObject.AtlException = new AtlantisException(requestData,
+                                                                "ChangeAccountRequest.RequestHandler",
+                                                                "Failure changing account", data, ex);
+            responseObject.IsSuccess = false;
+            responseData.IsSuccess = false;
+          }
+          responseData.ChangeAccountResponses.Add(responseObject);
+        }
+      }
+
+      return responseData;
+    }
+
+    public IResponseData SingleRequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      var responseData = new ChangeAccountResponseData();
+      var requestData = (ChangeAccountRequestData)oRequestData;
+      var configuration = (WsConfigElement)oConfig;
+
+      using (var manager = new BonsaiManager.Service())
+      {
+        manager.Url = configuration.WSURL;
+        manager.Timeout = (int) requestData.RequestTimeout.TotalMilliseconds;
+
+        try
+        {
+          int resultCode;
+          int basketCode = manager.ChangeAccountRequest(requestData.ResourceID, requestData.ResourceType,
+                                                        requestData.IDType, requestData.AccountChangeXML,
+                                                        requestData.RenewalPFID,
+                                                        requestData.RenewalPeriods, requestData.ItemRequestXML,
+                                                        out resultCode);
+          if (resultCode < 0 || basketCode < 0)
+          {
+            string data = string.Format("Result Code: {0}, Basket Result Code {1}, ResourceID: {2},"
+                                        + " ResourceType: {3}, IDType: {4}, AccountChangeXML: {5}, RenewalPFID: {6},"
+                                        + " RenewalPeriods: {7}, ItemRequestXML: {8}",
+                                        resultCode, basketCode, requestData.ResourceID,
+                                        requestData.ResourceType, requestData.IDType, requestData.AccountChangeXML,
+                                        requestData.RenewalPFID, requestData.RenewalPeriods,
+                                        requestData.ItemRequestXML);
+            responseData.AtlException = new AtlantisException(requestData,
+                                                              "ChangeAccountRequest.RequestHandler",
+                                                              "Failure changing account", data);
+            responseData.IsSuccess = false;
+          }
+          else
+          {
+            responseData.BasketResultCode = basketCode;
+            responseData.IsSuccess = true;
+          }
+        }
+        catch (Exception ex)
+        {
+          string data = string.Format("ResourceID: {0},"
+                                      + " ResourceType: {1}, IDType: {2}, AccountChangeXML: {3}, RenewalPFID: {4},"
+                                      + " RenewalPeriods: {5}, ItemRequestXML: {6}",
+                                      requestData.ResourceID, requestData.ResourceType, requestData.IDType,
+                                      requestData.AccountChangeXML, requestData.RenewalPFID,
+                                      requestData.RenewalPeriods, requestData.ItemRequestXML);
+          responseData.AtlException = new AtlantisException(requestData,
+                                                            "ChangeAccountRequest.RequestHandler",
+                                                            "Failure changing account", data, ex);
+          responseData.IsSuccess = false;
+        }
+      }
+
+      return responseData;
+    }
+
+  }
+}
---
+++ Atlantis.Framework.ChangeAccount/Atlantis.Framework.ChangeAccount.Tests/ChangeAccountTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Atlantis.Framework.ChangeAccount.Interface;
+
+namespace Atlantis.Framework.ChangeAccount.Tests
+{
+  [TestClass]
+  public class ChangeAccountTests
+  {
+    public ChangeAccountTests() {}
+
+    public TestContext TestContext { get; set; }
+
+    #region Additional test attributes  
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ChangeAccountAddPrepaids()
+    {
+      string accountChangeXML =
+        "<ClientChange ProductNamespace=\"hosting\" TreeID=\"0\" AccountID=\"375973\" ShopperID=\"857020\" />";
+
+      string itemRequestXML =
+            "<itemRequest><itemTemplate itemTrackingCode=\"mbl_mya_sharedhosting_renew\" /></itemRequest>";
+
+      ChangeAccountRequestData requestData = new ChangeAccountRequestData("857020", "http://localhost",
+        "", "", 0, "375973", "hosting", "billing", accountChangeXML, 52002, 1, itemRequestXML);
+
+      ChangeAccountResponseData responseData = (ChangeAccountResponseData)Engine.Engine.ProcessRequest(requestData, 77);
+      Assert.AreEqual(true, responseData.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ChangeAccountBatchAddPrepaids()
+    {
+      List<ChangeAccountRequestObject> changeList = new List<ChangeAccountRequestObject>();
+      string accountChangeXML =
+        "<ClientChange ProductNamespace=\"hosting\" TreeID=\"0\" AccountID=\"375973\" ShopperID=\"857020\" />";
+
+      string itemRequestXML =
+            "<itemRequest><itemTemplate itemTrackingCode=\"mbl_mya_sharedhosting_renew\" /></itemRequest>";
+
+
+      ChangeAccountRequestObject requestObject = new ChangeAccountRequestObject("375973", "hosting", "billing", accountChangeXML,
+        52002, 1, itemRequestXML);
+      changeList.Add(requestObject);
+
+      accountChangeXML =
+       "<ClientChange ProductNamespace=\"email\" TreeID=\"0\" AccountID=\"375017\" ShopperID=\"857020\" />";
+
+      itemRequestXML =
+            "<itemRequest><itemTemplate itemTrackingCode=\"mbl_mya_email_renew\" /></itemRequest>";
+
+
+      requestObject = new ChangeAccountRequestObject("375017", "email", "billing", accountChangeXML,
+        11866, 1, itemRequestXML);
+      changeList.Add(requestObject);
+
+      ChangeAccountRequestData requestData = new ChangeAccountRequestData("857020", "http://localhost",
+      "", "", 0, changeList);
+      ChangeAccountResponseData responseData = (ChangeAccountResponseData)Engine.Engine.ProcessRequest(requestData, 77);
+      Assert.AreEqual(true, responseData.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void InvalidChangeAccountBatchAddPrepaids()
+    {
+      List<ChangeAccountRequestObject> changeList = new List<ChangeAccountRequestObject>();
+      string accountChangeXML =
+        "<ClientChange ProductNamespace=\"hosting\" TreeID=\"0\" AccountID=\"375973\" ShopperID=\"857020\" />";
+
+      string itemRequestXML =
+            "<itemRequest><itemTemplate itemTrackingCode=\"mbl_mya_sharedhosting_renew\" /></itemRequest>";
+
+
+      ChangeAccountRequestObject requestObject = new ChangeAccountRequestObject("375973", "hosting", "billing", accountChangeXML,
+        52002, 1, itemRequestXML);
+      changeList.Add(requestObject);
+
+      accountChangeXML =
+       "<ClientChange ProductNamespace=\"eail\" TreeID=\"0\" AccountID=\"37517\" ShopperID=\"857020\" />";
+
+      itemRequestXML =
+            "<itemRequest><itemTemplate itemTrackingCode=\"mbl_ma_email_renew\" /></itemRequest>";
+
+
+      requestObject = new ChangeAccountRequestObject("37507", "emil", "blling", accountChangeXML,
+        11866, 1, itemRequestXML);
+      changeList.Add(requestObject);
+
+      ChangeAccountRequestData requestData = new ChangeAccountRequestData("857020", "http://localhost",
+      "", "", 0, changeList);
+      ChangeAccountResponseData responseData = (ChangeAccountResponseData)Engine.Engine.ProcessRequest(requestData, 77);
+      Assert.AreEqual(false, responseData.IsSuccess);
+    }
+  }
+}

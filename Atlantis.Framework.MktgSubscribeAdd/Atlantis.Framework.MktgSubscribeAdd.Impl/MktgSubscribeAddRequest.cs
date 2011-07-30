d385d3b2a0efe3d28be8d5363b58b010edd62249
Michael Micco <mmicco@godaddy.com>
2011-07-30 08:03:37 +0000

    MktgSubscribeAdd update to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208211

---
+++ Atlantis.Framework.MktgSubscribeAdd/Atlantis.Framework.MktgSubscribeAdd.Impl/MktgSubscribeAddRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.MktgSubscribeAdd.Impl.SubscribeWS;
+using Atlantis.Framework.MktgSubscribeAdd.Interface;
+
+namespace Atlantis.Framework.MktgSubscribeAdd.Impl
+{
+  public class MktgSubscribeAddRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData result = null;
+      string responseText = string.Empty;
+
+      try
+      {
+        MktgSubscribeAddRequestData mktgRequest = (MktgSubscribeAddRequestData)oRequestData;
+
+        using (Service service = new Service())
+        {
+          service.Url = ((WsConfigElement)oConfig).WSURL;
+          service.Timeout = (int)mktgRequest.RequestTimeout.TotalMilliseconds;
+
+          responseText = service.Subscribe(mktgRequest.Email, mktgRequest.PublicationId, mktgRequest.PrivateLabelId, mktgRequest.EmailType,
+                                           mktgRequest.FirstName, mktgRequest.LastName, mktgRequest.RequestedBy, mktgRequest.IPAddress, mktgRequest.Confirmed); 
+        }
+
+        result = new MktgSubscribeAddResponseData(responseText);
+      }
+      catch (Exception ex)
+      {
+        result = new MktgSubscribeAddResponseData(oRequestData, ex);
+      }
+
+      return result;
+    }
+
+    #endregion
+  }
+}

7df1efc2b3ea73b7d85722ad172ceeb22af4fc43
Michael Micco <mmicco@godaddy.com>
2011-07-30 08:09:59 +0000

    PrivacyAppInsertUpdate 4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208214

---
+++ Atlantis.Framework.PrivacyAppInsertUpdate/Atlantis.Framework.PrivacyAppInsertUpdate.Impl/PrivacyAppInsertUpdateRequest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.PrivacyAppInsertUpdate.Impl.InsertUpdateWS;
+using Atlantis.Framework.PrivacyAppInsertUpdate.Interface;
+
+namespace Atlantis.Framework.PrivacyAppInsertUpdate.Impl
+{
+  public class PrivacyAppInsertUpdateRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData response = null;
+      int result = 0;
+      string pbstrOutput = string.Empty;
+
+      try
+      {
+        PrivacyAppInsertUpdateRequestData request = (PrivacyAppInsertUpdateRequestData)oRequestData;
+        using (wscgdPrivacyAppService service = new wscgdPrivacyAppService())
+        {
+          service.Url = ((WsConfigElement)oConfig).WSURL;
+          AddClientCertificate(service, oConfig);
+          service.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+          string bstrXML = request.PrivacyXML;
+          result = service.InsertUpdate(bstrXML, out pbstrOutput);
+        }
+
+        response = new PrivacyAppInsertUpdateResponseData(result, pbstrOutput);
+      }
+      catch (Exception ex)
+      {
+        response = new PrivacyAppInsertUpdateResponseData(result, pbstrOutput, oRequestData, ex);
+      }
+      
+      return response;
+    }
+
+    private void AddClientCertificate(wscgdPrivacyAppService service, ConfigElement oConfig)
+    {
+      X509Certificate cert = GetCertificate(oConfig);
+      if(cert != null)
+        service.ClientCertificates.Add(cert);
+    }
+
+    private X509Certificate GetCertificate(ConfigElement oConfig)
+    {
+      X509Certificate cert = null;
+      string certificateName = oConfig.GetConfigValue("CertificateName");
+      if (!string.IsNullOrEmpty(certificateName))
+      {
+        X509Store certStore = new X509Store(StoreLocation.LocalMachine);
+        certStore.Open(OpenFlags.ReadOnly);
+        X509CertificateCollection certs = certStore.Certificates.Find(X509FindType.FindBySubjectName, certificateName, true);
+        if(certs.Count > 0)
+          cert = certs[0];
+      }
+      return cert;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.PrivacyAppInsertUpdate/Atlantis.Framework.PrivacyAppInsertUpdate.Interface/PrivacyAppInsertUpdateRequestData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PrivacyAppInsertUpdate.Interface
+{
+  public class PrivacyAppInsertUpdateRequestData : RequestData
+  {
+    private string _privacyXml = string.Empty;
+
+    public PrivacyAppInsertUpdateRequestData(
+      string shopperId,
+      string sourceURL,
+      string orderId,
+      string pathway,
+      int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(6);
+    }
+
+    public string PrivacyXML
+    {
+      get { return _privacyXml; }
+      set { _privacyXml = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("PrivacyAppInsertUpdate is not a cacheable request.");
+    }
+
+    public override string ToXML()
+    {
+      return string.Empty;
+    }
+
+  }
+}

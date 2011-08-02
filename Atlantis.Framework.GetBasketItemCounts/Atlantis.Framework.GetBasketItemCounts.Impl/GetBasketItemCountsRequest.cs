8360ad89580e72c5adb373cd439df5a4204e2bcc
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-02 23:13:44 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208665

---
+++ Atlantis.Framework.GetBasketItemCounts/Atlantis.Framework.GetBasketItemCounts.Impl/GetBasketItemCountsRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.GetBasketItemCounts.Interface;
+
+namespace Atlantis.Framework.GetBasketItemCounts.Impl
+{
+  public class GetBasketItemCountsRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      GetBasketItemCountsResponseData oResponseData;
+      string responseXml = string.Empty;
+
+      try
+      {
+        var oGetBasketItemCountsRequestData = (GetBasketItemCountsRequestData)oRequestData;
+
+        using (var oBasketWS = new WSCgdBasket.WscgdBasketService())
+        {
+          oBasketWS.Url = ((WsConfigElement) oConfig).WSURL;
+          responseXml = string.Empty;
+          responseXml = oBasketWS.GetItemCounts(oGetBasketItemCountsRequestData.ShopperID);
+          if (responseXml.IndexOf("<error>", StringComparison.OrdinalIgnoreCase) > -1)
+          {
+            var exAtlantis = new AtlantisException(oRequestData,
+                                                                 "GetBasketItemCountsRequest.RequestHandler",
+                                                                 responseXml,
+                                                                 oRequestData.ToXML());
+
+            oResponseData = new GetBasketItemCountsResponseData(responseXml, exAtlantis);
+          }
+          else
+            oResponseData = new GetBasketItemCountsResponseData(responseXml);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        oResponseData = new GetBasketItemCountsResponseData(responseXml, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new GetBasketItemCountsResponseData(responseXml, oRequestData, ex);
+      }
+
+      return oResponseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.GetBasketItemCounts/Atlantis.Framework.GetBasketItemCounts.Interface/GetBasketItemCountsRequestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GetBasketItemCounts.Interface
+{
+  public class GetBasketItemCountsRequestData : RequestData
+  {
+    public GetBasketItemCountsRequestData(string shopperId,
+                            string sourceUrl,
+                            string orderId,
+                            string pathway,
+                            int pageCount)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(2d);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("GetBasketItemCounts is not a cacheable request.");
+    }
+  }
+}

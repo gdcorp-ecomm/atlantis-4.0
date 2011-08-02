4d6b14053aad8cb2649b0e93cea2ac0293e6cad9
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-02 22:39:07 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208641

---
+++ Atlantis.Framework.EcommPaymentProfiles/Atlantis.Framework.EcommPaymentProfiles.Impl/EcommPaymentProfilesRequest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Atlantis.Framework.EcommPaymentProfiles.Impl.WsgdCPPSvc;
+using Atlantis.Framework.EcommPaymentProfiles.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommPaymentProfiles.Impl
+{
+  public class EcommPaymentProfilesRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      EcommPaymentProfilesResponseData responseData;
+      string responseXml = string.Empty;
+
+      try
+      {
+        var request = (EcommPaymentProfilesRequestData)requestData;
+
+        using (var ppWs = new PPWebSvcService())
+        {
+          ppWs.Url = ((WsConfigElement) config).WSURL;
+          ppWs.Timeout = (int) request.RequestTimeout.TotalMilliseconds;
+          responseXml = ppWs.GetInfoByShopperID(string.Empty, request.ShopperID);
+
+          if (responseXml.IndexOf("<error>", StringComparison.OrdinalIgnoreCase) > -1)
+          {
+            var exAtlantis = new AtlantisException(requestData
+                                                   , "EcommPaymentProfilesRequest.RequestHandler"
+                                                   , responseXml
+                                                   , requestData.ToXML());
+
+            responseData = new EcommPaymentProfilesResponseData(responseXml, exAtlantis);
+          }
+          else
+          {
+            responseData = new EcommPaymentProfilesResponseData(requestData, responseXml);
+          }
+        }
+      }
+
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new EcommPaymentProfilesResponseData(responseXml, exAtlantis);
+      }
+
+      catch (Exception ex)
+      {
+        responseData = new EcommPaymentProfilesResponseData(responseXml, requestData, ex);
+      }
+
+      return responseData;
+    }
+  }
+}

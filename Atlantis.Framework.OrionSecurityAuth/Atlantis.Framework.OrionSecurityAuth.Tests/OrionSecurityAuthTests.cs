2f784afeb7dad77b4779d3bab43d4c6edbac4f21
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-03 18:29:35 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208828

---
+++ Atlantis.Framework.OrionSecurityAuth/Atlantis.Framework.OrionSecurityAuth.Tests/OrionSecurityAuthTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Atlantis.Framework.OrionSecurityAuth.Interface;
+
+namespace Atlantis.Framework.OrionSecurityAuth.Tests
+{
+  [TestClass]
+  public class GetOrionSecurityAuthTests
+  {
+
+    private const string _shopperId = "840820";
+    const int _requestType = 128;
+
+
+    public GetOrionSecurityAuthTests()
+    {
+    }
+
+    public TestContext TestContext { get; set; }
+
+    #region Additional test attributes
+    
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("bin/netconnect.dll")]
+    public void OrionSecurityAuthTest()
+    {
+      var request = new OrionSecurityAuthRequestData(_shopperId
+         , string.Empty
+         , string.Empty
+         , string.Empty
+         , 0
+         , "orion_security_mya");
+
+      var response = (OrionSecurityAuthResponseData)DataCache.DataCache.GetProcessRequest(request, _requestType);
+
+      Debug.WriteLine(response.ToXML());
+      Assert.IsTrue(response.IsSuccess);
+    }
+  }
+}

05fd1c979feda1c436add1548cbf0811fc0ed51b
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-03 16:40:47 +0000

    Fixed Test project, out of sync
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208780

---
+++ Atlantis.Framework.GetOverrideHash/Atlantis.Framework.GetOverrideHash.Tests/GetOverrideHashTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Atlantis.Framework.GetOverrideHash.Interface;
+
+namespace Atlantis.Framework.GetOverrideHash.Tests
+{
+  [TestClass]
+  public class GetOverrideHashTest
+  {
+    public GetOverrideHashTest()
+    { }
+
+    public TestContext TestContext { get; set; }
+
+    #region Additional test attributes
+
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void TestMethod1()
+    {
+
+      var requestData = new GetOverrideHashRequestData("850774", string.Empty, string.Empty, string.Empty, 0, 1952, 2, 1);
+      //GetOverrideHashRequestData requestData = new GetOverrideHashRequestData(
+      // "850774", string.Empty, string.Empty, string.Empty, 0, 1234, 1, 3456, 2222);
+
+      var response = (GetOverrideHashResponseData)Engine.Engine.ProcessRequest(requestData, 26);
+      string hash = response.Hash;
+
+      Assert.IsFalse(string.IsNullOrWhiteSpace(hash));
+    }
+  }
+}

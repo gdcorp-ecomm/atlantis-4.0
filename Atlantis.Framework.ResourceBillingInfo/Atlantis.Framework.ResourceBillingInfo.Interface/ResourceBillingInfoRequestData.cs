1e1175888b3b003230871f6c18daca2d8009da67
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-03 19:59:18 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C208858

---
+++ Atlantis.Framework.ResourceBillingInfo/Atlantis.Framework.ResourceBillingInfo.Interface/ResourceBillingInfoRequestData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ResourceBillingInfo.Interface
+{
+  public class ResourceBillingInfoRequestData : RequestData
+  {
+    public int? BillingResourceId { get; set; }
+
+    public ResourceBillingInfoRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderId,
+                                  string pathway,
+                                  int pageCount,
+                                  int? billingResourceId)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      BillingResourceId = billingResourceId;
+      RequestTimeout = TimeSpan.FromSeconds(5d);
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+
+      byte[] stringBytes = System.Text.Encoding.ASCII.GetBytes(string.Format("{0}-{1}", ShopperID, (BillingResourceId.HasValue ? BillingResourceId.Value.ToString() : "GetAll")));
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", "");
+    }
+  }
+}
---
+++ Atlantis.Framework.ResourceBillingInfo/Atlantis.Framework.ResourceBillingInfo.Tests/ResourceBillingInfoTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Atlantis.Framework.ResourceBillingInfo.Interface;
+using Atlantis.Framework.Testing.MockHttpContext;
+
+namespace Atlantis.Framework.ResourceBillingInfo.Tests
+{
+  [TestClass]
+  public class GetResourceBillingInfoTests
+  {
+    private const string _shopperId = "856907";
+    private const int _requestType = 216;
+
+    public GetResourceBillingInfoTests()
+    { }
+
+    public TestContext TestContext { get; set; }
+
+    #region Additional test attributes
+    
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("bin/netconnect.dll")]
+    public void ResourceBillingInfoResourceIdTest()
+    {
+      MockHttpContext.SetMockHttpContext(string.Empty, "http://localhost", string.Empty);
+
+      const int billingResourceId = 400508;
+      var request = new ResourceBillingInfoRequestData(_shopperId
+                                                       , string.Empty
+                                                       , string.Empty
+                                                       , string.Empty
+                                                       , 0
+                                                       , billingResourceId);
+
+      var response1 = SessionCache.SessionCache.GetProcessRequest<ResourceBillingInfoResponseData>(request, _requestType);
+      var response2 = SessionCache.SessionCache.GetProcessRequest<ResourceBillingInfoResponseData>(request, _requestType);
+
+      Debug.WriteLine(response2.ToXML());
+      Assert.IsTrue(response2.IsSuccess);
+      Assert.IsTrue(response1.ToXML().Equals(response2.ToXML()));
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("bin/netconnect.dll")]
+    public void ResourceBillingInfoShopperIdTest()
+    {
+      MockHttpContext.SetMockHttpContext(string.Empty, "http://localhost", string.Empty);
+
+      var request = new ResourceBillingInfoRequestData(_shopperId
+                                                       , string.Empty
+                                                       , string.Empty
+                                                       , string.Empty
+                                                       , 0
+                                                       , null);
+
+      var response1 = SessionCache.SessionCache.GetProcessRequest<ResourceBillingInfoResponseData>(request, _requestType);
+      var response2 = SessionCache.SessionCache.GetProcessRequest<ResourceBillingInfoResponseData>(request, _requestType);
+
+      Debug.WriteLine(response2.ToXML());
+      Assert.IsTrue(response2.IsSuccess);
+      Assert.IsTrue(response1.ToXML().Equals(response2.ToXML()));
+    }
+  }
+}

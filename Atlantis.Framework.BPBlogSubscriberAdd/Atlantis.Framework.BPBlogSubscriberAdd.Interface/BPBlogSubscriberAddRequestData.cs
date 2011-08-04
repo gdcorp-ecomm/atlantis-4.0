b4fa4ee311f3e1c1109e3d9b33d60e38c95cf714
Sridhar Thota <sthota@godaddy.com>
2011-08-04 17:29:44 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C209043

---
+++ Atlantis.Framework.BPBlogSubscriberAdd/Atlantis.Framework.BPBlogSubscriberAdd.Interface/BPBlogSubscriberAddRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.BPBlogSubscriberAdd.Interface
+{
+  public class BPBlogSubscriberAddRequestData : RequestData
+  {
+    public string Email { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public bool Confirmed { get; private set; }
+
+    public BPBlogSubscriberAddRequestData(
+      string shopperId, string sourceUrl, string orderId, string pathway, int pageCount,
+      string email, string firstName, string lastName, bool confirmed)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      Email = email;
+      FirstName = firstName;
+      LastName = lastName;
+      Confirmed = confirmed;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("BPBlogSubscriberAdd is not a cacheable request.");
+    }
+  }
+}

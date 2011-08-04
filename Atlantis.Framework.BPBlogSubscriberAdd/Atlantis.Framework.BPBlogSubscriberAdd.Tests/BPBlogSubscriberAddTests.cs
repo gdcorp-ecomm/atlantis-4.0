e66b02f84979003d3a2999fc21b930d7b5454bca
Sridhar Thota <sthota@godaddy.com>
2011-08-04 18:09:23 +0000

    remove/sort usings
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C209065

--- Atlantis.Framework.BPBlogSubscriberAdd/Atlantis.Framework.BPBlogSubscriberAdd.Tests/BPBlogSubscriberAddTests.cs
+++ Atlantis.Framework.BPBlogSubscriberAdd/Atlantis.Framework.BPBlogSubscriberAdd.Tests/BPBlogSubscriberAddTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Atlantis.Framework.BPBlogSubscriberAdd.Interface;
+﻿using Atlantis.Framework.BPBlogSubscriberAdd.Interface;
 using Atlantis.Framework.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Atlantis.Framework.BPBlogSubscriberAdd.Tests
 {

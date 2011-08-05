a1310b7ac45a0771762d0c89d0e9e0ec4f69ca05
Michael Micco <mmicco@godaddy.com>
2011-08-05 16:19:50 +0000

    Convert MessagingProcess to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C209257

---
+++ Atlantis.Framework.MessagingProcess/Atlantis.Framework.MessagingProcess.Impl/MessagingProcessRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.MessagingProcess.Interface;
+
+namespace Atlantis.Framework.MessagingProcess.Impl
+{
+  public class MessagingProcessRequest : IRequest
+  {
+
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      MessagingProcessResponseData response = null;
+      string responseXml = string.Empty;
+
+      try
+      {
+        MessagingProcessRequestData request = (MessagingProcessRequestData)oRequestData;
+
+        using (MessagingWS.WSCgdMessagingSystemService messagingWS = new MessagingWS.WSCgdMessagingSystemService())
+        {
+          messagingWS.Url = ((WsConfigElement)oConfig).WSURL;
+          messagingWS.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+          responseXml = messagingWS.ProcessXml(request.ToXML());
+        }
+
+        response = new MessagingProcessResponseData(responseXml);
+      }
+      catch (Exception ex)
+      {
+        response = new MessagingProcessResponseData(responseXml, oRequestData, ex);
+      }
+
+      return response;
+    }
+
+    #endregion
+  }
+}

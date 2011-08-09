3f437f5eb918488a91ebe652ba9d45b4918e727b
Sridhar Thota <sthota@godaddy.com>
2011-08-09 19:55:36 +0000

    conversion to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C209927

---
+++ Atlantis.Framework.MYAGetMobileCarriers/Atlantis.Framework.MYAGetMobileCarriers.Interface/MobileCarrierItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Atlantis.Framework.MYAGetMobileCarriers.Interface
+{
+  public class MobileCarrierItem
+  {
+    public int MobileCarrierID { get; set; }
+    public string Description { get; set; }
+  }
+}

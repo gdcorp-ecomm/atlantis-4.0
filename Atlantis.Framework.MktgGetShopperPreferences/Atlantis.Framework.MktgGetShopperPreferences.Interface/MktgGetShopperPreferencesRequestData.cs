167b53fcfe20c94eb4eaaef8748cd9df3388d8ea
Sridhar Thota <sthota@godaddy.com>
2011-08-18 17:39:46 +0000

    update
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C211974

--- Atlantis.Framework.MktgGetShopperPreferences/Atlantis.Framework.MktgGetShopperPreferences.Interface/MktgGetShopperPreferencesRequestData.cs
+++ Atlantis.Framework.MktgGetShopperPreferences/Atlantis.Framework.MktgGetShopperPreferences.Interface/MktgGetShopperPreferencesRequestData.cs
@@ -5,13 +5,11 @@ namespace Atlantis.Framework.MktgGetShopperPreferences.Interface
 {
   public class MktgGetShopperPreferencesRequestData: RequestData
   {
-    public TimeSpan RequestTimeout { get; set; }
 
     public MktgGetShopperPreferencesRequestData(
       string shopperId, string sourceUrl, string orderId, string pathway, int pageCount)
       : base(shopperId, sourceUrl, orderId, pathway, pageCount)
     {
-      RequestTimeout = TimeSpan.FromSeconds(4);
     }
 
     public override string GetCacheMD5()

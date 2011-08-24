98b8c5cd5cc9b8f73a401cc47f856a609d5e98f7
Sridhar Thota <sthota@godaddy.com>
2011-08-24 20:41:44 +0000

    4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C213286

---
+++ Atlantis.Framework.DCCValidateNS/Atlantis.Framework.DCCValidateNS.Interface/DCCValidateNSRequestData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DCCValidateNS.Interface
+{
+  public class DCCValidateNSRequestData : RequestData
+  {
+    public int PrivateLabelId { get; set; }
+
+   
+    private List<string> _nameServers = new List<string>();
+    public List<string> NameServers
+    {
+      get
+      {
+        return _nameServers;
+      }
+      set
+      {
+        _nameServers = value;
+      }
+    }
+
+    public DCCValidateNSRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount, int privateLabelId,List<string> nameServers)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      PrivateLabelId = privateLabelId;
+      _nameServers = nameServers;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("DCCValidateNSRequest is not a cacheable request.");
+    }
+  }
+}

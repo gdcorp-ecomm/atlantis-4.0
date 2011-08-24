fb1096509428cc4cdb9142a107b345f4017f2cca
Sridhar Thota <sthota@godaddy.com>
2011-08-24 20:06:11 +0000

    buyerProfile Delete
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C213257

---
+++ Atlantis.Framework.BuyerProfileDelete/Atlantis.Framework.BuyerProfileDelete.Impl/BuyerProfileDeleteRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Atlantis.Framework.BuyerProfileDelete.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+
+namespace Atlantis.Framework.BuyerProfileDelete.Impl
+{
+  public class BuyerProfileDeleteRequest : IRequest
+  {
+    private const string PROC_NAME = "mya_DeleteBuyerProfile_sp";
+
+    #region Implementation of IRequest
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData oResponseData;
+      BuyerProfileDeleteRequestData request = (BuyerProfileDeleteRequestData)oRequestData;
+
+      DataSet ds = null;
+
+      try
+      {
+        string connectionString = NetConnect.LookupConnectInfo(oConfig);
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+          using (SqlCommand command = new SqlCommand(PROC_NAME, connection))
+          {
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandTimeout = (int)request.RequestTimeout.TotalSeconds;
+            command.Parameters.Add(new SqlParameter("@profile_id", request.ProfileID));
+            command.Parameters.Add(new SqlParameter("@shopper_id", request.ShopperID));
+
+            connection.Open();
+            ds = new DataSet(Guid.NewGuid().ToString());
+            SqlDataAdapter adp = new SqlDataAdapter(command);
+            adp.Fill(ds);
+          }
+        }
+        oResponseData = new BuyerProfileDeleteResponseData();
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new BuyerProfileDeleteResponseData(oRequestData, ex);
+      }
+
+      return oResponseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.BuyerProfileDelete/Atlantis.Framework.BuyerProfileDelete.Interface/BuyerProfileDeleteResponseData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.BuyerProfileDelete.Interface
+{
+  public class BuyerProfileDeleteResponseData :IResponseData
+  {
+
+    private AtlantisException _atlException = null;
+    private bool _success = false;
+
+    public BuyerProfileDeleteResponseData()
+    {
+      _success = true;
+    }
+
+    public BuyerProfileDeleteResponseData(RequestData oRequestData, Exception ex)
+    {
+      _success = false;
+      _atlException = new AtlantisException(oRequestData, "BuyerProfileDeleteResponseData", ex.Message, string.Empty);
+    }
+
+    public bool IsSuccess
+    {
+      get { return _success; }
+    }
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      return string.Empty;
+    }
+
+    #endregion
+
+    public AtlantisException GetException()
+    {
+      return _atlException;
+    }
+
+  }
+}

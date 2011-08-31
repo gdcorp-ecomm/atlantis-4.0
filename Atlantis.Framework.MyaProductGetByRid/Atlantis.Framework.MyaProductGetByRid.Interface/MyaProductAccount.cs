26c1038a38c3e7e1ee155558c8b9f3d23466bc20
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-08-31 07:34:10 +0000

    add new fields
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C214630

--- Atlantis.Framework.MyaProductGetByRid/Atlantis.Framework.MyaProductGetByRid.Interface/MyaProductAccount.cs
+++ Atlantis.Framework.MyaProductGetByRid/Atlantis.Framework.MyaProductGetByRid.Interface/MyaProductAccount.cs
@@ -22,6 +22,8 @@ namespace Atlantis.Framework.MyaProductGetByRid.Interface
     protected override string ProductIdKey { get { return "pf_id"; } }
     protected override string ProductTypeIdKey { get { return "gdshop_product_typeID"; } }
     protected override string RenewalProductIdKey { get { return "renewal_pf_id"; } }
+    protected string ParentProductTypeIdKey { get { return "parent_product_typeID"; } }
+    protected string UpgradeProductIdKey { get { return "defaultUpgrade_pf_id"; } }
 
     private MyaProductType? _productType;
     public override MyaProductType ProductType
@@ -36,6 +38,40 @@ namespace Atlantis.Framework.MyaProductGetByRid.Interface
           }
         }
         return _productType.Value;
+      }
+    }
+
+    private int? _parentProductTypeId;
+    public int ParentProductTypeId
+    {
+      get
+      {
+        if (_parentProductTypeId == null)
+        {
+          _parentProductTypeId = 0;
+          if (IsPropertyInDictionary(ParentProductTypeIdKey))
+          {
+            _parentProductTypeId = Convert.ToInt32(PropertiesDictionary[ParentProductTypeIdKey]);
+          }
+        }
+        return _parentProductTypeId.Value;
+      }
+    }
+
+    private int? _upgradeProductId;
+    public int UpgradeProductId
+    {
+      get
+      {
+        if (_upgradeProductId == null)
+        {
+          _upgradeProductId = 0;
+          if (IsPropertyInDictionary(UpgradeProductIdKey))
+          {
+            _upgradeProductId = Convert.ToInt32(PropertiesDictionary[UpgradeProductIdKey]);
+          }
+        }
+        return _upgradeProductId.Value;
       }
     }
 

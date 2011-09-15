2ced5ce0a4e05c15e576b843f3b47b6675a69799
Sridhar Thota <sthota@godaddy.com>
2011-09-15 17:26:39 +0000

    to dev
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C217650

--- Atlantis.Framework.MktgGetShopperPreferences/Atlantis.Framework.MktgGetShopperPreference.Tests/UnitTest1.cs
+++ Atlantis.Framework.MktgGetShopperPreferences/Atlantis.Framework.MktgGetShopperPreference.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace Atlantis.Framework.MktgGetShopperPreference.Tests
     [TestMethod]
     public void TestMethod1()
     {
-      MktgGetShopperPreferencesRequestData request = new MktgGetShopperPreferencesRequestData("75866", string.Empty, string.Empty, string.Empty,
+      MktgGetShopperPreferencesRequestData request = new MktgGetShopperPreferencesRequestData("850774", string.Empty, string.Empty, string.Empty,
                                  0);
       MktgGetShopperPreferencesResponseData response = (MktgGetShopperPreferencesResponseData)Engine.Engine.ProcessRequest(request, 340);
       Assert.IsTrue(response.IsSuccess);

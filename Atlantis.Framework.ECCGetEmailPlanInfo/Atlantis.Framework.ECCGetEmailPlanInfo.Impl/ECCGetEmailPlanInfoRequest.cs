955a65f8a22f3ab99503b93e6255e44c55b837e1
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-09-15 22:01:15 +0000

    updated to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C217758

---
+++ Atlantis.Framework.ECCGetEmailPlanInfo/Atlantis.Framework.ECCGetEmailPlanInfo.Impl/ECCGetEmailPlanInfoRequest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface.Authentication;
+using Atlantis.Framework.Ecc.Interface.jsonHelpers;
+using Atlantis.Framework.ECCGetEmailPlanInfo.Interface;
+using Atlantis.Framework.ECCGetEmailPlanInfoForShopper.Impl.Json;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+
+namespace Atlantis.Framework.ECCGetEmailPlanInfo.Impl
+{
+  public class ECCGetEmailPlanInfoRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData responseData;
+      var plansRequest = (EccGetEmailPlanInfoRequestData)oRequestData;
+
+      const int pageNumber = 1;
+      const int resultsPerPage = 100000;
+      const string requestMethod = "getEmailPlanInfoForShopper";
+
+      string requestKey;
+      string authName;
+      string authToken;
+
+      string nimitzAuthXml = NetConnect.LookupConnectInfo(oConfig, ConnectLookupType.Xml);
+      NimitzAuthHelper.GetConnectionCredentials(nimitzAuthXml, out requestKey, out authName, out authToken);
+
+      try
+      {
+        var wsConfig = ((WsConfigElement)oConfig);
+
+        var oJsonRequestBody = new EccJsonEmailPlanInfoRequest
+                                 {
+                                   EmailType = ((int) plansRequest.EmailType).ToString(),
+                                   ShopperId = plansRequest.ShopperID,
+                                   ResellerId = plansRequest.ResellerId.ToString(),
+                                   AccountUid = plansRequest.AccountUid,
+                                   SubAccount = plansRequest.Subaccount,
+                                   DeepLoad = plansRequest.DeepLoad
+                                 };
+
+        var oRequest = new EccJsonRequest<EccJsonEmailPlanInfoRequest>
+                         {
+                           Id = authName,
+                           Token = authToken,
+                           Return = new EccJsonPaging(pageNumber, resultsPerPage, string.Empty, string.Empty),
+                           Parameters = oJsonRequestBody
+                         };
+
+        string sRequest = oRequest.ToJson();
+
+        string response = EccJsonRequestHandler.PostRequest(sRequest, wsConfig.WSURL, requestMethod, requestKey, plansRequest.RequestTimeout);
+        responseData = new EccGetEmailPlanInfoResponseData(response);
+      }
+      catch (Exception ex)
+      {
+        responseData = new EccGetEmailPlanInfoResponseData(oRequestData, ex);
+      }
+
+      return responseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.ECCGetEmailPlanInfo/Atlantis.Framework.ECCGetEmailPlanInfo.Interface/ECGetEmailPlanInfoResponseData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ECCGetEmailPlanInfo.Interface
+{
+  public class EccGetEmailPlanInfoResponseData : EccResponseDataBase<EccEmailPlanDetails>
+  {
+    public EccEmailPlanDetails EmailPlanDetails
+    {
+      get
+      {
+        if (Response != null && Response.Item != null && Response.Item.Results != null && Response.Item.Results.Count > 0)
+        {
+          return Response.Item.Results[0];
+        }
+        return null;
+      }
+    }
+
+    public EccGetEmailPlanInfoResponseData(string resultJson) : base(resultJson)
+    {
+    }
+    
+    public EccGetEmailPlanInfoResponseData(AtlantisException atlantisException) : base(atlantisException)
+    {
+    }
+
+    public EccGetEmailPlanInfoResponseData(RequestData requestData, Exception exception) : base (requestData, exception)
+    {
+    }
+  }
+}

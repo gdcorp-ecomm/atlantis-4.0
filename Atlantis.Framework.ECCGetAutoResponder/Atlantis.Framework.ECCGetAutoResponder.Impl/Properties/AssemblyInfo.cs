92746f9a1ad015e464cf6f149bfe92624cbc15f4
kklink <kklink@jomax.tfs.local>
2011-09-22 16:31:41 +0000

    Initial post-branch check-in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C218970

---
+++ Atlantis.Framework.ECCGetAutoResponder/Atlantis.Framework.ECCGetAutoResponder.Impl/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Atlantis.Framework.ECCGetAutoResponder.Impl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GoDaddy.com")]
+[assembly: AssemblyProduct("Atlantis.Framework.ECCGetAutoResponder.Impl")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17bfc627-2876-4be6-9273-af6c8137654b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.11.9.22")]

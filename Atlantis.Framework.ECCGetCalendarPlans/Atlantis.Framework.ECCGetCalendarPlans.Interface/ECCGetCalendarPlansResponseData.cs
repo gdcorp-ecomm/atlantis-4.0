8b4a3f1900640382651b436c926d70603d0d80d8
kklink <kklink@jomax.tfs.local>
2011-09-22 16:52:31 +0000

    initial post-branch check-in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C218987

---
+++ Atlantis.Framework.ECCGetCalendarPlans/Atlantis.Framework.ECCGetCalendarPlans.Interface/ECCGetCalendarPlansResponseData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ECCGetCalendarPlans.Interface
+{
+  public class ECCGetCalendarPlansResponseData : EccResponseDataBase<EccCalendarPlan>
+  {
+    public ECCGetCalendarPlansResponseData(string jsonResponse) : base (jsonResponse)
+    {
+    }
+
+    public ECCGetCalendarPlansResponseData(RequestData requestData, Exception ex) : base(requestData, ex)
+    {
+    }
+  }
+}

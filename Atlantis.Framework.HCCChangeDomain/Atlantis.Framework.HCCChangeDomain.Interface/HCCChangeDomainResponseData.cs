2f0ef888e67e65a457bc978b42fc5dbdb7a757c6
pmccormack <pmccormack@jomax.tfs.local>
2011-09-23 17:51:48 +0000

    Converted to 4.0. Test passed.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C219275

---
+++ Atlantis.Framework.HCCChangeDomain/Atlantis.Framework.HCCChangeDomain.Interface/HCCChangeDomainResponseData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Atlantis.Framework.HCC.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HCCChangeDomain.Interface
+{
+  public class HCCChangeDomainResponseData : IResponseData
+  {
+    HCCDomainMgmtResponse _response;
+    AtlantisException _exception = null;
+    string _resultXml = string.Empty;
+
+    bool _success = false;
+
+    public bool IsSuccess { get { return _success; } }
+
+
+    public HCCChangeDomainResponseData() { }
+    public HCCChangeDomainResponseData(HCCDomainMgmtResponse hccResponse)
+    {
+      _response = hccResponse;
+      _success = hccResponse.GetResponseStatusCode() == 0;
+    }
+
+    public HCCChangeDomainResponseData(string xml)
+    {
+      _resultXml = xml;
+      _success = true;
+    }
+
+    public HCCChangeDomainResponseData(AtlantisException exception)
+    {
+      _exception = exception;
+    }
+
+    public HCCChangeDomainResponseData(RequestData requestData, Exception exception)
+    {
+      _exception = new AtlantisException(requestData,
+        "HCCChangeDomainResponseData",
+        exception.Message,
+        string.Empty);
+    }
+
+
+    public HCCDomainMgmtResponse Response { get { return _response; } }
+
+    public string ToXML()
+    {
+      if (_response != null)
+      {
+        _resultXml = _response.ToXML();
+      }
+
+      return _resultXml;
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+  }
+}

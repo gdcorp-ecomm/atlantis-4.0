a0c991be29f26dc32418946893f39b20b73b0551
pmccormack <pmccormack@jomax.tfs.local>
2011-09-24 21:04:32 +0000

    Converted to 4.0. All tests passed.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C219462

---
+++ Atlantis.Framework.HCCIsPasswordValid/Atlantis.Framework.HCCIsPasswordValid.Interface/GetOffersWithPlIdResponseData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Atlantis.Framework.HCC.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HCCIsPasswordValid.Interface
+{
+  public class HCCIsPasswordValidResponseData : IResponseData
+  {
+    HCCIsPasswordValidResponse _hccResponse;
+    AtlantisException _exception = null;
+    string _resultXml = string.Empty;
+
+    bool _success = false;
+
+    public bool IsSuccess { get { return _success; } }
+
+    public HCCIsPasswordValidResponseData(HCCIsPasswordValidResponse hccResponse)
+    {
+      _hccResponse = hccResponse;
+      _success = true;
+    }
+
+    public HCCIsPasswordValidResponseData(AtlantisException exception)
+    {
+      _exception = exception;
+    }
+
+    public HCCIsPasswordValidResponseData(RequestData requestData, Exception exception)
+    {
+      _exception = new AtlantisException(requestData,
+        "HCCIsPasswordValidResponseData",
+        exception.Message,
+        string.Empty);
+    }
+
+    public string ToXML()
+    {
+      if (_hccResponse != null)
+      {
+        _resultXml = _hccResponse.ToXML();
+      }
+
+      return _resultXml;
+    }
+
+    public HCCIsPasswordValidResponse Response { get { return _hccResponse; } }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+  }
+}
---
+++ Atlantis.Framework.HCCIsPasswordValid/Atlantis.Framework.HCCIsPasswordValid.Interface/HCCIsPasswordValidRequestData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HCCIsPasswordValid.Interface
+{
+  public class HCCIsPasswordValidRequestData : RequestData
+  {
+    static readonly TimeSpan _requestTimeout = TimeSpan.FromSeconds(20);
+
+    public HCCIsPasswordValidRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderId,
+                                  string pathway,
+                                  int pageCount,
+                                  string password)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      Password = password;
+      RequestTimeout = _requestTimeout;
+    }
+    
+    public string Password { get; set; }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("GetCacheMD5 not implemented in HCCIsPasswordValidRequestData");
+    }
+
+
+  }
+}

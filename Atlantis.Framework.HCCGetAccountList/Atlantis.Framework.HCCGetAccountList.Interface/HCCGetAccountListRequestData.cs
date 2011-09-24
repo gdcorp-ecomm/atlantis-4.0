de4200ffecbf57d912c18c6aaf0d416da7394491
pmccormack <pmccormack@jomax.tfs.local>
2011-09-24 20:50:03 +0000

    Convert to 4.0. Tests passed.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C219460

---
+++ Atlantis.Framework.HCCGetAccountList/Atlantis.Framework.HCCGetAccountList.Interface/HCCGetAccountListRequestData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HCCGetAccountList.Interface
+{
+  public class HCCGetAccountListRequestData : RequestData
+  {
+    static readonly TimeSpan _requestTimeout = TimeSpan.FromSeconds(20);
+    public HCCGetAccountListRequestData(string shopperId,
+								                        string sourceUrl,
+								                        string orderId,
+								                        string pathway,
+								                        int pageCount) : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = _requestTimeout;
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 md5 = new MD5CryptoServiceProvider();
+
+      byte[] data = Encoding.UTF8.GetBytes(base.ShopperID);
+
+      byte[] hash = md5.ComputeHash(data);
+      string result = Encoding.UTF8.GetString(hash);
+      return result;
+    }
+  }
+}

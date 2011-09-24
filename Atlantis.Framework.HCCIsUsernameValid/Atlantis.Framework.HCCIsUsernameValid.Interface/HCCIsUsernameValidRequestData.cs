67fe4a007da618b131f0c2988835f79f8ccbfe96
pmccormack <pmccormack@jomax.tfs.local>
2011-09-24 21:10:42 +0000

    Converted to 4.0. Test passed.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C219463

---
+++ Atlantis.Framework.HCCIsUsernameValid/Atlantis.Framework.HCCIsUsernameValid.Interface/HCCIsUsernameValidRequestData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HCCIsUsernameValid.Interface
+{
+  public class HCCIsUsernameValidRequestData : RequestData
+  {
+    static readonly TimeSpan _requestTimeout = TimeSpan.FromSeconds(20);
+
+    public HCCIsUsernameValidRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderIo,
+                                  string pathway,
+                                  int pageCount,
+                                  string username)
+      : base(shopperId, sourceUrl, orderIo, pathway, pageCount)
+    {
+      UserName = username;
+      RequestTimeout = _requestTimeout;
+    }
+
+    public string UserName { get; set; }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("GetCacheMD5 not implemented in HCCIsUsernameValidRequestData");     
+    }
+
+
+  }
+}

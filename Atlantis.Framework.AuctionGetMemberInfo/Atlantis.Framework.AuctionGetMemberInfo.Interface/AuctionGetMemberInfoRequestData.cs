989f8de912d3754a3a80927a74c4b32c43a58629
triedy <triedy@jomax.tfs.local>
2011-09-26 17:32:42 +0000

    Converted to 4.0.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C219544

---
+++ Atlantis.Framework.AuctionGetMemberInfo/Atlantis.Framework.AuctionGetMemberInfo.Interface/AuctionGetMemberInfoRequestData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionGetMemberInfo.Interface
+{
+  public class AuctionGetMemberInfoRequestData : RequestData
+  {
+    private static readonly TimeSpan _requestTimeout = TimeSpan.FromSeconds(20);
+
+    public AuctionGetMemberInfoRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      RequestTimeout = _requestTimeout;
+    }
+
+    #region Overrides of RequestData
+
+    public override string GetCacheMD5()
+    {
+      MD5 md5 = new MD5CryptoServiceProvider();
+
+      byte[] data = Encoding.UTF8.GetBytes("AuctionGetMemberInfoRequestData::" + ShopperID);
+
+      byte[] hash = md5.ComputeHash(data);
+      string result = Encoding.UTF8.GetString(hash);
+      return result;
+    }
+
+    #endregion
+  }
+}

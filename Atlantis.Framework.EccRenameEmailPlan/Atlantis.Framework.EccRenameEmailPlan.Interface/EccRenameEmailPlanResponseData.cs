c6396b0d11c34bc2b75e6e75a3e6e8af380b74f9
kklink <kklink@jomax.tfs.local>
2011-10-04 16:44:34 +0000

    initial post branch check in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221147

---
+++ Atlantis.Framework.EccRenameEmailPlan/Atlantis.Framework.EccRenameEmailPlan.Interface/EccRenameEmailPlanResponseData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EccRenameEmailPlan.Interface
+{
+  public class EccRenameEmailPlanResponseData : EccResponseDataBase<object>
+  {
+
+    public EccRenameEmailPlanResponseData(string resultJson) : base (resultJson)
+    {}
+
+    public EccRenameEmailPlanResponseData(AtlantisException atlantisException) : base (atlantisException)
+    {
+    }
+
+    public EccRenameEmailPlanResponseData(RequestData requestData, Exception exception) : base(requestData, exception)
+    {
+    }
+
+
+  }
+  
+}

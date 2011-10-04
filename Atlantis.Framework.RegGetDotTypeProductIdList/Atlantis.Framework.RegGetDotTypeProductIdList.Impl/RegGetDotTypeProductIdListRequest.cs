c4042f816e0d1820c89b32168dfbd8b89b03fb69
triedy <triedy@jomax.tfs.local>
2011-10-04 20:46:26 +0000

    Converted to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221249

---
+++ Atlantis.Framework.RegGetDotTypeProductIdList/Atlantis.Framework.RegGetDotTypeProductIdList.Impl/RegGetDotTypeProductIdListRequest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegGetDotTypeProductIdList.Interface;
+
+namespace Atlantis.Framework.RegGetDotTypeProductIdList.Impl
+{
+  public class RegGetDotTypeProductIdListRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement configElement)
+    {
+      IResponseData responseData = null;
+      string responseXML = String.Empty;
+
+      RegistrationApiWebSvc.RegistrationApiWebSvc ws = new RegistrationApiWebSvc.RegistrationApiWebSvc();
+
+      try
+      {
+        RegGetDotTypeProductIdListRequestData request = (RegGetDotTypeProductIdListRequestData)requestData;
+
+        WsConfigElement ce = (WsConfigElement)configElement;
+        string requestXml = request.ToXML();
+        ws.Url = ((WsConfigElement)ce).WSURL;
+        ws.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+        responseXML = ws.GetProductIdList(requestXml);
+
+        if (!string.IsNullOrEmpty(responseXML))
+        {
+          responseData = new RegGetDotTypeProductIdListResponseData(responseXML);
+        }
+        else
+        {
+          throw new AtlantisException(requestData,
+                                      "Framework: RegGetDotTypeProductIdListRequest.RequestHandler",
+                                      "Invalid request, null or empty string returned",
+                                      string.Empty);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new RegGetDotTypeProductIdListResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new RegGetDotTypeProductIdListResponseData(requestData, ex);
+      }
+      finally
+      {
+        ws.Dispose();
+      }
+
+      return responseData;
+    }
+  }
+}
---
+++ Atlantis.Framework.RegGetDotTypeProductIdList/Atlantis.Framework.RegGetDotTypeProductIdList.Interface/RegGetDotTypeProductIdListRequestData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RegGetDotTypeProductIdList.Interface
+{
+  public class RegGetDotTypeProductIdListRequestData : RequestData
+  {
+    #region Properties
+
+    private static readonly TimeSpan _requestTimeout = TimeSpan.FromSeconds(20);
+
+    private string _dotTypeName;
+    public string DotTypeName
+    {
+      get { return _dotTypeName; }
+      set { _dotTypeName = value; }
+    }
+
+    #endregion Properties
+
+    #region Constructors
+
+    public RegGetDotTypeProductIdListRequestData(string sShopperID,
+                                         string sSourceURL,
+                                         string sOrderID,
+                                         string sPathway,
+                                         int iPageCount,
+                                         string dotTypeName)
+      : base(sShopperID, sSourceURL, sOrderID, sPathway, iPageCount)
+    {
+      this._dotTypeName = dotTypeName;
+      RequestTimeout = _requestTimeout;
+    }
+
+    #endregion Constructors
+
+    #region Public Methods
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+      xtwRequest.WriteStartElement("request");
+      xtwRequest.WriteAttributeString("tldname", this._dotTypeName);
+      xtwRequest.WriteAttributeString("plgrouptype", "0");
+      xtwRequest.WriteEndElement();
+      return sbRequest.ToString();
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    #endregion Private Methods
+  }
+}

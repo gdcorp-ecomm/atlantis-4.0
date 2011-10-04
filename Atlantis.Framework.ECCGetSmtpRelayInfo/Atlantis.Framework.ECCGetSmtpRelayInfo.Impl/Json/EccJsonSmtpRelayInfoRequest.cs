dab9758dc512ce11c3968dc40f1b8b628afac82c
kklink <kklink@jomax.tfs.local>
2011-10-04 16:40:25 +0000

    initial post branch check in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221143

---
+++ Atlantis.Framework.ECCGetSmtpRelayInfo/Atlantis.Framework.ECCGetSmtpRelayInfo.Impl/Json/EccJsonSmtpRelayInfoRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Atlantis.Framework.ECCGetSmtpRelayInfo.Impl.Json
+{ 
+  [DataContract]
+  public class EccJsonSmtpRelayInfoRequest
+  {
+      [DataMember(Name = "shopper")]
+      public string ShopperId { get; set; }
+
+      [DataMember(Name = "reseller")]
+      public string ResellerId { get; set; }
+
+      [DataMember(Name = "type")]
+      public string EmailType { get; set; }
+    }
+}

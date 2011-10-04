64c90e6c3656d9a91b1406b47a076092ad115f7f
kklink <kklink@jomax.tfs.local>
2011-10-04 16:27:08 +0000

    initial post branch checkin
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221135

---
+++ Atlantis.Framework.ECCGetOFFAccountsForShopper/Atlantis.Framework.ECCGetOFFAccountsForShopper.Int/ECCGetOFFAccountsForShopperResponseData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ECCGetOFFAccountsForShopper.Interface
+{
+  public class ECCGetOFFAccountsForShopperResponseData : EccResponseDataBase<EccOFFAccountDetails>
+  {
+
+    public ECCGetOFFAccountsForShopperResponseData(string jsonResponse)
+      : base(jsonResponse)
+    {
+    }
+
+    public ECCGetOFFAccountsForShopperResponseData(RequestData requestData, Exception ex)
+      : base(requestData, ex)
+    {
+    }
+  }
+}

0d0a1e01eb991a45026dcfc781563c5f93b2c613
kklink <kklink@jomax.tfs.local>
2011-10-04 16:36:02 +0000

    initial post branch check in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221140

---
+++ Atlantis.Framework.ECCGetProductsForEmailAddress/Atlantis.Framework.ECCGetProductsForEmailAddress.Impl/Json/ECCJsonGetProductsForEmailAddressRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Atlantis.Framework.ECCGetProductsForEmailAddress.Impl.Json
+{
+  [DataContract]
+  public class ECCJsonGetProductsForEmailAddressRequest
+  {
+    [DataMember(Name = "shopper")]
+    public string ShopperId { get; set; }
+
+    [DataMember(Name = "reseller")]
+    public string ResellerId { get; set; }
+
+    [DataMember(Name = "emailaddress")]
+    public string EmailAddress { get; set; }
+
+    [DataMember(Name = "subaccount")]
+    public string Subaccount { get; set; }
+  }
+}
---
+++ Atlantis.Framework.ECCGetProductsForEmailAddress/Atlantis.Framework.ECCGetProductsForEmailAddress.Int/ECCGetProductsForEmailAddressRequestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ECCGetProductsForEmailAddress.Interface
+{
+  public class ECCGetProductsForEmailAddressRequestData : RequestData 
+  {
+    public ECCGetProductsForEmailAddressRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount, int privateLabelId, string emailAddress, string subaccount) : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      PrivateLabelId = privateLabelId;
+      EmailAddress = emailAddress;
+      Subaccount = subaccount;
+    }
+
+    public int PrivateLabelId { get; set; }
+
+    public string EmailAddress { get; set; }
+
+    public string Subaccount { get; set; }
+
+    #region Overrides of RequestData
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("ECCGetProductsForEmailAddress is not a cacheable request.");
+    }
+
+    #endregion
+  }
+}

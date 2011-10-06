c866a1488a0dedf117d70344b04cfbd1aa2528a0
trwalker <trwalker@jomax.tfs.local>
2011-10-06 15:02:21 +0000

    Atlantis.Framework.DCCGetDomainRankInfo converted to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C221641

---
+++ Atlantis.Framework.DCCGetDomainRankInfo/Atlantis.Framework.DCCGetDomainRankInfo.Impl/DCCGetDomainRankInfoAsyncRequest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Atlantis.Framework.DCCGetDomainRankInfo.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DCCGetDomainRankInfo.Impl
+{
+  public class DCCGetDomainRankInfoAsyncRequest : IAsyncRequest
+  {
+    public IAsyncResult BeginHandleRequest(RequestData requestData, ConfigElement config, AsyncCallback callback, object state)
+    {
+      DCCGetDomainRankInfoRequestData request = (DCCGetDomainRankInfoRequestData)requestData;
+
+      WebScoreBossWebSvc.WebScoreBossWebSvc domainRankInfoWS = new WebScoreBossWebSvc.WebScoreBossWebSvc();
+      domainRankInfoWS.Url = ((WsConfigElement)config).WSURL;
+      domainRankInfoWS.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+
+      AsyncState asyncState = new AsyncState(requestData, config, domainRankInfoWS, state);
+      IAsyncResult asyncResult = domainRankInfoWS.BeginGetRankInfoForDomainIds(request.ToXML(), callback, asyncState);
+
+      return asyncResult;
+    }
+
+    public IResponseData EndHandleRequest(IAsyncResult asyncResult)
+    {
+      IResponseData responseData;
+      string responseXml;
+      AsyncState asyncState = (AsyncState)asyncResult.AsyncState;
+      WebScoreBossWebSvc.WebScoreBossWebSvc domainRankService = null;
+
+      try
+      {
+        domainRankService = (WebScoreBossWebSvc.WebScoreBossWebSvc)asyncState.Request;
+        responseXml = domainRankService.EndGetRankInfoForDomainIds(asyncResult);
+
+        if (!string.IsNullOrEmpty(responseXml) && responseXml.Contains("<success>1</success>"))
+        {
+          responseData = new DCCGetDomainRankInfoResponseData(responseXml);
+        }
+        else
+        {
+          DCCGetDomainRankInfoRequestData reqData = (DCCGetDomainRankInfoRequestData)asyncState.RequestData;
+          responseData = new DCCGetDomainRankInfoResponseData(reqData, responseXml);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DCCGetDomainRankInfoResponseData(exAtlantis);
+      }
+
+      catch (Exception ex)
+      {
+        responseData = new DCCGetDomainRankInfoResponseData(asyncState.RequestData, ex);
+      }
+      finally
+      {
+        if(domainRankService != null)
+        {
+          domainRankService.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+  }
+}

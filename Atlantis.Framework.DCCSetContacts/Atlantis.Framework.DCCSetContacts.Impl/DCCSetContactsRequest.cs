cdaf67d70d14e346ac7d58c22c3bfad9ca694867
trwalker <trwalker@jomax.tfs.local>
2011-10-11 00:27:24 +0000

    Atlantis.Framework.DCCSetContacts conversion to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C222447

---
+++ Atlantis.Framework.DCCSetContacts/Atlantis.Framework.DCCSetContacts.Impl/DCCSetContactsRequest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Atlantis.Framework.DCCSetContacts.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DCCSetContacts.Impl
+{
+  public class DCCSetContactsRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DCCSetContactsResponseData responseData;
+      string responseXml = string.Empty;
+      string verifyResponseXml;
+      DsWebValidate.RegDCCValidateWebSvc validationService = null;
+
+      try
+      {
+        DCCSetContactsRequestData oRequest = (DCCSetContactsRequestData)oRequestData;
+
+        string verifyAction;
+        string verifyDomains;
+        oRequest.XmlToVerify(out verifyAction, out verifyDomains);
+
+        validationService = new DsWebValidate.RegDCCValidateWebSvc();
+        string sValidateUrl = ((WsConfigElement)oConfig).WSURL.Replace("RegDCCRequestWebSvc/RegDCCRequestWebSvc.dll", "RegDCCValidateWebSvc/RegDCCValidateWebSvc.asmx");
+        validationService.Url = sValidateUrl;
+        validationService.Timeout = (int)oRequest.RequestTimeout.TotalMilliseconds;
+        string validateResponseXml = validationService.ValidateContactUpdate(verifyAction, verifyDomains);
+
+        if (validateResponseXml.Contains("<ACTIONRESULTS></ACTIONRESULTS>"))
+        {
+          DsWebVerify.RegDCCVerifyWebSvcService oDsWebVerify = new DsWebVerify.RegDCCVerifyWebSvcService();
+          oDsWebVerify.Url = ((WsConfigElement)oConfig).WSURL.Replace("RegDCCRequestWebSvc/RegDCCRequestWebSvc.dll", "RegDCCVerifyWebSvc/RegDCCVerifyWebSvc.dll");
+          oDsWebVerify.Timeout = (int)oRequest.RequestTimeout.TotalMilliseconds;
+          verifyResponseXml = oDsWebVerify.VerifyContactUpdate(verifyAction, verifyDomains);
+
+          if (verifyResponseXml.Contains("ActionResultID=\"0\""))
+          {
+            DsWebSubmit.RegDCCRequestWebSvcService oDsWeb = new DsWebSubmit.RegDCCRequestWebSvcService();
+            oDsWeb.Url = ((WsConfigElement)oConfig).WSURL;
+            oDsWeb.Timeout = (int)oRequest.RequestTimeout.TotalMilliseconds;
+
+            responseXml = oDsWeb.SubmitRequestStandard(oRequest.XmlToSubmit());
+            responseData = new DCCSetContactsResponseData(responseXml);
+          }
+          else
+          {            
+            responseData = new DCCSetContactsResponseData(verifyResponseXml,oRequestData);
+          }
+        }
+        else
+        {
+          responseData = new DCCSetContactsResponseData(validateResponseXml, false);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DCCSetContactsResponseData(responseXml, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new DCCSetContactsResponseData(responseXml, oRequestData, ex);
+      }
+      finally
+      {
+        if(validationService != null)
+        {
+          validationService.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+  }
+}
---
+++ Atlantis.Framework.DCCSetContacts/Atlantis.Framework.DCCSetContacts.Interface/DCCSetContactsResponseData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DCCSetContacts.Interface
+{
+  public class DCCSetContactsResponseData : IResponseData
+  {
+    string _responseXml;
+    string _validationMsg = "";
+    AtlantisException _exception;
+    bool _isSuccess;
+
+    public DCCSetContactsResponseData(string responseXml)
+    {
+      _responseXml = responseXml;
+      PopulateFromXML(responseXml);
+    }
+
+    public DCCSetContactsResponseData(string validationXml, bool isSuccess)
+    {
+      _responseXml = validationXml;
+      _isSuccess = isSuccess;
+      _validationMsg = parseValidationDesc(validationXml);
+    }
+
+    public DCCSetContactsResponseData(string responseXml, AtlantisException exAtlantis)
+    {
+      _responseXml = responseXml;
+      _exception = exAtlantis;
+    }
+
+    public DCCSetContactsResponseData(string responseXml, RequestData oRequestData, Exception ex)
+    {
+      _validationMsg = ParseVerificationDesc(responseXml);
+      _responseXml = responseXml;
+      _exception = new AtlantisException(oRequestData,
+                                   "DCCSetContactsResponseData",
+                                   ex.Message,
+                                   oRequestData.ToXML());
+    }
+
+
+    public DCCSetContactsResponseData(string responseXml, RequestData oRequestData)
+    {
+      _validationMsg = ParseVerificationDesc(responseXml);
+      _responseXml = responseXml;      
+    }
+
+
+    private string ParseVerificationDesc(string xml)
+    {
+      string sResult = "";
+      XmlDocument oDoc = new XmlDocument();
+      oDoc.LoadXml(xml);
+
+      XmlElement oEle = (XmlElement)oDoc.SelectSingleNode("/VERIFICATION/ACTIONRESULTS/ACTIONRESULT");
+      sResult = oEle.Attributes["Description"].Value;
+      return sResult;
+    }
+
+    string parseValidationDesc(string validationDoc)
+    {
+      string sResult = "";
+      XmlDocument oDoc = new XmlDocument();
+      oDoc.LoadXml(validationDoc);
+
+      XmlElement oEle = (XmlElement)oDoc.SelectSingleNode("/VALIDATION/ACTIONRESULTS/ACTIONRESULT");
+      sResult = oEle.Attributes["Description"].Value;
+      return sResult;
+    }
+
+    void PopulateFromXML(string resultXML)
+    {
+      if (resultXML.Contains("<success"))
+      {
+        _isSuccess = true;
+      }
+        /*
+      else if (resultXML.Contains("<error"))
+      {
+        XmlDocument responseDoc = new XmlDocument();
+        responseDoc.LoadXml(resultXML);
+        responseDoc.Attributes["desc"].Value
+
+      }
+         * */
+    }
+
+    public bool IsSuccess
+    {
+      get { return (_exception == null && _isSuccess); }
+    }
+
+    public string ValidationMsg
+    {
+      get { return _validationMsg; }
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    public string ToXML()
+    {
+      return _responseXml;
+    }
+  }
+}

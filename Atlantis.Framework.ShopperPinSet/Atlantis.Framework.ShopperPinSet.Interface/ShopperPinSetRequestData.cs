9ca0ef0a8f1ee1af7441f12ac6d24732ad0e1d0b
pmccormack <pmccormack@jomax.tfs.local>
2011-10-20 19:54:05 +0000

    Converted 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224275

---
+++ Atlantis.Framework.ShopperPinSet/Atlantis.Framework.ShopperPinSet.Interface/ShopperPinSetRequestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ShopperPinSet.Interface
+{
+  public class ShopperPinSetRequestData : RequestData
+  {
+    public ShopperPinSetRequestData(string shopperID,
+                            string sourceURL,
+                            string orderID,
+                            string pathway,
+                            int pageCount)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("ShopperPinSet is not a cacheable request.");
+    }
+  }
+}

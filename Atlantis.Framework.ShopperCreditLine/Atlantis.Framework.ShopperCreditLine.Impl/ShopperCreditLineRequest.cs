31e534f43ed2b00f1eb1e251b10c198d7c6eccbb
pmccormack <pmccormack@jomax.tfs.local>
2011-10-20 19:46:49 +0000

    Convert to 4.0. Updated config ws url to http://gdcomm.dev.glbt1.gdg/WSgdLOCPublic/Service.asmx
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224271

---
+++ Atlantis.Framework.ShopperCreditLine/Atlantis.Framework.ShopperCreditLine.Impl/ShopperCreditLineRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.ShopperCreditLine.Interface;
+
+namespace Atlantis.Framework.ShopperCreditLine.Impl
+{
+
+  public class ShopperCreditLineRequest : IRequest
+  {
+
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      ShopperCreditLineResponseData oResponseData;
+      string sResponseXML = "";
+      try
+      {
+        ShopperCreditLineRequestData oShopperCreditRequest = (ShopperCreditLineRequestData)oRequestData;
+        gdLoc.Service oSvc = new gdLoc.Service();
+        oSvc.Url = ((WsConfigElement)oConfig).WSURL;
+        oSvc.Timeout = (int)oShopperCreditRequest.RequestTimeout.TotalMilliseconds;
+        bool shopperLOC;
+        sResponseXML = oSvc.ShopperHasLOC(oShopperCreditRequest.ShopperID, out shopperLOC);
+        if (sResponseXML.IndexOf("<error>", StringComparison.OrdinalIgnoreCase) > -1)
+        {
+          AtlantisException exAtlantis = new AtlantisException(oRequestData,
+                                                               "ShopperCreditLine.RequestHandler",
+                                                               sResponseXML,
+                                                               oRequestData.ToXML());
+
+          oResponseData = new ShopperCreditLineResponseData(sResponseXML, exAtlantis);
+        }
+        else
+        {
+          oResponseData = new ShopperCreditLineResponseData(sResponseXML, shopperLOC);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        oResponseData = new ShopperCreditLineResponseData(sResponseXML, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new ShopperCreditLineResponseData(sResponseXML, oRequestData, ex);
+      }
+      return oResponseData;
+    }
+
+    #endregion
+
+  }
+}
---
+++ Atlantis.Framework.ShopperCreditLine/Atlantis.Framework.ShopperCreditLine.Interface/ShopperCreditLineRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ShopperCreditLine.Interface
+{
+  public class ShopperCreditLineRequestData : RequestData
+  {
+    public ShopperCreditLineRequestData(string sShopperID,
+                                   string sSourceURL,
+                                   string sOrderID,
+                                   string sPathway,
+                                   int iPageCount)
+                                   : base(sShopperID, sSourceURL, sOrderID, sPathway, iPageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(2);
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 md5 = new MD5CryptoServiceProvider();
+      md5.Initialize();
+      byte[] md5Bytes
+        = md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(ShopperID));
+      return BitConverter.ToString(md5Bytes).Replace("-", "");
+    }
+  }
+}

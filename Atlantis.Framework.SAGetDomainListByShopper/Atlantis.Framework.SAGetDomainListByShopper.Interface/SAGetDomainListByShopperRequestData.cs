e37795b7cc86d0abb86f98512d05e58b23d4e520
pmccormack <pmccormack@jomax.tfs.local>
2011-10-20 18:25:33 +0000

    Converted to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224244

---
+++ Atlantis.Framework.SAGetDomainListByShopper/Atlantis.Framework.SAGetDomainListByShopper.Interface/SAGetDomainListByShopperRequestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.SAGetDomainListByShopper.Interface
+{
+  public class SAGetDomainListByShopperRequestData : RequestData
+  {
+    public SAGetDomainListByShopperRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      RequestTimeout = new TimeSpan(0, 0, 0, 30);
+    }
+    
+    public override string GetCacheMD5()
+    {
+      MD5 md5 = new MD5CryptoServiceProvider();
+
+      byte[] data = Encoding.UTF8.GetBytes(ShopperID);
+
+      byte[] hash = md5.ComputeHash(data);
+      string result = Encoding.UTF8.GetString(hash);
+      return result;
+
+    }
+
+
+  }
+}

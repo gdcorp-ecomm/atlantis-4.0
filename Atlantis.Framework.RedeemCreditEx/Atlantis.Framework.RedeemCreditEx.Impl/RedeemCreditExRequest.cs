1b8f67a7702c68c060fae6a4b66d33695e0e65fe
Sridhar Thota <sthota@godaddy.com>
2011-10-21 15:33:46 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224420

---
+++ Atlantis.Framework.RedeemCreditEx/Atlantis.Framework.RedeemCreditEx.Impl/RedeemCreditExRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RedeemCreditEx.Interface;
+
+namespace Atlantis.Framework.RedeemCreditEx.Impl
+{
+  public class RedeemCreditExRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      RedeemCreditExRequestData requestData = (RedeemCreditExRequestData)oRequestData;
+      RedeemCreditExResponseData responseData = null;
+      string response = string.Empty;
+      try
+      {
+        using (CMS.WSCgdCMSService cms = new CMS.WSCgdCMSService())
+        {
+          cms.Url = ((WsConfigElement)oConfig).WSURL;
+          response = cms.RedeemCreditEx(requestData.RedeemXML);
+        }
+
+        if (string.Compare(response.Substring(0, 36), "<RESPONSE><MESSAGE>Success</MESSAGE>", true) != 0)
+        {
+          string data = string.Format("Redeem XML: {0}, Response: {1}", requestData.RedeemXML, response);
+          AtlantisException aex = new AtlantisException(oRequestData, "RedeemCreditExRequest.RequestHandler", "Could not redeem credit", data);
+          responseData = new RedeemCreditExResponseData(aex);
+        }
+        else
+        {
+          responseData = new RedeemCreditExResponseData(response);
+        }
+      }
+      catch (Exception ex)
+      {
+        string data = "Redeem XML: " + requestData.RedeemXML;
+        AtlantisException aex = new AtlantisException(oRequestData, "RedeemCreditExRequest.RequestHandler", "Could not redeem credit", data, ex);
+        responseData = new RedeemCreditExResponseData(aex);
+      }
+
+      return responseData;
+    }
+  }
+}
---
+++ Atlantis.Framework.RedeemCreditEx/Atlantis.Framework.RedeemCreditEx.Interface/RedeemCreditExResponseData.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RedeemCreditEx.Interface
+{
+  public class RedeemCreditExResponseData : IResponseData
+  {
+    #region Properties
+
+    public bool Success { get; private set; }
+    public string ResourceId { get; private set; }
+    public AtlantisException AtlException { get; set; }
+    public string XML { get; set; }
+
+    #endregion 
+
+    public RedeemCreditExResponseData() { }
+
+    public RedeemCreditExResponseData(string xml)
+    {
+      XmlDocument xDoc = new XmlDocument();
+      xDoc.LoadXml(xml);
+      XmlNode resourseNode = xDoc.SelectSingleNode("RESPONSE/CREDIT/ResourceID");
+      if (resourseNode != null)
+      {
+        ResourceId = resourseNode.InnerText.Trim();
+        Success = true;
+      }
+    }
+
+    public RedeemCreditExResponseData(AtlantisException aex)
+    {
+      AtlException = aex;
+      Success = false;
+    }
+
+    public AtlantisException GetException()
+    {
+      return AtlException;
+    }
+
+    public string ToXML()
+    {
+      return XML;
+    }
+
+  }
+}

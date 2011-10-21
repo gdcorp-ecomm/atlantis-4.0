506ec940fd5d1b407ee1039e69f1dcbef8c8b794
Sridhar Thota <sthota@godaddy.com>
2011-10-21 14:55:51 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224409

---
+++ Atlantis.Framework.GetCreditGroupSummary/Atlantis.Framework.GetCreditGroupSummary.Impl/GetCreditGroupSummaryRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml;
+using Atlantis.Framework.GetCreditGroupSummary.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GetCreditGroupSummary.Impl
+{
+  public class GetCreditGroupSummaryRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      GetCreditGroupSummaryRequestData requestData = (GetCreditGroupSummaryRequestData)oRequestData;
+      GetCreditGroupSummaryResponseData responseData = new GetCreditGroupSummaryResponseData();
+      string response = string.Empty;
+      try
+      {
+        using (CMS.WSCgdCMSService cms = new CMS.WSCgdCMSService())
+        {
+          cms.Url = ((WsConfigElement)oConfig).WSURL;
+          response = cms.GetCreditGroupSummary(requestData.ShopperID, requestData.DisplayGroupID, 0);
+        }
+
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(response);
+
+        if(string.Compare(doc.SelectSingleNode("/RESPONSE/MESSAGE").InnerText, "Success", true) != 0)
+        {
+          string data = string.Format("DisplayGroupID: {0}, Response: {1}", requestData.DisplayGroupID, response);
+          responseData.AtlException = new AtlantisException(requestData,
+            "GetCreditGroupSummaryRequest.RequestHandler", "Could not retrieve summary of credit group", data);
+        }
+        else
+        {
+          responseData.XML = doc.SelectSingleNode("/RESPONSE/CREDITS").OuterXml;
+        }
+      }
+      catch (Exception ex)
+      {
+        string data = string.Format("DisplayGroupID: {0}", requestData.DisplayGroupID);
+        responseData.AtlException = new AtlantisException(requestData,
+          "GetCreditGroupSummaryRequest.RequestHandler", "Could not retrieve summary of credit group", data, ex);
+      }
+
+      return responseData;
+    }
+  }
+}
---
+++ Atlantis.Framework.GetCreditGroupSummary/Atlantis.Framework.GetCreditGroupSummary.Interface/GetCreditGroupSummaryRequestData.cs
@@ -0,0 +1,26 @@
+﻿using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GetCreditGroupSummary.Interface
+{
+  public class GetCreditGroupSummaryRequestData : RequestData
+  {
+
+    private GetCreditGroupSummaryRequestData()
+      : base("", "", "", "", 0)
+    { }
+
+    public GetCreditGroupSummaryRequestData(string shopperID, string sourceURL, string orderID, 
+                                            string pathway, int pageCount, int displayGroupID)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      DisplayGroupID = displayGroupID;
+    }
+
+    public override string GetCacheMD5()
+    {
+      return string.Empty;
+    }
+
+    public int DisplayGroupID { get; set; }
+  }
+}

a29d4385de97bac0d0eea402cd5660f1b9ba5cb3
pmccormack <pmccormack@jomax.tfs.local>
2011-10-24 23:57:38 +0000

    Converted to 4.0. Updated WS so that impl can build successfully. Provided new WS url.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224804

---
+++ Atlantis.Framework.GiftCardBalance/Atlantis.Framework.GiftCardBalance.Interface/GiftCardBalanceResponseData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GiftCardBalance.Interface
+{
+  public class GiftCardBalanceResponseData : IResponseData
+  {
+    private AtlantisException _exception = null;
+    private string _resultXML = string.Empty;
+    private bool _success = false;
+    private int _accountBalance = 0;
+
+    public int AccountBalance
+    {
+      get
+      {
+        return _accountBalance;
+      }
+    }
+
+    public bool IsSuccess
+    {
+      get
+      {
+        return _success;
+      }
+    }
+
+    public GiftCardBalanceResponseData(int giftCardBalance)
+    {
+      _accountBalance = giftCardBalance;
+      _success = true;
+    }
+
+    public GiftCardBalanceResponseData(AtlantisException atlantisException)
+    {
+      this._exception = atlantisException;
+    }
+
+    public GiftCardBalanceResponseData(RequestData requestData, Exception exception)
+    {
+      this._exception = new AtlantisException(requestData,
+                                   "GiftCardBalanceResponseData",
+                                   exception.Message,
+                                   requestData.ToXML());
+    }
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+
+      xtwRequest.WriteStartElement("INFO");
+      xtwRequest.WriteAttributeString("AcctBalance", System.Convert.ToString(_accountBalance));
+      xtwRequest.WriteEndElement();
+      return sbRequest.ToString();
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.GiftCardBalance/Atlantis.Framework.GiftCardBalance.impl/GiftCardBalanceRequest.cs
@@ -0,0 +1,32 @@
+﻿using Atlantis.Framework.GiftCardBalance.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.GiftCardBalance.Impl
+{
+  public class GiftCardBalanceRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      GiftCardBalanceRequestData getBalance = (GiftCardBalanceRequestData)oRequestData;
+      GiftCardBalanceResponseData oResponseData = null;
+      string sResponseXML = string.Empty;
+      try
+      {
+        WscGiftCard.wscGiftCardService oSvc = new WscGiftCard.wscGiftCardService();
+        oSvc.Url = ((WsConfigElement)oConfig).WSURL;
+        string resultXML = string.Empty;
+        int result = oSvc.GetGiftCardBalance(getBalance.AccountNumber,getBalance.OrderID);
+        oResponseData = new GiftCardBalanceResponseData(result);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        oResponseData = new GiftCardBalanceResponseData(oRequestData, exAtlantis);
+      }
+      return oResponseData;
+    }
+
+    #endregion
+  }
+}

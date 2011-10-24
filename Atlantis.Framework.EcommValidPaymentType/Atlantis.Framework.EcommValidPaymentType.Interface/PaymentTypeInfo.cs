d6aa3442d89745c7d482c97c2b4ea2f6f190d246
pmccormack <pmccormack@jomax.tfs.local>
2011-10-24 20:46:38 +0000

    Converted to 4.0. Test Successfully. Added additonal fail message information.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224704

---
+++ Atlantis.Framework.EcommValidPaymentType/Atlantis.Framework.EcommValidPaymentType.Interface/PaymentTypeInfo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Atlantis.Framework.EcommValidPaymentType.Interface
+{
+  public class PaymentTypeInfo
+  {
+    public string PaymentType { get; private set; }
+  
+    public string Name { get; private set; }
+
+    public PaymentTypeInfo(string paymentType, string name)
+    {
+      PaymentType = paymentType;
+      Name = name;
+    }
+  }
+}

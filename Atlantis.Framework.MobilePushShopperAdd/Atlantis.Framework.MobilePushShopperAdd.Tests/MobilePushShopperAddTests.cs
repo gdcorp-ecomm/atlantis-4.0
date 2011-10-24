6c48ca163633db8c5467d3eafc1778b68a688c53
trwalker <trwalker@jomax.tfs.local>
2011-10-24 21:47:21 +0000

    Atlantis.Framework.MobilePushShopperDelete initial check in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224741

--- Atlantis.Framework.MobilePushShopperAdd/Atlantis.Framework.MobilePushShopperAdd.Tests/MobilePushShopperAddTests.cs
+++ Atlantis.Framework.MobilePushShopperAdd/Atlantis.Framework.MobilePushShopperAdd.Tests/MobilePushShopperAddTests.cs
@@ -46,6 +46,36 @@ namespace Atlantis.Framework.MobilePushShopperAdd.Tests
 
     [TestMethod]
     [DeploymentItem("atlantis.config")]
+    public void ValidShopperAddNotification2()
+    {
+      string registrationId = "d3d4d197-638d-47b3-a379-1a9f094cf504";
+      string deviceId = "5d022bdf-e080-45b2-8712-4f12eef79923";
+      string mobileAppId = "1"; //iPhone
+
+      MobilePushShopperAddRequestData requestData = new MobilePushShopperAddRequestData(registrationId,
+                                                                                        mobileAppId,
+                                                                                        deviceId,
+                                                                                        "840820",
+                                                                                        "http://www.MobilePushShopperAddTests.com",
+                                                                                        string.Empty,
+                                                                                        Guid.NewGuid().ToString(),
+                                                                                        1);
+
+      try
+      {
+        MobilePushShopperAddResponseData responseData = (MobilePushShopperAddResponseData)Engine.Engine.ProcessRequest(requestData, 426);
+        WriteMessageToConsole(responseData.Xml);
+        Assert.IsTrue(responseData.IsSuccess);
+      }
+      catch (Exception ex)
+      {
+        WriteMessageToConsole(ex.Message + " " + ex.StackTrace);
+        Assert.Fail();
+      }
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
     public void InValidShopperAddNotification()
     {
       string registrationId = "d3d4d197-638d-47b3-a379-1a9f094cf502";

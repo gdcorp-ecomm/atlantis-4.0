b134b8db9e71f41391400a4243d90cd286b357f5
pmccormack <pmccormack@jomax.tfs.local>
2011-10-24 21:18:15 +0000

    Converted to 4.0. Refactored Nimitz implmenation.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224729

---
+++ Atlantis.Framework.ExpressCheckoutDelete/Atlantis.Framework.ExpressCheckoutDelete.Interface/ExpressCheckoutDeleteRequestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ExpressCheckoutDelete.Interface
+{
+  public class ExpressCheckoutDeleteRequestData : RequestData
+  {
+    public ExpressCheckoutDeleteRequestData(
+      string shopperId,      
+      string sourceURL,
+      string orderId,
+      string pathway,
+      int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

ca068e6a975134a75047dd95106658a2a214254a
pmccormack <pmccormack@jomax.tfs.local>
2011-10-25 23:18:19 +0000

    Added App Config. Test succesfully
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225037

---
+++ Atlantis.Framework.MyaRenewalBundleItems/Atlantis.Framework.MyaRenewalBundleItems.Tests/MyaRenewalBundleItemsTests.cs
@@ -0,0 +1,44 @@
+﻿using Atlantis.Framework.Engine;
+using Atlantis.Framework.MyaRenewalBundleItems.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyaRenewalBundleItems.Tests
+{
+  [TestClass]
+  public class MyaRenewalBundleItemsTests
+  {
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("App.config")]
+    public void MYAGetChildItems()
+    {
+      MyaRenewalBundleItemsRequestData requestData = new MyaRenewalBundleItemsRequestData(
+        "822497", string.Empty, string.Empty, string.Empty, 0, 13689);      
+      MyaRenewalBundleItemsResponseData response = (MyaRenewalBundleItemsResponseData)Engine.ProcessRequest(requestData, 196);
+      var foo = response.BundleXML;
+      Assert.IsTrue(response.IsSuccess);
+    }
+  }
+}

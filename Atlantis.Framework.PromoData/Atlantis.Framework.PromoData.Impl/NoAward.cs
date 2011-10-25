86cb0a4f968a5611af048830edadd42e403e5782
triedy <triedy@jomax.tfs.local>
2011-10-25 19:04:59 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C224940

---
+++ Atlantis.Framework.PromoData/Atlantis.Framework.PromoData.Impl/NoAward.cs
@@ -0,0 +1,46 @@
+﻿using Atlantis.Framework.PromoData.Interface;
+
+namespace Atlantis.Framework.PromoData.Impl
+{
+  class NoAward : IProductAward
+  {
+    public string AwardType
+    {
+      get { return string.Empty; }
+    }
+
+    public int QuantityLimit
+    {
+      get { return -1; }
+    }
+
+    private int _productId;
+    public int ProductId
+    {
+      get { return _productId; }
+    }
+    public string AwardField
+    {
+      get { return string.Empty; }
+    }
+    public string AwardFieldValue
+    {
+      get { return string.Empty; }
+    }
+
+    public IPromoCondition PromoCondition
+    {
+      get { return new PromoCondition(); }
+    }
+
+    public NoAward(int productId)
+    {
+      this._productId = productId;
+    }
+
+    public int GetAwardAmount(string currencyType)
+    {
+      return 0;
+    }
+  }
+}
---
+++ Atlantis.Framework.PromoData/Atlantis.Framework.PromoData.Tests/PromoDataTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Atlantis.Framework.PromoData.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.PromoData.Tests
+{
+  [TestClass]
+  public class PromoDataTests
+  {
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void GetPromoData()
+    {
+      int amount = 0;
+      string awardType = string.Empty;
+      //PromoDataRequestData request = new PromoDataRequestData("77311", string.Empty,
+      //  String.Empty, string.Empty, 0, "sample");
+      //PromoDataRequestData request = new PromoDataRequestData("77311", string.Empty,
+      //  String.Empty, string.Empty, 0, "iscSSLStd1299");
+      PromoDataRequestData request = new PromoDataRequestData("77311", string.Empty,
+       String.Empty, string.Empty, 0, "iscSSLStd1299");
+      request.RequestTimeout = new TimeSpan(0,0,60);
+      PromoDataResponseData response
+        = (PromoDataResponseData)Engine.Engine.ProcessRequest(request, 365);
+
+      if (response.IsValid 
+        && response.PromoProduct.IsActivePromoForPrivateLabelTypeId(1))
+      {
+        IProductAward award = response.PromoProduct.GetProductAward(1);
+        amount = award.GetAwardAmount("EUR");
+        awardType = award.AwardType;
+      }
+
+      //Assert.IsTrue(amount > 0 && !string.IsNullOrEmpty(awardType));
+      Assert.IsTrue(response.IsValid);
+    }
+  }
+}

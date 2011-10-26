327ff14c51b11e25878d131115f860b140855efb
triedy <triedy@jomax.tfs.local>
2011-10-26 18:06:23 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225144

---
+++ Atlantis.Framework.PurchaseEmail/Atlantis.Framework.PurchaseEmail.Interface/Emails/DBPAdminFeesConfirmationEmail.cs
@@ -0,0 +1,37 @@
+﻿using Atlantis.Framework.Providers.ProviderContainer.Impl;
+using Atlantis.Framework.PurchaseEmail.Interface.Providers;
+
+namespace Atlantis.Framework.PurchaseEmail.Interface.Emails
+{
+  internal class DBPAdminFeesConfirmationEmail : AdminFeeConfirmationEmail
+  {
+    public DBPAdminFeesConfirmationEmail(OrderData orderData, EmailRequired emailRequired, ObjectProviderContainer objectContainer)
+      : base(orderData, emailRequired, objectContainer)
+    {
+    }
+
+    protected override string DomainAgreement
+    {
+      get
+      {
+        return Links.GetUrl(LinkTypes.DomainsByProxy, "popup/popup/ShowDoc.aspx", "pageid", "domain_nameproxy"); ;
+      }
+    }
+
+    protected override string AgreementText
+    {
+      get
+      {
+        return "Domains By Proxy's Domain Name Proxy Agreement";
+      }
+    }
+
+    protected override string ProcessingText
+    {
+      get
+      {
+        return "DBP's processing of this inquiry";
+      }
+    }
+  }
+}

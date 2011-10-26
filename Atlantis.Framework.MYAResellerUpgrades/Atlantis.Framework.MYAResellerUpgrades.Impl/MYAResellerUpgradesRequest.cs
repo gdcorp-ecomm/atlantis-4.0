073f6bd53e3023641553816eb5d6363511e85957
smyers <smyers@jomax.tfs.local>
2011-10-26 18:48:51 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225172

---
+++ Atlantis.Framework.MYAResellerUpgrades/Atlantis.Framework.MYAResellerUpgrades.Impl/MYAResellerUpgradesRequest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.MYAResellerUpgrades.Interface;
+//using netConnect;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Atlantis.Framework.MYAResellerUpgrades.Impl
+{
+  public class MYAResellerUpgradesRequest : IRequest
+  {
+    #region Parameter Constants
+
+    private const string CONFIG_STORED_PROCEDURE = "gdshop_resellerUpgradeByRecurringID_sp";
+    private const string RESOURCE_ID_PARAM = "@n_recurring_id";
+
+    #endregion Parameter Constants
+
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      MYAResellerUpgradesResponseData responseData = null;
+      List<ResellerUpgrade> resellerUpgrades = new List<ResellerUpgrade>();
+
+      try
+      {
+        MYAResellerUpgradesRequestData myaResellerUpgradeRequestData = (MYAResellerUpgradesRequestData)requestData;
+        resellerUpgrades = GetUpgrades(myaResellerUpgradeRequestData, config);
+
+        responseData = new MYAResellerUpgradesResponseData(resellerUpgrades);
+      }
+
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new MYAResellerUpgradesResponseData(exAtlantis);
+      }
+
+      catch (Exception ex)
+      {
+        responseData = new MYAResellerUpgradesResponseData(requestData, ex);
+      }
+
+      return responseData;
+    }
+
+    private List<ResellerUpgrade> GetUpgrades(MYAResellerUpgradesRequestData requestData, ConfigElement config)
+    {
+      List<ResellerUpgrade> resellerUpgrades = new List<ResellerUpgrade>();
+      using (var cn = new SqlConnection(Nimitz.NetConnect.LookupConnectInfo(config)))
+      {
+        cn.Open();
+
+        using (var cmd = new SqlCommand(CONFIG_STORED_PROCEDURE, cn))
+        {
+          cmd.CommandTimeout = (int)requestData.RequestTimeout.TotalSeconds;
+          cmd.CommandType = CommandType.StoredProcedure;
+          cmd.Parameters.Add(new SqlParameter(RESOURCE_ID_PARAM, requestData.BillingResourceId));
+
+          using (SqlDataReader dr = cmd.ExecuteReader())
+          {
+            while (dr.Read())
+            {
+              resellerUpgrades.Add(PopulateObjectFromDB(dr));
+            }
+          }
+        }
+      }
+
+      return resellerUpgrades;
+    }
+
+    private ResellerUpgrade PopulateObjectFromDB(IDataReader dr)
+    {
+      ResellerUpgrade resellerUpgrade = new ResellerUpgrade();
+
+      resellerUpgrade.ProductId = Convert.ToInt32(dr["upgrade_pf_id"]);
+      resellerUpgrade.Description = dr["name"] == DBNull.Value ? string.Empty : Convert.ToString(dr["name"].ToString());
+
+      return resellerUpgrade;
+    }
+  }
+}

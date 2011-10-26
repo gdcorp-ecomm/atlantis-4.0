4cd2005c11ba3b45f21fc91a52cbd62849eb4704
pmccormack <pmccormack@jomax.tfs.local>
2011-10-26 00:21:54 +0000

    Converted to 4.0. Test successfully.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225053

---
+++ Atlantis.Framework.PremiumDNS/Atlantis.Framework.PremiumDNS/Atlantis.Framework.PremiumDNS.Interface/GetPremiumDNSDefaultNameServersRequestData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PremiumDNS.Interface
+{
+  public class GetPremiumDNSDefaultNameServersRequestData : RequestData
+  {
+    public int PrivateLabelId { get; set; }
+
+    private TimeSpan _requestTimeout = TimeSpan.FromSeconds(3);
+
+    public GetPremiumDNSDefaultNameServersRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount, int privateLabelId) : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      PrivateLabelId = privateLabelId;
+      RequestTimeout = _requestTimeout;
+    }
+
+    #region Overrides of RequestData
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("GetPremiumDNSDefaultNameServers is not a cacheable request.");
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.PremiumDNS/Atlantis.Framework.PremiumDNS/Atlantis.Framework.PremiumDNS.Interface/GetPremiumDNSStatusRequestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PremiumDNS.Interface
+{
+  public class GetPremiumDNSStatusRequestData : RequestData
+  {
+    public int PrivateLabelId { get; set; }
+
+    private TimeSpan _requestTimeout = TimeSpan.FromSeconds(3);
+
+    public GetPremiumDNSStatusRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount, int privateLabelId) : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      PrivateLabelId = privateLabelId;
+      RequestTimeout = _requestTimeout;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("GetPremiumDNSStatus is not a cacheable request.");
+    }
+  }
+}

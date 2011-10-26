a9c4c8bf8939e0a5b9300965e4c2a11ab9838294
triedy <triedy@jomax.tfs.local>
2011-10-26 17:15:06 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225121

---
+++ Atlantis.Framework.Providers.PromoData/Atlantis.Framework.Providers.PromoData/PromoDataEngineRequests.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Atlantis.Framework.Providers.PromoData
+{
+  public static class PromoDataEngineRequests
+  {
+    private static int _getPromoDataRequest = 365;
+    public static int GetPromoDataRequest
+    {
+      get { return _getPromoDataRequest; }
+      set { _getPromoDataRequest = value; }
+    }
+  }
+}

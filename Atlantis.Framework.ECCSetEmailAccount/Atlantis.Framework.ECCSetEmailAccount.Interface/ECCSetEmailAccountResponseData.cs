a2361db3ec163ecc221b4031522802a45b021c4e
triedy <triedy@jomax.tfs.local>
2011-10-26 21:58:00 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225242

---
+++ Atlantis.Framework.ECCSetEmailAccount/Atlantis.Framework.ECCSetEmailAccount.Interface/ECCSetEmailAccountResponseData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Atlantis.Framework.Ecc.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ECCSetEmailAccount.Interface
+{
+  public class EccSetEmailAccountResponseData : EccResponseDataBase<object>
+  {
+    public EccSetEmailAccountResponseData(string resultJson) : base(resultJson)
+    {
+    }
+
+    public EccSetEmailAccountResponseData(AtlantisException atlantisException) : base(atlantisException)
+    {
+    }
+
+    public EccSetEmailAccountResponseData(RequestData requestData, Exception exception):base(requestData, exception)
+    {
+    }
+  }
+}

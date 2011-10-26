f00a74f144d3d9fd795af454615bab7343618815
pmccormack <pmccormack@jomax.tfs.local>
2011-10-26 00:04:45 +0000

    Converted to 4.0.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225050

---
+++ Atlantis.Framework.MyaRenewalChildItems/Atlantis.Framework.MyaRenewalChildItems.Interface/RenewalChildItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Atlantis.Framework.MYAGetExpiringProductsDetail.Interface
+{
+  public class RenewalChildItem
+  {
+    public int PFID { get; set; }
+    public int ProductTypeID { get; set; }    
+    public int Quantity { get; set; }
+
+  }
+}

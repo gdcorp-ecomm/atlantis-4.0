f6cf97704150c6da56b08f0adad7351a2456b82e
triedy <triedy@jomax.tfs.local>
2011-10-26 20:02:17 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225199

---
+++ Atlantis.Framework.PrivacyRenewalBasketXml/Atlantis.Framework.PrivacyRenewalBasketXml.Impl/PrivacyRenewalBasketXmlRequest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+using Atlantis.Framework.PrivacyRenewalBasketXml.Impl.EComPrivacyRenewalXmlService;
+using Atlantis.Framework.PrivacyRenewalBasketXml.Interface;
+
+namespace Atlantis.Framework.PrivacyRenewalBasketXml.Impl
+{
+  public class PrivacyRenewalBasketXmlRequest : IRequest
+  {
+    private const string PROC_NAME = "gdshop_billingGetBundleID_sp";
+    private const string DOMAIN_BILLING_RESOURCE_ID_PARAM = "@domain_resource_id";
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      string responseXml = string.Empty;
+      string errorXml = string.Empty;
+      PrivacyRenewalBasketXmlResponseData responseData;
+      wscgdDomainProtectionXMLService oEComWeb = null;
+
+      try
+      {
+        PrivacyRenewalBasketXmlRequestData oRequest = (PrivacyRenewalBasketXmlRequestData)oRequestData;
+        oEComWeb = new wscgdDomainProtectionXMLService();
+        oEComWeb.Url = ((WsConfigElement)oConfig).WSURL;
+        oEComWeb.Timeout = (int)oRequest.RequestTimeout.TotalMilliseconds;
+        responseXml = oEComWeb.GetBundleXML(oRequest.ToXML(), out errorXml);
+
+        // We have to make a call to the GoDaddyBilling database to get the protection parent_bundle_id
+        if(oRequest.RenewalType == PrivacyRenewalBasketXmlRequestData.PrivateRenewalType.Protection)
+        {
+          int parentBundleId = LookUpProtectionBundleId(oRequest,oConfig);
+          responseData = new PrivacyRenewalBasketXmlResponseData(responseXml, parentBundleId, oRequestData, errorXml);
+        }
+        else
+        {
+          responseData = new PrivacyRenewalBasketXmlResponseData(responseXml, oRequestData, errorXml);
+        }
+      }
+      catch (Exception ex)
+      {
+        responseData = new PrivacyRenewalBasketXmlResponseData(responseXml, oRequestData, ex);
+      }
+      finally
+      {
+        if (oEComWeb != null)
+        {
+          oEComWeb.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+
+    private int LookUpProtectionBundleId(PrivacyRenewalBasketXmlRequestData request, ConfigElement oConfig)
+    {
+      int parentBundleId = 0;
+
+      string connectionString = LookupConnectionString(request, oConfig);
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        using (SqlCommand command = new SqlCommand(PROC_NAME, connection))
+        {
+          command.CommandType = CommandType.StoredProcedure;
+          command.CommandTimeout = 5;
+          command.Parameters.Add(new SqlParameter(DOMAIN_BILLING_RESOURCE_ID_PARAM, request.DomainBillingResourceId));
+          connection.Open();
+          using (SqlDataReader reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              object parentBundleIdObject = reader[0];
+
+              if (parentBundleIdObject == null)
+              {
+                throw new AtlantisException(request,
+                                            MethodBase.GetCurrentMethod().DeclaringType.FullName,
+                                            "Error retrieving parentBundleId for domainBillingResourceId: " + request.DomainBillingResourceId,
+                                            string.Empty);
+              }
+
+              parentBundleId = Convert.ToInt32(parentBundleIdObject);
+            }
+          }
+        }
+      }
+
+      return parentBundleId;
+    }
+
+    private static string LookupConnectionString(PrivacyRenewalBasketXmlRequestData request, ConfigElement oConfig)
+    {
+      string nimitzConnectionString = NetConnect.LookupConnectInfo(oConfig, ConnectLookupType.NetConnectionString);
+
+      if (string.IsNullOrEmpty(nimitzConnectionString))
+      {
+        throw new AtlantisException(request, "LookupConnectionString",
+                                    "Database connection string lookup failed", 
+                                    string.Empty);
+      }
+      return nimitzConnectionString;
+    }
+  }
+}
---
+++ Atlantis.Framework.PrivacyRenewalBasketXml/Atlantis.Framework.PrivacyRenewalBasketXml.Interface/PrivacyRenewalBasketXmlRequestData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PrivacyRenewalBasketXml.Interface
+{
+  public class PrivacyRenewalBasketXmlRequestData : RequestData
+  {
+    public enum PrivateRenewalType
+    {
+      Privacy,
+      Business,
+      Protection
+    }
+
+    public int PrivacyProductId { get; private set; }
+    public PrivateRenewalType RenewalType { get; private set; }
+    public int Duration { get; private set; }
+    public int DomainBillingResourceId { get; private set; }
+
+    /// <summary>
+    /// Default of 30 seconds.
+    /// </summary>
+    private TimeSpan _requestTimeout = TimeSpan.FromSeconds(30);
+
+    public PrivacyRenewalBasketXmlRequestData(int privacyProductId,
+                                             PrivateRenewalType renewalType,
+                                             int duration,
+                                             int domainBillingResouceId,
+                                             string shopperId,
+                                             string sourceUrl,
+                                             string orderId,
+                                             string pathway,
+                                             int pageCount)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      PrivacyProductId = privacyProductId;
+      RenewalType = renewalType;
+      Duration = duration;
+      DomainBillingResourceId = domainBillingResouceId;
+      RequestTimeout = _requestTimeout;      
+    }
+
+    /* <request bundle_pf_id="776001" billing_resource_id="376100" duration="2.000" /> */
+    public override string ToXML()
+    {
+      XmlDocument requestDoc = new XmlDocument();
+      requestDoc.LoadXml("<request/>");
+
+      XmlElement oRoot = requestDoc.DocumentElement;
+
+      AddAttribute(oRoot, "bundle_pf_id", PrivacyProductId.ToString());
+      AddAttribute(oRoot, "duration", Duration.ToString());
+      AddAttribute(oRoot, "billing_resource_id", DomainBillingResourceId.ToString());
+
+      return requestDoc.InnerXml;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("PrivacyRenewalBasketXmlRequestData is not a cacheable request.");
+    }
+
+    private static void AddAttribute(XmlNode node, string attributeName, string attributeValue)
+    {
+      XmlAttribute attribute = node.OwnerDocument.CreateAttribute(attributeName);
+      node.Attributes.Append(attribute);
+      attribute.Value = attributeValue;
+    }
+  }
+}

c298cc8b0a40d8a72bc2e57dbb1203da1cced3f8
triedy <triedy@jomax.tfs.local>
2011-10-27 17:26:50 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225361

---
+++ Atlantis.Framework.DCCForwardingUpdate/Atlantis.Framework.DCCForwardingUpdate.Tests/DCCForwardingUpdateTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atlantis.Framework.DCCForwardingUpdate.Interface;
+
+
+namespace Atlantis.Framework.DCCForwardingUpdate.Tests
+{
+  [TestClass]
+  public class DCCForwardingUpdateTests
+  {
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ForwardingUpdatePermanentValid()
+    {
+      DCCForwardingUpdateRequestData request = new DCCForwardingUpdateRequestData("857020", 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  0, 
+                                                                                  1,
+                                                                                  1667126, 
+                                                                                  "MOBILE_CSA_DCC", 
+                                                                                  "http://testy123.com", 
+                                                                                  RedirectType.Permanent,
+                                                                                  true);
+      
+      DCCForwardingUpdateResponseData response = (DCCForwardingUpdateResponseData)Engine.Engine.ProcessRequest(request, 111);
+
+      Console.WriteLine(string.Format("Call Success: {0}", !string.IsNullOrEmpty(response.ResponseXml)));
+      Console.WriteLine(string.Format("Operation Success: {0}", response.IsSuccess));
+      Console.WriteLine(string.Format("Error: {0}", !string.IsNullOrEmpty(response.ValidationMsg) ? response.ValidationMsg : "none"));
+      Console.WriteLine(response.ToXML());
+
+      Assert.IsTrue(response.IsSuccess || (!response.IsSuccess && !string.IsNullOrEmpty(response.ValidationMsg)));
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ForwardingUpdateTemporaryValid()
+    {
+      DCCForwardingUpdateRequestData request = new DCCForwardingUpdateRequestData("857020", 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  0, 
+                                                                                  1, 
+                                                                                  1665502, 
+                                                                                  "MOBILE_CSA_DCC", 
+                                                                                  "http://testy123.com", 
+                                                                                  RedirectType.Temporary,
+                                                                                  true);
+
+      DCCForwardingUpdateResponseData response = (DCCForwardingUpdateResponseData)Engine.Engine.ProcessRequest(request, 111);
+      
+      Console.WriteLine(string.Format("Call Success: {0}", !string.IsNullOrEmpty(response.ResponseXml)));
+      Console.WriteLine(string.Format("Operation Success: {0}", response.IsSuccess));
+      Console.WriteLine(string.Format("Error: {0}", !string.IsNullOrEmpty(response.ValidationMsg) ? response.ValidationMsg:"none"));
+      Console.WriteLine(response.ToXML());
+
+      Assert.IsTrue(response.IsSuccess || (!response.IsSuccess && !string.IsNullOrEmpty(response.ValidationMsg)));
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ForwardingUpdateForDomainThatShopperDoesNotOwn()
+    {
+      DCCForwardingUpdateRequestData request = new DCCForwardingUpdateRequestData("847235", 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  string.Empty, 
+                                                                                  0, 
+                                                                                  1, 
+                                                                                  1665502, 
+                                                                                  "MOBILE_CSA_DCC",
+                                                                                  "http://testy123.com",
+                                                                                  RedirectType.Permanent,
+                                                                                  true);
+
+
+      DCCForwardingUpdateResponseData response = (DCCForwardingUpdateResponseData)Engine.Engine.ProcessRequest(request, 111);
+      
+      Console.WriteLine(string.Format("Call Success: {0}", !string.IsNullOrEmpty(response.ResponseXml)));
+      Console.WriteLine(string.Format("Operation Success: {0}", response.IsSuccess));
+      Console.WriteLine(string.Format("Error: {0}", !string.IsNullOrEmpty(response.ValidationMsg) ? response.ValidationMsg : "none"));
+      Console.WriteLine(response.ToXML());
+
+      // This is returning success, the DCC team is fixing this in a future release
+      Assert.IsTrue(response.IsSuccess || (!response.IsSuccess && !string.IsNullOrEmpty(response.ValidationMsg)));
+    }
+  }
+}

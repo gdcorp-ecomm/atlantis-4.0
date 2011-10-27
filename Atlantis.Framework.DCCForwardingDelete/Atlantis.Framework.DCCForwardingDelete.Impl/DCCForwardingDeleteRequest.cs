8f2943c3457289c8a0d7a141b9386bfbf87a12fd
triedy <triedy@jomax.tfs.local>
2011-10-27 18:01:06 +0000

    Convert to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C225371

---
+++ Atlantis.Framework.DCCForwardingDelete/Atlantis.Framework.DCCForwardingDelete.Impl/DCCForwardingDeleteRequest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Atlantis.Framework.DCCForwardingDelete.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DCCForwardingDelete.Impl
+{
+  public class DCCForwardingDeleteRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DCCForwardingDeleteResponseData responseData = null;
+      string responseXml = string.Empty;
+      string verifyResponseXml = string.Empty;
+
+      DsWebVerify.RegDCCVerifyWebSvcService oDsWebVerify = new DsWebVerify.RegDCCVerifyWebSvcService();
+
+      try
+      {
+        DCCForwardingDeleteRequestData oRequest = (DCCForwardingDeleteRequestData)oRequestData;
+
+        string verifyAction = "";
+        string verifyDomains = "";
+        oRequest.XmlToVerify(out verifyAction, out verifyDomains);
+
+
+        string sVerifyUrl = ((WsConfigElement)oConfig).WSURL.Replace("RegDCCRequestWebSvc/RegDCCRequestWebSvc.dll", "RegDCCVerifyWebSvc/RegDCCVerifyWebSvc.dll");
+
+        oDsWebVerify.Url = sVerifyUrl;
+        oDsWebVerify.Timeout = (int)oRequest.RequestTimeout.TotalMilliseconds;
+        verifyResponseXml = oDsWebVerify.VerifyDomainForwardingDelete(verifyAction, verifyDomains);
+
+        if (verifyResponseXml.Contains("ActionResultID=\"0\""))
+        {
+          using (DsWebSubmit.RegDCCRequestWebSvcService oDsWeb = new DsWebSubmit.RegDCCRequestWebSvcService())
+          {
+            oDsWeb.Url = ((WsConfigElement) oConfig).WSURL;
+            oDsWeb.Timeout = (int) oRequest.RequestTimeout.TotalMilliseconds;
+
+            responseXml = oDsWeb.SubmitRequestStandard(oRequest.XmlToSubmit());
+            responseData = new DCCForwardingDeleteResponseData(responseXml);
+          }
+        }
+        else
+        {
+          responseData = new DCCForwardingDeleteResponseData(verifyResponseXml, oRequestData);
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DCCForwardingDeleteResponseData(responseXml, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new DCCForwardingDeleteResponseData(responseXml, oRequestData, ex);
+      }
+      finally
+      {
+        oDsWebVerify.Dispose();
+      }
+
+      return responseData;
+    }
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.DCCForwardingDelete/Atlantis.Framework.DCCForwardingDelete.Tests/DCCForwardingDeleteTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Atlantis.Framework.DCCForwardingDelete.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.DCCForwardingDelete.Tests
+{
+  [TestClass]
+  public class DCCForwardingDeleteTests
+  {
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void DCCForwardingDeleteValid()
+    {
+      DCCForwardingDeleteRequestData request = new DCCForwardingDeleteRequestData("857020", string.Empty, string.Empty, string.Empty, 0, 1, 1667126, "MOBILE_CSA_DCC");
+      DCCForwardingDeleteResponseData response = (DCCForwardingDeleteResponseData)Engine.Engine.ProcessRequest(request, 112);
+
+      Console.WriteLine(string.Format("Operation Success: {0}", response.IsSuccess));
+      Console.WriteLine(string.Format("Error: {0}", !string.IsNullOrEmpty(response.ValidationMsg) ? response.ValidationMsg : "none"));
+      Console.WriteLine(response.ToXML());
+
+      Assert.IsTrue(response.IsSuccess || (!response.IsSuccess && !string.IsNullOrEmpty(response.ValidationMsg)));
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void DCCForwardingDeleteForDomainThatShopperDoesNotOwn()
+    {
+      DCCForwardingDeleteRequestData request = new DCCForwardingDeleteRequestData("847235", string.Empty, string.Empty, string.Empty, 0, 1, 1665499, "MOBILE_CSA_DCC");
+      DCCForwardingDeleteResponseData response = (DCCForwardingDeleteResponseData)Engine.Engine.ProcessRequest(request, 112);
+
+      Console.WriteLine(string.Format("Operation Success: {0}", response.IsSuccess));
+      Console.WriteLine(string.Format("Error: {0}", !string.IsNullOrEmpty(response.ValidationMsg) ? response.ValidationMsg : "none"));
+      Console.WriteLine(response.ToXML());
+
+      // This is returning success true, the DCC team is fixing this in a future release
+      Assert.IsTrue(response.IsSuccess || (!response.IsSuccess && !string.IsNullOrEmpty(response.ValidationMsg)));
+    }
+  }
+}

ee60460da1dcbe4ccb2a4af7f7777fe167ab74aa
syukna:167 <syukna:167@jomax.tfs.local>
2011-11-03 04:25:07 +0000

    Skeleton
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C226547

---
+++ Atlantis.Framework.EcommInvoiceToBasket/Atlantis.Framework.EcommInvoiceToBasket.Impl/EcommInvoiceToBasketRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInvoiceToBasket.Impl
+{
+  class EcommInvoiceToBasketRequest: IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Atlantis.Framework.EcommInvoiceToBasket/Atlantis.Framework.EcommInvoiceToBasket.Interface/EcommInvoiceToBasketRequestData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInvoiceToBasket.Interface
+{
+  class EcommInvoiceToBasketRequestData: RequestData
+  {
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

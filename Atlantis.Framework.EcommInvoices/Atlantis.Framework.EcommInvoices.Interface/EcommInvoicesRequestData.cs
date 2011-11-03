8d9864fae610f36608f0fc8b59e9af9352df283e
syukna:167 <syukna:167@jomax.tfs.local>
2011-11-03 04:12:37 +0000

    Skeleton
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C226542

---
+++ Atlantis.Framework.EcommInvoices/Atlantis.Framework.EcommInvoices.Interface/EcommInvoicesRequestData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInvoices.Interface
+{
+  class EcommInvoicesRequestData: RequestData
+  {
+    public EcommInvoicesRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    { 
+    
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Atlantis.Framework.EcommInvoices/Atlantis.Framework.EcommInvoices.Interface/EcommInvoicesResponseData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInvoices.Interface
+{
+  class EcommInvoicesResponseData : IResponseData
+  {
+
+    public EcommInvoicesResponseData()
+    {
+
+    }
+
+    public EcommInvoicesResponseData(RequestData oRequestData, Exception ex)
+    {
+     // _success = false;
+    //  _atlException = new AtlantisException(oRequestData, "EcommInvoicesResponseData", ex.Message, string.Empty);
+    }
+
+    public string ToXML()
+    {
+      throw new NotImplementedException();
+    }
+
+    public AtlantisException GetException()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
+

dd95a1833f88dacc0a6c28932bcb2a0587651aee
cgregory <cgregory@jomax.tfs.local>
2011-11-03 17:43:10 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C226633

---
+++ Atlantis.Framework.GetDomainInfo/Atlantis.Framework.GetDomainInfo.Interface/GetDomainInfoResponseData.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+using Atlantis.Framework.Interface;
+
+
+namespace Atlantis.Framework.GetDomainInfo.Interface
+{
+  public class GetDomainInfoResponseData : IResponseData
+  {
+    private AtlantisException _exception = null;
+    private string _resultXML = string.Empty;
+    private bool _success = false;
+    
+
+    public GetDomainInfoResponseData(string resultXML)
+    {
+      _resultXML = resultXML;
+      _success = true;
+    }
+
+    public GetDomainInfoResponseData(AtlantisException atlantisException)
+    {
+      this._exception = atlantisException;
+    }
+
+    public GetDomainInfoResponseData(RequestData requestData, Exception exception)
+    {
+      this._exception = new AtlantisException(requestData,
+                                   "GetDomainInfoResponseData",
+                                   exception.Message,
+                                   requestData.ToXML());
+    }
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      return _resultXML;
+    }
+    
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    public bool IsSuccess
+    {
+      get
+      {
+        return _success;
+      }
+    }
+    #endregion
+
+  }
+}
+
---
+++ Atlantis.Framework.GetDomainInfo/Atlantis.Framework.GetDomainInfo.Tests/GetDomainInfoTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atlantis.Framework.GetDomainInfo.Interface;
+
+namespace Atlantis.Framework.GetDomainInfo.Test
+{
+  /// <summary>
+  /// Summary description for GetDomainInfoTests
+  /// </summary>
+  [TestClass]
+  public class GetDomainInfoTests
+  {
+    public GetDomainInfoTests()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void GetDomainInfoTypeBasic()
+    {
+      GetDomainInfoRequestData request = new GetDomainInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, "849362", "BATHRAKAALI.COM");
+
+      GetDomainInfoResponseData response = (GetDomainInfoResponseData) Engine.Engine.ProcessRequest(request, 93);
+
+      Assert.IsTrue(response.IsSuccess);
+    }
+  }
+}

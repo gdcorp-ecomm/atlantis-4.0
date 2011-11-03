efd5324c5a13b9d79301ce2cf46ff0c0a84fc599
syukna:167 <syukna:167@jomax.tfs.local>
2011-11-03 04:19:47 +0000

    Skeleton
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C226546

---
+++ Atlantis.Framework.EcommInvoiceDetails/Atlantis.Framework.EcommInvoiceDetails.Impl/EcommInvoiceDetailsRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInvoiceDetails.Impl
+{
+  class EcommInvoiceDetailsRequest: IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Atlantis.Framework.EcommInvoiceDetails/Atlantis.Framework.EcommInvoiceDetails.Interface/EcommInvoiceDetailsRequestData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+namespace Atlantis.Framework.EcommInvoiceDetails.Interface
+{
+  class EcommInvoiceDetailsRequestData : RequestData
+  {
+    public EcommInvoiceDetailsRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

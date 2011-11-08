ac778eec00d1b70b2cfa7b93325c4785d77fc949
syukna:167 <syukna:167@jomax.tfs.local>
2011-11-08 19:52:49 +0000

    Update strong pw requirment reg ex.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C227455

--- Atlantis.Framework.AuthChangePassword/Atlantis.Framework.AuthChangePassword.Impl/AuthChangePasswordRequest.cs
+++ Atlantis.Framework.AuthChangePassword/Atlantis.Framework.AuthChangePassword.Impl/AuthChangePasswordRequest.cs
@@ -12,7 +12,7 @@ namespace Atlantis.Framework.AuthChangePassword.Impl
     private static readonly Regex _newPasswordInvalidCharactersRegex = new Regex("[^\x20-\x7E]", RegexOptions.Compiled);
     private static readonly Regex _newLoginInvalidCharactersRegex = new Regex(@"[^\-A-Za-z0-9]", RegexOptions.Compiled);
     private static readonly Regex _newHintInvalidCharactersRegex = new Regex("[^\x20-\x3b\x3f-\x7e]", RegexOptions.Compiled);
-    private static readonly Regex _meetsStrongPwReqs = new Regex("(?=^[A-z])(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@$%]).*$");
+    private static readonly Regex _meetsStrongPwReqs = new Regex("(?=.*[A-Z])(?=.{8,})(?=.*\\d).*$");
 
     private HashSet<int> ValidateRequest(AuthChangePasswordRequestData request, WScgdAuthenticateService service)
     {

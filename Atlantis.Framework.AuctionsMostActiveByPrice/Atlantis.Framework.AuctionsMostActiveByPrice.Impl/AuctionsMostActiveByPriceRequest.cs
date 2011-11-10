10325cd779774e2250d9eebf85bc6f515ec2a2b8
Sridhar Thota <sthota@godaddy.com>
2011-11-10 18:57:23 +0000

    update to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C227942

---
+++ Atlantis.Framework.AuctionsMostActiveByPrice/Atlantis.Framework.AuctionsMostActiveByPrice.Impl/AuctionsMostActiveByPriceRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Atlantis.Framework.AuctionsMostActiveByPrice.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionsMostActiveByPrice.Impl
+{
+  public class AuctionsMostActiveByPriceRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      string responseXML = string.Empty;
+      AuctionsMostActiveByPriceResponseData responseData = null;
+
+      try
+      {
+        AuctionsMostActiveByPriceRequestData auctionData = (AuctionsMostActiveByPriceRequestData)oRequestData;
+        using (TdnamDomainService.trpLandingDomainsService auctionWS = new TdnamDomainService.trpLandingDomainsService())
+        {
+          auctionWS.Url = ((WsConfigElement)oConfig).WSURL;
+          auctionWS.Timeout = (int)auctionData.RequestTimeout.TotalMilliseconds;
+          responseXML = auctionWS.RetrieveMostActiveByPrice(auctionData.AuctionCount);
+        }
+        responseData = new AuctionsMostActiveByPriceResponseData(responseXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new AuctionsMostActiveByPriceResponseData(responseXML, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new AuctionsMostActiveByPriceResponseData(responseXML, oRequestData, ex);
+      }
+      return responseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.AuctionsMostActiveByPrice/Atlantis.Framework.AuctionsMostActiveByPrice.Interface/AuctionsMostActiveByPriceRequestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionsMostActiveByPrice.Interface
+{
+  public class AuctionsMostActiveByPriceRequestData : RequestData
+  {
+    public int AuctionCount { get; set; }
+
+    public AuctionsMostActiveByPriceRequestData( 
+      string shopperID, string sourceURL, string orderID, string pathway,
+      int pageCount, int auctionCount)
+      : base (shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      AuctionCount = auctionCount;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("AuctionsByArea is not a cacheable request.");
+    }
+  }
+}

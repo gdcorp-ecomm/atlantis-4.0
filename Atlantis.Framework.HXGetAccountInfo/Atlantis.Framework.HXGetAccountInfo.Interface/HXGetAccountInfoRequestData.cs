8a4d17dd9577d6ac0e2a2f93822110acbadf991e
Michael Micco <mmicco@godaddy.com>
2011-11-10 16:03:09 +0000

    4.0 conversion of HXGetAccountInfo
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C227880

---
+++ Atlantis.Framework.HXGetAccountInfo/Atlantis.Framework.HXGetAccountInfo.Interface/HXGetAccountInfoRequestData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HXGetAccountInfo.Interface
+{
+  public class HXGetAccountInfoRequestData : RequestData
+  {
+    public string AccountUid { get; set; }
+
+    public HXGetAccountInfoRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderId,
+                                  string pathway,
+                                  int pageCount,
+                                  string accountUid)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      if (string.IsNullOrEmpty(accountUid)) { throw new ArgumentException("accountUid cannot be null or empty"); }
+      AccountUid = accountUid;
+      RequestTimeout = TimeSpan.FromSeconds(6.0);
+    }
+
+    public override string GetCacheMD5()
+    {
+      // It is not expected that this request will use Session or Data cache
+      throw new NotImplementedException("HxGetAccountInfo is not a cacheable Request");
+    }
+
+    public override string ToXML()
+    {
+      return string.Format(@"<HXGetAccountInfoRequestData><ShopperId>{0}</ShopperId><AccountUid>{1}</AccountUid></HXGetAccountInfoRequestData>", ShopperID, AccountUid);
+    }
+  }
+}
---
+++ Atlantis.Framework.HXGetAccountInfo/Atlantis.Framework.HXGetAccountInfo.Interface/HXGetAccountInfoResponseData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.HXGetAccountInfo.Interface
+{
+  public class HXGetAccountInfoResponseData : IResponseData
+  {
+    private AtlantisException _exception = null;
+    private bool _success = false;
+    public HXAccountInfo AccountInfo { get; private set; }
+
+    public bool IsSuccess
+    {
+      get
+      {
+        return _success;
+      }
+    }
+
+    public HXGetAccountInfoResponseData(HXAccountInfo accountInfo)
+    {
+      _success = true;
+      AccountInfo = accountInfo;
+    }
+
+    public HXGetAccountInfoResponseData(RequestData requestData, Exception exception)
+    {
+      this._exception = new AtlantisException(requestData,
+                                   "HXGetAccountInfoResponseData",
+                                   exception.Message,
+                                   requestData.ToXML());
+    }
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      return string.Empty;
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    #endregion
+
+  }
+}

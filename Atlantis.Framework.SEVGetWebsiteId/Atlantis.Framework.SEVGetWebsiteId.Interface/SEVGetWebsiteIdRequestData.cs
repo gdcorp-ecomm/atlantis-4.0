801bfadbd1f188776cc5d8040a1b6c008d57d420
Kent Searle <ksearle@godaddy.com>
2011-11-22 22:59:02 +0000

    SEV Special Functionality for UserWebsiteId
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C230322

---
+++ Atlantis.Framework.SEVGetWebsiteId/Atlantis.Framework.SEVGetWebsiteId.Interface/SEVGetWebsiteIdRequestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.SEVGetWebsiteId.Interface
+{
+  public class SEVGetWebsiteIdRequestData : RequestData
+  {
+
+    public SEVGetWebsiteIdRequestData(string shopperId
+      , string sourceUrl
+      , string orderId
+      , string pathway
+      , int pageCount)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("GetCacheMD5 not implemented in SEVGetWebsiteIdRequestData");     
+    }
+  }
+}

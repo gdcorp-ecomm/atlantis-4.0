bc3bbef9b01ebaaca76217d599481fe2a0bf70f4
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-11-28 02:44:36 +0000

    Return result code
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C230563

--- Atlantis.Framework.OrionAddAttribute/Atlantis.Framework.OrionAddAttribute.Interface/OrionAddAttributeResponseData.cs
+++ Atlantis.Framework.OrionAddAttribute/Atlantis.Framework.OrionAddAttribute.Interface/OrionAddAttributeResponseData.cs
@@ -8,12 +8,14 @@ namespace Atlantis.Framework.OrionAddAttribute.Interface
     public AtlantisException AtlantisException { get; private set; }
     public bool IsSuccess { get; private set; }
     public string Error { get; private set; }
+    public int ResponseCode { get; private set; }
 
     public OrionAddAttributeResponseData(int responseCode, string attributeUid, string error)
     {
       AttributeUid = attributeUid;
       IsSuccess = responseCode == 0 && !string.IsNullOrEmpty(attributeUid);
       Error = error;
+      ResponseCode = responseCode;
     }
 
     public OrionAddAttributeResponseData(AtlantisException exception)

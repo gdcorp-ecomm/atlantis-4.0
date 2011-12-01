b16de6b6ba4896bf6853832f55f75748a9332b74
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-01 23:49:58 +0000

    converted to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C231619

---
+++ Atlantis.Framework.AuctionsRetrieveDomains/Atlantis.Framework.AuctionsRetrieveDomains.Impl/AuctionsRetrieveDomains.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using Atlantis.Framework.AuctionsRetrieveDomains.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionsRetrieveDomains.Impl
+{
+  public class AuctionsRetrieveDomainsRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DataSet ds = null;
+      AuctionsRetrieveDomainsResponseData responseData = null;
+
+      TdnamDomainService.trpLandingDomainsService auctionWS = null;
+      try
+      {
+        AuctionsRetrieveDomainsRequestData auctionData = (AuctionsRetrieveDomainsRequestData)oRequestData;
+        auctionWS = new TdnamDomainService.trpLandingDomainsService();
+        auctionWS.Url = ((WsConfigElement)oConfig).WSURL;
+        auctionWS.Timeout = (int)auctionData.RequestTimeout.TotalMilliseconds;
+        ds = auctionWS.RetrieveDomains(auctionData.AuctionCount);
+        responseData = new AuctionsRetrieveDomainsResponseData(ds);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new AuctionsRetrieveDomainsResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new AuctionsRetrieveDomainsResponseData(ds, oRequestData, ex);
+      }
+      finally
+      {
+        if (auctionWS != null)
+        {
+          auctionWS.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.AuctionsRetrieveDomains/Atlantis.Framework.AuctionsRetrieveDomains.Interface/AuctionsRetrieveDomainsRequestData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionsRetrieveDomains.Interface
+{
+  public class AuctionsRetrieveDomainsRequestData : RequestData
+  {
+    private int _auctionCount;
+
+    public AuctionsRetrieveDomainsRequestData( 
+      string shopperID, string sourceURL, string orderID, string pathway,
+      int pageCount, int auctionCount)
+      : base (shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      _auctionCount = auctionCount;
+      RequestTimeout = new TimeSpan(0, 0, 4);
+    }
+
+    public int AuctionCount
+    {
+      get { return _auctionCount; }
+      set { _auctionCount = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(_auctionCount.ToString());
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", string.Empty);
+    }
+  }
+}

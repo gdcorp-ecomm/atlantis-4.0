8866c548487533c8e4b7e6f304bce2b342a0ae6d
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-01 23:15:40 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C231607

---
+++ Atlantis.Framework.AuctionRecommendations/Atlantis.Framework.AuctionRecommendations.Tests/AuctionRecommendationsTests.cs
@@ -0,0 +1,34 @@
+﻿using Atlantis.Framework.AuctionRecommendations.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.AuctionRecommendations.Tests
+{
+  [TestClass]
+  public class AuctionRecommendationsTests
+  {
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void AuctionRecommendationsGet()
+    {
+      AuctionRecommendationsResponseData response = null;
+      string domain = "topdog.com";
+      AuctionDomainToCheck auctionCheck = new AuctionDomainToCheck(domain);
+      AuctionRecommendationsRequestData request = new AuctionRecommendationsRequestData("850398", string.Empty, string.Empty, string.Empty, 0, auctionCheck, string.Empty, string.Empty, "whois", 3000);
+      response = (AuctionRecommendationsResponseData)Engine.Engine.ProcessRequest(request, 324);
+
+      Assert.IsTrue(response._isSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ActionRecommendationsGetShopperId()
+    {
+      //please contact the auctions team for a new searchbyshopperkey
+      AuctionRecommendationsResponseData response = null;
+      AuctionRecommendationsRequestData request = new AuctionRecommendationsRequestData("850398", "1327C016-B5D3-4eca-B70E-E8F10A15FCDB", string.Empty, string.Empty, string.Empty, 0, "sales", 3000);
+      response = (AuctionRecommendationsResponseData)Engine.Engine.ProcessRequest(request, 324);
+
+      Assert.IsTrue(response._isSuccess);
+    }
+  }
+}
---
+++ Atlantis.Framework.AuctionRecommendations/Atlantis.Framework.AuctionRecommendations/AuctionRecommendationsRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Atlantis.Framework.AuctionRecommendations.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.AuctionRecommendations.Impl
+{
+  public class AuctionRecommendationsRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      AuctionRecommendationsResponseData oReponseData = null;
+
+      gdAuctionsLeprechaunWS.gdAuctionsLeprechaunWS service = null;
+      try
+      {
+        AuctionRecommendationsRequestData request = (AuctionRecommendationsRequestData)oRequestData;
+        service = new gdAuctionsLeprechaunWS.gdAuctionsLeprechaunWS();
+        service.Url = ((WsConfigElement)oConfig).WSURL;
+        service.Timeout = (int)Math.Truncate(request.RequestTimeout.TotalMilliseconds);
+        string responseXML = service.AuctionRecommendationsSync(request.RequestXML);
+        oReponseData = new AuctionRecommendationsResponseData(responseXML);
+      }
+      catch (Exception ex)
+      {
+        oReponseData = new AuctionRecommendationsResponseData(oRequestData, ex);
+      }
+      finally
+      {
+        if (service != null)
+        {
+          service.Dispose();
+        }
+      }
+
+      return oReponseData;
+    }
+  }
+}

9115f8c6b02a3fd1b280cd70208ac6ec86f82e49
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-02 17:23:48 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C231709

---
+++ Atlantis.Framework.ManagerGetProductDetail/Atlantis.Framework.ManagerGetProductDetail.Interface/ManagerGetProductDetailRequestData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.ManagerGetProductDetail.Interface
+{
+  public class ManagerGetProductDetailRequestData: RequestData
+  {
+    public ManagerGetProductDetailRequestData(
+      string shopperId,
+      string sourceURL,
+      string orderId,
+      string pathway,
+      int pageCount,
+      decimal pfid,
+      int privateLabelId,
+      int adminFlag,
+      int mgrUserId) 
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      Pfid = pfid;
+      PrivateLabelId = privateLabelId;
+      AdminFlag = adminFlag;
+      ManagerUserId = mgrUserId;
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public decimal Pfid { get; private set; }
+    public int PrivateLabelId { get; private set; }
+    public int AdminFlag { get; private set; }
+    public int ManagerUserId { get; private set; }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

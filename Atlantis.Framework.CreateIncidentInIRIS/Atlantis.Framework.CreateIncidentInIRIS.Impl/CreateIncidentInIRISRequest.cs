54d9969081f132be511db5b87c82e8d322380100
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-03 04:20:24 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C231891

---
+++ Atlantis.Framework.CreateIncidentInIRIS/Atlantis.Framework.CreateIncidentInIRIS.Impl/CreateIncidentInIRISRequest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Atlantis.Framework.CreateIncidentInIRIS.Interface;
+using Atlantis.Framework.CreateIncidentInIRIS.Impl.IrisWS;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.CreateIncidentInIRIS.Impl
+{
+  public class CreateIncidentInIRISRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      CreateIncidentInIRISResponseData oResponseData = null;
+
+      IrisWS.IrisWebService service = null;
+      try
+      {
+        CreateIncidentInIRISRequestData request = (CreateIncidentInIRISRequestData)oRequestData;
+        service = new IrisWebService();
+        service.Url = ((WsConfigElement)oConfig).WSURL;
+        service.Timeout = (int)Math.Truncate(request.RequestTimeout.TotalMilliseconds);
+
+        long irisResult = service.CreateIncidentInIRIS(request.SubscriberId,
+                                                       request.Subject,
+                                                       request.Note,
+                                                       request.CustomerEmailAddress,
+                                                       request.OriginalIPAddress,
+                                                       request.GroupId,
+                                                       request.ServiceId,
+                                                       request.PrivateLabelId,
+                                                       request.ShopperId,
+                                                       request.CreatedBy);
+
+        oResponseData = new CreateIncidentInIRISResponseData(irisResult);
+
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new CreateIncidentInIRISResponseData(oRequestData, ex);
+      }
+      finally
+      {
+        if (service != null)
+        {
+          service.Dispose();
+        }
+      }
+
+      return oResponseData;
+    }
+
+    #endregion
+    
+  }
+}

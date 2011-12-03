aac1fe6f697eefd23cbfa000bca91619a6e93719
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-03 00:07:52 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C231870

---
+++ Atlantis.Framework.CostcoGetMemberInfo/Atlantis.Framework.CostcoGetMemberInfo.Interface/CostcoGetMemberInfoRequestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.CostcoGetMemberInfo.Interface
+{
+  public class CostcoGetMemberInfoRequestData : RequestData
+  {
+    public CostcoGetMemberInfoRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount) 
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      if (string.IsNullOrEmpty(shopperId))
+      {
+        throw new ArgumentException("Must provide a value.", "shopperId");
+      }
+      RequestTimeout = new TimeSpan(0, 0, 10);
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

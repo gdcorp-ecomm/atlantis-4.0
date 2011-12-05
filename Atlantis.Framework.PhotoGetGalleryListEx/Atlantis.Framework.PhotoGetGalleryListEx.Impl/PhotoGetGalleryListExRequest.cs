09930f61e9f66e8261d0892e1c643da8bb461608
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 23:21:48 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232241

---
+++ Atlantis.Framework.PhotoGetGalleryListEx/Atlantis.Framework.PhotoGetGalleryListEx.Impl/PhotoGetGalleryListExRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.PhotoGetGalleryListEx.Impl.PhotoServiceWS;
+using Atlantis.Framework.PhotoGetGalleryListEx.Interface;
+
+namespace Atlantis.Framework.PhotoGetGalleryListEx.Impl
+{
+  public class PhotoGetGalleryListExRequest : IRequest
+  {
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      PhotoGetGalleryListExResponseData responseData = null;
+      PhotoServiceWS.PhotoServiceResponse outResponse = new PhotoServiceWS.PhotoServiceResponse();
+      PhotoServiceWS.Gallery[] returnedGallery = null;
+      List<GalleryItem> responseGallery = new List<GalleryItem>();
+
+      PhotoServiceWS.PhotoService serviceWS = null;
+      try
+      {
+        PhotoGetGalleryListExRequestData requestData = (PhotoGetGalleryListExRequestData)oRequestData;
+        serviceWS = new PhotoServiceWS.PhotoService();
+        serviceWS.Url = ((WsConfigElement)oConfig).WSURL;
+        serviceWS.Timeout = (int)Math.Truncate(requestData.RequestTimeout.TotalMilliseconds);
+        returnedGallery = serviceWS.GetGalleryListEx(requestData.Domain, out outResponse);
+
+        if ((returnedGallery.Length > 0) && (outResponse.ResponseCode == 0))
+        {
+          foreach (Gallery gallery in returnedGallery)
+          {
+            GalleryItem data = new GalleryItem(gallery.GalleryId, gallery.Title, gallery.Notes, gallery.TotalPhotos);
+            responseGallery.Add(data);
+          }
+        }
+
+        responseData = new PhotoGetGalleryListExResponseData(responseGallery, outResponse.ResponseCode);
+
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new PhotoGetGalleryListExResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new PhotoGetGalleryListExResponseData(responseGallery, oRequestData, ex);
+      }
+      finally
+      {
+        if (serviceWS != null)
+        {
+          serviceWS.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+  }
+}
---
+++ Atlantis.Framework.PhotoGetGalleryListEx/Atlantis.Framework.PhotoGetGalleryListEx.Interface/PhotoGetGalleryListExRequestData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PhotoGetGalleryListEx.Interface
+{
+  public class PhotoGetGalleryListExRequestData : RequestData
+  {
+    private string _domain;
+
+    public PhotoGetGalleryListExRequestData(
+      string shopperID, string sourceURL, string orderID, string pathway,
+      int pageCount, string domain)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      _domain = domain;
+      RequestTimeout = new TimeSpan(0, 0, 4);
+    }
+
+    public string Domain
+    {
+      get { return _domain; }
+      set { _domain = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(_domain);
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", string.Empty);
+    }
+  }
+}

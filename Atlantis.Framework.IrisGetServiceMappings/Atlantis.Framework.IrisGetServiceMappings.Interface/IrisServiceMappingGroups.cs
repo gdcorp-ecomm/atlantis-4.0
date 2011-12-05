0c2146d23018b8ee87f350de008bf6a53fdd63f9
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 21:59:26 +0000

    4.0 Conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232189

---
+++ Atlantis.Framework.IrisGetServiceMappings/Atlantis.Framework.IrisGetServiceMappings.Interface/IrisServiceMappingGroups.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Atlantis.Framework.IrisGetServiceMappings.Interface
+{
+  public class IrisServiceMappingGroups
+  {
+    public static readonly string groupGlobal = "Global";
+    public static readonly string groupOfferings = "Offerings";
+    public static readonly string groupInternal = "Internal";
+    public static readonly string groupAssisted = "Assisted";
+  }
+}

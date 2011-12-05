b3a8a721ad714baba4eb855febd03304575aa05f
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 18:18:52 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232071

---
+++ Atlantis.Framework.DBSCreateTdnamAuction/Atlantis.Framework.DBSCreateTdnamAuction.Impl/DBSCreateTdnamAuctionRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Atlantis.Framework.DBSCreateTdnamAuction.Impl.DbsWebService;
+using Atlantis.Framework.DBSCreateTdnamAuction.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DBSCreateTdnamAuction.Impl
+{
+  public class DBSCreateTdnamAuctionRequest : IRequest
+  {
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DBSCreateTdnamAuctionResponseData responseData = null;
+      string resultXML = string.Empty;
+
+      DbsWebService.DomainServices DbsWS = null;
+      try
+      {
+        DBSCreateTdnamAuctionRequestData requestData = (DBSCreateTdnamAuctionRequestData)oRequestData;
+        DbsWS = new DomainServices();
+        DbsWS.Url = ((WsConfigElement)oConfig).WSURL;
+        DbsWS.Timeout = (int)Math.Truncate(requestData.RequestTimeout.TotalMilliseconds);
+
+        resultXML = DbsWS.DomainBuy_CreateTdnamAuction(requestData.RequestXml);
+        responseData = new DBSCreateTdnamAuctionResponseData(resultXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DBSCreateTdnamAuctionResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new DBSCreateTdnamAuctionResponseData(responseData.ToString(), oRequestData, ex);
+      }
+      finally
+      {
+        if ( DbsWS != null )
+        {
+          DbsWS.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+  }
+}

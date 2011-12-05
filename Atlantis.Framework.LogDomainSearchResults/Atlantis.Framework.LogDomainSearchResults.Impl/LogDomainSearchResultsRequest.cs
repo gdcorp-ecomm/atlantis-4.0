bee827004d859cb50d043f57efbd8bd0f95d792e
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 22:06:19 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232194

---
+++ Atlantis.Framework.LogDomainSearchResults/Atlantis.Framework.LogDomainSearchResults.Impl/LogDomainSearchResultsRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.LogDomainSearchResults.Impl.LogDomainSearchResultsWS;
+using Atlantis.Framework.LogDomainSearchResults.Interface;
+
+namespace Atlantis.Framework.LogDomainSearchResults.Impl
+{
+  public class LogDomainSearchResultsRequest : IRequest
+  {
+
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      LogDomainSearchResultsResponseData oResponseData = null;
+
+      LogDomainSearchResultsWS.Service service = null;
+      try
+      {
+        LogDomainSearchResultsRequestData request = (LogDomainSearchResultsRequestData)oRequestData;
+        service = new Service();
+        service.Url = ((WsConfigElement)oConfig).WSURL;
+        service.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+
+        service.LogDomainSearchResults(request.ToXML());
+
+        oResponseData = new LogDomainSearchResultsResponseData();
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new LogDomainSearchResultsResponseData(oRequestData, ex);
+      }
+      finally
+      {
+        if (service != null)
+        {
+          service.Dispose();
+        }
+      }
+
+      return oResponseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.LogDomainSearchResults/Atlantis.Framework.LogDomainSearchResults.Interface/DomainAvailability.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Atlantis.Framework.LogDomainSearchResults.Interface
+{
+  public static class DomainAvailability
+  {
+    public const int NotAvailable = 0;
+    public const int Available = 1;
+    public const int Backorder = 2;
+    public const int Unknown = 99;
+  }
+}

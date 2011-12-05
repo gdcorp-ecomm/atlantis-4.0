5c20c176b8d5da8f7b43c38faadd89b80e69060f
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 18:17:51 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232068

---
+++ Atlantis.Framework.DBSGetDomainShopper/Atlantis.Framework.DBSGetDomainShopper.Impl/DBSGetDomainShopperRequest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Atlantis.Framework.DBSGetDomainShopper.Impl.DbsWebService;
+using Atlantis.Framework.DBSGetDomainShopper.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DBSGetDomainShopper.Impl
+{
+  public class DBSGetDomainShopperRequest : IRequest
+  {
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DBSGetDomainShopperResponseData responseData = null;
+      string resultXML = string.Empty;
+
+      DbsWebService.DomainServices DbsWS = null;
+      try
+      {
+        DBSGetDomainShopperRequestData requestData = (DBSGetDomainShopperRequestData)oRequestData;
+        DbsWS = new DomainServices();
+        DbsWS.Url = ((WsConfigElement)oConfig).WSURL;
+        DbsWS.Timeout = (int)Math.Truncate(requestData.RequestTimeout.TotalMilliseconds);
+
+        resultXML = DbsWS.GetDomainShopperByLegalResourceId(requestData.ResourceId);
+        responseData = new DBSGetDomainShopperResponseData(resultXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DBSGetDomainShopperResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new DBSGetDomainShopperResponseData(responseData.ToString(), oRequestData, ex);
+      }
+      finally
+      {
+        if (DbsWS != null)
+        {
+          DbsWS.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+  }
+}
---
+++ Atlantis.Framework.DBSGetDomainShopper/Atlantis.Framework.DBSGetDomainShopper.Interface/DBSGetDomainShopperRequestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DBSGetDomainShopper.Interface
+{
+  public class DBSGetDomainShopperRequestData : RequestData
+  {
+    private int _resourceId;
+    
+    public DBSGetDomainShopperRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount, 
+    int resourceId)
+      : base (shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      _resourceId = resourceId;
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    public int ResourceId
+    {
+      get { return _resourceId; }
+      set { _resourceId = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("DBSGetDomainShopper is not a cacheable request.");
+    }
+
+  }
+}

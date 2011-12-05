b0eb8a044c98e55a43502a6481e888e5167b0291
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-05 20:54:08 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232132

---
+++ Atlantis.Framework.EcommGiftCardBalance/Atlantis.Framework.EcommGiftCardBalance.Impl/EcommGiftCardBalanceRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Atlantis.Framework.EcommGiftCardBalance.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommGiftCardBalance.Impl
+{
+  public class EcommGiftCardBalanceRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      EcommGiftCardBalanceRequestData getBalance = (EcommGiftCardBalanceRequestData)oRequestData;
+      EcommGiftCardBalanceResponseData oResponseData = null;
+      WscGiftCard.wscGiftCardService oSvc = null;
+      try
+      {
+        oSvc = new WscGiftCard.wscGiftCardService();
+        oSvc.Url = ((WsConfigElement)oConfig).WSURL;
+        oSvc.Timeout = (int)Math.Truncate(getBalance.RequestTimeout.TotalMilliseconds);
+        int result = oSvc.GetGiftCardBalance(getBalance.AccountNumber, getBalance.OrderID);
+        oResponseData = new EcommGiftCardBalanceResponseData(result);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        oResponseData = new EcommGiftCardBalanceResponseData(oRequestData, exAtlantis);
+      }
+      finally
+      {
+        if (oSvc != null)
+        {
+          oSvc.Dispose();
+        }
+      }
+      return oResponseData;
+    }
+
+    #endregion
+  }
+}

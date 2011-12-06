50e1aab309730a7cd187d62ef7c96feaa9d3d71e
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 18:08:45 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232423

---
+++ Atlantis.Framework.RegRegistryPartnersData/Atlantis.Framework.RegRegistryPartnersData.INterface/RegRegistryPartnersDataRequestData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RegRegistryPartnersData.Interface
+{
+  public class RegRegistryPartnersDataRequestData : RequestData
+  {
+    public RegRegistryPartnersDataRequestData(
+      string shopperID,
+      string sourceURL,
+      string orderID,
+      string pathway,
+      int pageCount)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    private string _bronsonSourceId = "1";
+    public string BronsonSourceId
+    {
+      get
+      {
+        return _bronsonSourceId;
+      }
+      set
+      {
+        _bronsonSourceId = value;
+      }
+    }
+
+    #region RequestData Members
+
+    public override string GetCacheMD5()
+    {
+      return string.Empty;
+    }
+
+    public override string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+
+      xtwRequest.WriteStartElement("REQUEST");
+      xtwRequest.WriteStartElement("BRONSONSOURCE");
+      xtwRequest.WriteAttributeString("id", BronsonSourceId);
+      xtwRequest.WriteEndElement();
+      xtwRequest.WriteEndElement();
+
+      return sbRequest.ToString();
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.RegRegistryPartnersData/Atlantis.Framework.RegRegistryPartnersData.Impl/RegRegistryPartnersDataRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegRegistryPartnersData.Interface;
+
+namespace Atlantis.Framework.RegRegistryPartnersData.Impl
+{
+  public class RegRegistryPartnersDataRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData responseData = null;
+      string responseXML = string.Empty;
+
+      RegistryPartnersData.RegistryPartnersData registryPartnersDataWebService = null;
+      try
+      {
+        WsConfigElement wsConfigElement = (WsConfigElement)oConfig;
+        registryPartnersDataWebService = new RegistryPartnersData.RegistryPartnersData();
+        registryPartnersDataWebService.Url = wsConfigElement.WSURL;
+        registryPartnersDataWebService.Timeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalMilliseconds);
+
+        string inputXml = oRequestData.ToXML();
+        responseXML = registryPartnersDataWebService.GetCurrentTldSnapshotList(inputXml);
+        responseData = new RegRegistryPartnersDataResponseData(responseXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new RegRegistryPartnersDataResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new RegRegistryPartnersDataResponseData(responseXML, oRequestData, ex);
+      }
+      finally
+      {
+        if (registryPartnersDataWebService != null)
+        {
+          registryPartnersDataWebService.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+
+    #endregion
+  }
+}

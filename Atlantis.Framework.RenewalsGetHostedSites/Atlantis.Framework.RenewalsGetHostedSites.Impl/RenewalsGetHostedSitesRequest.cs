c7278860471c2cc2c1b46b839f72e8951c2f9201
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 21:52:35 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232532

---
+++ Atlantis.Framework.RenewalsGetHostedSites/Atlantis.Framework.RenewalsGetHostedSites.Impl/RenewalsGetHostedSitesRequest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+using Atlantis.Framework.RenewalsGetHostedSites.Interface;
+
+namespace Atlantis.Framework.RenewalsGetHostedSites.Impl
+{
+  public class RenewalsGetHostedSitesRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData oResponseData = null;
+      DataSet ds = null;
+
+      try
+      {
+        string connectionString = NetConnect.LookupConnectInfo(oConfig);
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+          using (SqlCommand command = new SqlCommand(_PROCNAME, connection))
+          {
+            RenewalsGetHostedSitesRequestData request = (RenewalsGetHostedSitesRequestData)oRequestData;
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add(new SqlParameter(_SHOPPERID, request.ShopperID));
+            command.Parameters.Add(new SqlParameter(_PAGENO, request.PageNumber));
+            command.Parameters.Add(new SqlParameter(_ROWSPERPAGE, request.RowsPerPage));
+            command.Parameters.Add(new SqlParameter(_SORTCOL, request.SortColumn));
+            command.Parameters.Add(new SqlParameter(_SORTDIR, request.SortDirection));
+            command.CommandTimeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalSeconds);
+
+            connection.Open();
+            ds = new DataSet(Guid.NewGuid().ToString());
+            SqlDataAdapter adp = new SqlDataAdapter(command);
+            adp.Fill(ds);
+          }
+        }
+
+        oResponseData = new RenewalsGetHostedSitesResponseData(ds);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        oResponseData = new RenewalsGetHostedSitesResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        oResponseData = new RenewalsGetHostedSitesResponseData(ds, oRequestData, ex);
+      }
+
+      return oResponseData;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private const string _PROCNAME = "MyRenewalsGetHostedSites_sp";
+    private const string _SHOPPERID = "s_shopper_id";
+    private const string _PAGENO = "pageno";
+    private const string _ROWSPERPAGE = "rowsperpage";
+    private const string _SORTCOL = "sortcol";
+    private const string _SORTDIR = "sortdir";
+
+    #endregion
+  }
+}

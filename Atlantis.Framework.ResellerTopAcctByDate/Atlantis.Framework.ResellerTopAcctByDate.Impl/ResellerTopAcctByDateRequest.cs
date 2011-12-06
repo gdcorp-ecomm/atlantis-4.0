619078ce3d98ccdfbdbe02a557638bc38d3fd7c4
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 21:09:33 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232505

---
+++ Atlantis.Framework.ResellerTopAcctByDate/Atlantis.Framework.ResellerTopAcctByDate.Impl/ResellerTopAcctByDateRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+using Atlantis.Framework.ResellerTopAcctByDate.Interface;
+
+namespace Atlantis.Framework.ResellerTopAcctByDate.Impl
+{
+  public class ResellerTopAcctByDateRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DataTable resultTable = null;
+      ResellerTopAcctByDateResponseData responseData = null;
+
+      try
+      {
+        ResellerTopAcctByDateRequestData requestData = (ResellerTopAcctByDateRequestData)oRequestData;
+
+        string connectionString = NetConnect.LookupConnectInfo(oConfig);
+        string procName = "dbo.rex_ResellerTopAcctByDate_sp";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+          using (SqlCommand command = new SqlCommand(procName, connection))
+          {
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add(new SqlParameter("@StartDate", requestData.StartDate));
+            command.Parameters.Add(new SqlParameter("@EndDate", requestData.EndDate));
+            command.Parameters.Add(new SqlParameter("@NumRows", requestData.NumRows));
+            command.CommandTimeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalSeconds);
+
+            DataSet dataSet = new DataSet();
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+              adapter.Fill(dataSet);
+            }
+
+            if (dataSet.Tables.Count > 1)
+            {
+              resultTable = dataSet.Tables[1];
+            }
+          }
+        }
+        responseData = new ResellerTopAcctByDateResponseData(resultTable);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new ResellerTopAcctByDateResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new ResellerTopAcctByDateResponseData(resultTable, oRequestData, ex);
+      }
+      return responseData;
+    }
+  }
+}

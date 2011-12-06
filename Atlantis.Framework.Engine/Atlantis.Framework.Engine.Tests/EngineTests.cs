17876821d973d8ae20506351c17089d7195f22e0
Michael Micco <mmicco@godaddy.com>
2011-12-06 22:19:19 +0000

    Engine Firewall Test Diagnostics App
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232545

--- Atlantis.Framework.Engine/Atlantis.Framework.Engine.Tests/EngineTests.cs
+++ Atlantis.Framework.Engine/Atlantis.Framework.Engine.Tests/EngineTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Atlantis.Framework.Engine.Tests.MockTriplet;
 using Atlantis.Framework.Interface;
+using System.Threading.Tasks;
 
 namespace Atlantis.Framework.Engine.Tests
 {
@@ -85,6 +86,23 @@ namespace Atlantis.Framework.Engine.Tests
     {
       AtlantisException ex = new AtlantisException("EngineTests.LogException", "911", "Test log message only.", string.Empty, null, null);
       Engine.LogAtlantisException(ex);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void RecentExceptions()
+    {
+      Engine.MaxRecentExceptions = 3;
+
+      Parallel.For(1, 6, i =>
+        {
+          AtlantisException ex = new AtlantisException("EngineTests.RecentException" + i.ToString(), "911", "Test log message only. #" + i.ToString(), string.Empty, null, null);
+          Engine.LogAtlantisException(ex);
+        }
+        );
+
+      List<AtlantisException> recents = Engine.GetRecentExceptions();
+      Assert.AreEqual(3, recents.Count);
     }
   }
 }

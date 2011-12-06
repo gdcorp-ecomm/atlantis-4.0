1dfc521b42072c3e5c603ef182e4171b05c3dc91
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 00:20:08 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232271

---
+++ Atlantis.Framework.RegCAValidateProfileData/Atlantis.Framework.RegCAValidateProfileData.Impl/RegCAValidateProfileDataRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegCAValidateProfileData.Interface;
+
+namespace Atlantis.Framework.RegCAValidateProfileData.Impl
+{
+  public class RegCAValidateProfileDataRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData responseData = null;
+      string responseXML = string.Empty;
+
+      RegCaDataWebService.RegCaDataWebSvc regCaDataWebService = null;
+      try
+      {
+        RegCAValidateProfileDataRequestData regCAValidateProfileDataRequestData = (RegCAValidateProfileDataRequestData)oRequestData;
+
+        regCaDataWebService = new RegCaDataWebService.RegCaDataWebSvc();
+
+        WsConfigElement wsConfigElement = (WsConfigElement)oConfig;
+        regCaDataWebService.Url = wsConfigElement.WSURL;
+        regCaDataWebService.Timeout = (int)regCAValidateProfileDataRequestData.RequestTimeout.TotalMilliseconds;
+
+        string inputXml = oRequestData.ToXML();
+        responseXML = regCaDataWebService.RegistrantCompare(inputXml);
+        responseData = new RegCAValidateProfileDataResponseData(responseXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new RegCAValidateProfileDataResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new RegCAValidateProfileDataResponseData(responseXML, oRequestData, ex);
+      }
+      finally
+      {
+        if (regCaDataWebService != null)
+        {
+          regCaDataWebService.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.RegCAValidateProfileData/Atlantis.Framework.RegCAValidateProfileData.Interface/RegCAValidateProfileDataRequestData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RegCAValidateProfileData.Interface
+{
+  public class RegCAValidateProfileDataRequestData : RequestData
+  {
+    private string _ciraProfileId;
+    private string _ciraRegistrantName;
+
+    public RegCAValidateProfileDataRequestData(
+      string shopperID,
+      string sourceURL,
+      string orderID,
+      string pathway,
+      int pageCount)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    public RegCAValidateProfileDataRequestData(
+      string shopperID,
+      string sourceURL,
+      string orderID,
+      string pathway,
+      int pageCount,
+      string ciraRegistrantName,
+      string ciraProfileId)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      _ciraRegistrantName = ciraRegistrantName;
+      _ciraProfileId = ciraProfileId;
+
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    #region RequestData Members
+
+    public override string GetCacheMD5()
+    {
+      return string.Empty;
+    }
+
+    public override string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+
+      xtwRequest.WriteStartElement("profile");
+      xtwRequest.WriteAttributeString("caprofileid", _ciraProfileId);
+      xtwRequest.WriteAttributeString("caregistrantname", _ciraRegistrantName);
+      xtwRequest.WriteEndElement();
+
+      return sbRequest.ToString();
+    }
+
+    #endregion
+  }
+}

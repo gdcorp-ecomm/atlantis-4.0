fc7426d6d486b34ab11f820951128095d024e4a0
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 22:22:43 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232547

---
+++ Atlantis.Framework.DBSSellerNotInterested/Atlantis.Framework.DBSSellerNotInterested.Impl/DBSSellerNotInterestedRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Atlantis.Framework.DBSSellerNotInterested.Impl.DbsWebService;
+using Atlantis.Framework.DBSSellerNotInterested.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DBSSellerNotInterested.Impl
+{
+  public class DBSSellerNotInterestedRequest : IRequest
+  {
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      DBSSellerNotInterestedResponseData responseData = null;
+      string resultXML = string.Empty;
+
+      DbsWebService.DomainServices DbsWS = null;
+      try
+      {
+        DBSSellerNotInterestedRequestData requestData = (DBSSellerNotInterestedRequestData)oRequestData;
+        DbsWS = new DomainServices();
+        DbsWS.Url = ((WsConfigElement)oConfig).WSURL;
+        DbsWS.Timeout = (int)Math.Truncate(requestData.RequestTimeout.TotalMilliseconds);
+
+        resultXML = DbsWS.DomainBuy_SellerNotInterestedByResourceId(requestData.ResourceId);
+        responseData = new DBSSellerNotInterestedResponseData(resultXML);
+
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new DBSSellerNotInterestedResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new DBSSellerNotInterestedResponseData(responseData.ToString(), oRequestData, ex);
+      }
+      finally
+      {
+        if (DbsWS != null)
+        {
+          DbsWS.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+  }
+}
---
+++ Atlantis.Framework.DBSSellerNotInterested/Atlantis.Framework.DBSSellerNotInterested.Interface/DBSSellerNotInterestedRequestData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.DBSSellerNotInterested.Interface
+{
+  public class DBSSellerNotInterestedRequestData : RequestData
+  {
+    private int _resourceId;
+
+    public DBSSellerNotInterestedRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount,
+    int resourceId)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      _resourceId = resourceId;
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    public int ResourceId
+    {
+      get { return _resourceId; }
+      set { _resourceId = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new Exception("DBSSellerNotInterested is not a cacheable request.");
+    }
+
+  }
+}

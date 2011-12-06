0f546e5f65678969056bc3dd1984cf458905e5e0
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 20:06:51 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232466

---
+++ Atlantis.Framework.RegGetCAAgreement/Atlantis.Framework.RegGetCAAgreement.Impl/RegGetCAAgreementRequest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegGetCAAgreement.Interface;
+
+namespace Atlantis.Framework.RegGetCAAgreement.Impl
+{
+  public class RegGetCAAgreementRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData responseData = null;
+      string responseXML = string.Empty;
+
+      RegCaDataWebService.RegCaDataWebSvc regCaDataWebService = null;
+      try
+      {
+        WsConfigElement wsConfigElement = (WsConfigElement)oConfig;
+        regCaDataWebService = new RegCaDataWebService.RegCaDataWebSvc();
+        regCaDataWebService.Url = wsConfigElement.WSURL;
+        regCaDataWebService.Timeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalMilliseconds);
+
+        responseXML = regCaDataWebService.GetAgreement();
+        if (!string.IsNullOrEmpty(responseXML))
+        {
+          responseData = new RegGetCAAgreementResponseData(responseXML);
+        }
+        else
+        {
+          throw new Exception("RegGetCAAgreementRequest returned empty string.");
+        }
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new RegGetCAAgreementResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new RegGetCAAgreementResponseData(responseXML, oRequestData, ex);
+      }
+      finally
+      {
+        if (regCaDataWebService != null)
+        {
+          regCaDataWebService.Dispose();
+        }
+      }
+      return responseData;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.RegGetCAAgreement/Atlantis.Framework.RegGetCAAgreement.Interface/RegGetCAAgreementRequestData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RegGetCAAgreement.Interface
+{
+  public class RegGetCAAgreementRequestData : RequestData
+  {
+    public RegGetCAAgreementRequestData(
+      string shopperID,
+      string sourceURL,
+      string orderID,
+      string pathway,
+      int pageCount)
+      : base(shopperID, sourceURL, orderID, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(4);
+    }
+
+    #region RequestData Members
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes("RegGetCAAgreementRequestData");
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", "");
+    }
+
+    #endregion
+  }
+}

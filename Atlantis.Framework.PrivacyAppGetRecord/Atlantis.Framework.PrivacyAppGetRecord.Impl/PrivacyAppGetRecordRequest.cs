cb0e7cea9d97841203ea359fa4116487911e01b8
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 00:23:23 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232274

---
+++ Atlantis.Framework.PrivacyAppGetRecord/Atlantis.Framework.PrivacyAppGetRecord.Impl/PrivacyAppGetRecordRequest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.PrivacyAppGetRecord.Impl.privacyWS;
+using Atlantis.Framework.PrivacyAppGetRecord.Interface;
+
+namespace Atlantis.Framework.PrivacyAppGetRecord.Impl
+{
+  public class PrivacyAppGetRecordRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData result = null;
+
+      string responseXml = string.Empty;
+      wscgdPrivacyAppService service = null;
+
+      try
+      {
+        PrivacyAppGetRecordRequestData request = (PrivacyAppGetRecordRequestData)oRequestData;
+
+        service = new wscgdPrivacyAppService();
+        service.Url = ((WsConfigElement)oConfig).WSURL;
+        service.Timeout = (int)request.RequestTimeout.TotalMilliseconds;
+        service.GetRecord(request.HashKey, request.ApplicationId, out responseXml);
+
+        result = new PrivacyAppGetRecordResponseData(responseXml);
+
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        result = new PrivacyAppGetRecordResponseData(responseXml, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        result = new PrivacyAppGetRecordResponseData(responseXml, oRequestData, ex);
+      }
+      finally
+      {
+        if (service != null)
+        {
+          service.Dispose();
+        }
+      }
+
+      return result;
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.PrivacyAppGetRecord/Atlantis.Framework.PrivacyAppGetRecord.Interface/PrivacyAppGetRecordRequestData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.PrivacyAppGetRecord.Interface
+{
+  public class PrivacyAppGetRecordRequestData : RequestData
+  {
+    private int _appId = 0;
+    private string _hashKey = string.Empty;
+
+    public PrivacyAppGetRecordRequestData(
+      string shopperId,
+      string sourceURL,
+      string orderId,
+      string pathway,
+      int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public string HashKey
+    {
+      get { return _hashKey; }
+      set { _hashKey = value; }
+    }
+
+    public int ApplicationId
+    {
+      get { return _appId; }
+      set { _appId = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("PrivacyAppGetRecord is not a cacheable request.");
+    }
+
+    public override string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+      xtwRequest.WriteStartElement("request");
+
+
+      xtwRequest.WriteEndElement();
+      return sbRequest.ToString();
+    }
+
+  }
+
+}

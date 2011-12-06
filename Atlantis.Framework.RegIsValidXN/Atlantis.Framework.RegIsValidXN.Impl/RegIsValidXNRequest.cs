33dd96b30197fbee743a5a697b1c07df6595670d
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 17:26:47 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232388

---
+++ Atlantis.Framework.RegIsValidXN/Atlantis.Framework.RegIsValidXN.Impl/RegIsValidXNRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegIsValidXN.Impl.RegIDNCheckerWS;
+using Atlantis.Framework.RegIsValidXN.Interface;
+
+namespace Atlantis.Framework.RegIsValidXN.Impl
+{
+  public class RegIsValidXNRequest : IRequest
+  {
+
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData result = null;
+      string responseXml = string.Empty;
+
+      RegIDNCheckerWebSvc xnRequest = null;
+      try
+      {
+        string serviceUrl = ((WsConfigElement)oConfig).WSURL;
+        xnRequest = new RegIDNCheckerWebSvc();
+        xnRequest.Url = serviceUrl;
+        xnRequest.Timeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalMilliseconds);
+        responseXml = xnRequest.IsValidXN(oRequestData.ToXML());
+        result = new RegIsValidXNResponseData(responseXml);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        result = new RegIsValidXNResponseData(responseXml, exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        result = new RegIsValidXNResponseData(responseXml, oRequestData, ex);
+      }
+      finally
+      {
+        if (xnRequest != null)
+        {
+          xnRequest.Dispose();
+        }
+      }
+
+      return result;
+    }
+
+    #endregion
+
+  }
+}
---
+++ Atlantis.Framework.RegIsValidXN/Atlantis.Framework.RegIsValidXN.Interface/RegIsValidXNRequestData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.RegIsValidXN.Interface
+{
+  public class RegIsValidXNRequestData : RequestData
+  {
+    private string _domainName = string.Empty;
+    private int _tldId = -1;
+    private string _scriptTag = string.Empty;
+
+    public RegIsValidXNRequestData(
+      string shopperId, string sourceUrl, string orderId, string pathway, int pageCount,
+      string domainName, int tldId, string scriptTag)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      _domainName = domainName;
+      _tldId = tldId;
+      _scriptTag = scriptTag;
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public string DomainName
+    {
+      get { return _domainName; }
+      set { _domainName = value; }
+    }
+
+    public int TldId
+    {
+      get { return _tldId; }
+      set { _tldId = value; }
+    }
+
+    public string ScriptTag
+    {
+      get { return _scriptTag; }
+      set { _scriptTag = value; }
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("RegIsValidXN is not a cacheable request.");
+    }
+
+    public override string ToXML()
+    {
+      StringBuilder sbRequest = new StringBuilder();
+      XmlTextWriter xtwRequest = new XmlTextWriter(new StringWriter(sbRequest));
+
+      xtwRequest.WriteStartElement("request");
+      xtwRequest.WriteAttributeString("appname", Environment.MachineName);
+      xtwRequest.WriteAttributeString("domainname", _domainName);
+      xtwRequest.WriteAttributeString("tldid", _tldId.ToString());
+      xtwRequest.WriteAttributeString("langtag", _scriptTag);
+
+      xtwRequest.WriteEndElement();
+      return sbRequest.ToString();
+    }
+
+  }
+}

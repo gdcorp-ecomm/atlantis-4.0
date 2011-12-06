997ce890102369a1258c3b85a6295e93456c1c2b
Jeffrey M. Burke <jburke@godaddy.com>
2011-12-06 17:08:21 +0000

    4.0 conversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C232378

---
+++ Atlantis.Framework.RegCheckCaContacts/Atlantis.Framework.RegCheckCaContacts.Impl/RegCheckCaContactsRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.RegCheckCaContacts.Interface;
+
+namespace Atlantis.Framework.RegCheckCaContacts.Impl
+{
+  public class RegCheckCaContactsRequest : IRequest
+  {
+    #region IRequest Members
+
+    public IResponseData RequestHandler(RequestData oRequestData, ConfigElement oConfig)
+    {
+      IResponseData responseData = null;
+      string responseXML = string.Empty;
+
+      RegCaDataWebSvc.RegCaDataWebSvc regCaDataWebService = null;
+      try
+      {
+        WsConfigElement wsConfigElement = (WsConfigElement)oConfig;
+        regCaDataWebService = new RegCaDataWebSvc.RegCaDataWebSvc();
+        regCaDataWebService.Url = wsConfigElement.WSURL;
+        regCaDataWebService.Timeout = (int)Math.Truncate(oRequestData.RequestTimeout.TotalMilliseconds);
+        string inputXml = oRequestData.ToXML();
+        responseXML = regCaDataWebService.ValidateDomainAndContactsCA(inputXml);
+        responseData = new RegCheckCaContactsResponseData(responseXML);
+      }
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new RegCheckCaContactsResponseData(exAtlantis);
+      }
+      catch (Exception ex)
+      {
+        responseData = new RegCheckCaContactsResponseData(responseXML, oRequestData, ex);
+      }
+      finally
+      {
+        if (regCaDataWebService != null)
+        {
+          regCaDataWebService.Dispose();
+        }
+      }
+
+      return responseData;
+    }
+
+    #endregion
+  }
+}

abbbecaaed038ca17d971857be02976a5b5da67c
tbird <tbird@jomax.tfs.local>
2011-12-13 21:31:53 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C234223

---
+++ Atlantis.Framework.EcomOrderLevelPromo/Atlantis.Framework.EcomOrderLevelPromo/Atlantis.Framework.EcomOrderLevelPromo.Interface/EcomOrderLevelPromoRequestData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcomOrderLevelPromo.Interface
+{
+  public class EcomOrderLevelPromoRequestData : RequestData
+  {
+
+    public EcomOrderLevelPromoRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderIo,
+                                  string pathway,
+                                  int pageCount,
+                                  int privateLabelID)
+      : base(shopperId, sourceUrl, orderIo, pathway, pageCount)
+    {
+      PrivateLabelID = privateLabelID;
+      RequestTimeout = TimeSpan.FromSeconds(5);
+    }
+
+    public int PrivateLabelID{get;set;}
+
+    public override string GetCacheMD5()
+    {
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+
+      oMD5.Initialize();
+
+      byte[] stringBytes
+
+      = System.Text.ASCIIEncoding.ASCII.GetBytes("EcomOrderLevelPromo:PrivateLabelID:" + PrivateLabelID);
+
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+
+      return sValue.Replace("-", "");
+    }
+
+
+  }
+}

ef559f30a853af48f516368e924ebb39bf7297a8
tbird <tbird@jomax.tfs.local>
2011-12-13 20:45:15 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C234185

---
+++ Atlantis.Framework.EcommDelayedPayment/Atlantis.Framework.EcommDelayedPayment/Atlantis.Framework.EcommDelayedPayment.Interface/EcommDelayedPaymentResponseData.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommDelayedPayment.Interface
+{
+  public class EcommDelayedPaymentResponseData : IResponseData
+  {
+    private AtlantisException _exception = null;
+    private string _resultXML = string.Empty;
+    private bool _success = false;
+
+    public string RedirectURL { get; set; }
+    public string ErrorOccured { get; set; }
+    public string InvoiceID { get; set; }
+
+    public bool IsSuccess
+    {
+      get
+      {
+        return _success;
+      }
+    }
+
+    public EcommDelayedPaymentResponseData(RequestData requestData,string redirectXML,string errorXML,string invoiceID)
+    {
+      try
+      {
+        ErrorOccured = string.Empty;
+        RedirectURL = string.Empty;
+        InvoiceID = string.Empty;
+        if (!string.IsNullOrEmpty(redirectXML))
+        {
+          XmlDocument redirectDoc = new XmlDocument();
+          redirectDoc.LoadXml(redirectXML);
+          XmlNode redirectURL = redirectDoc.SelectSingleNode("//Redirect/URL");
+          RedirectURL = redirectURL.InnerText;
+        }
+        else if (!string.IsNullOrEmpty(errorXML))
+        {
+          XmlDocument errorsDoc = new XmlDocument();
+          errorsDoc.LoadXml(errorXML);
+          XmlNode errorNode = errorsDoc.SelectSingleNode("//ERRORS/ERROR");
+          ErrorOccured = errorNode.InnerText;
+        }
+        if (!string.IsNullOrEmpty(invoiceID))
+        {
+          InvoiceID = invoiceID;
+        }
+        _success = true;
+      }
+      catch (System.Exception ex)
+      {
+        this._exception = new AtlantisException(requestData,
+                                    "EcommDelayedPaymentResponseData",
+                                    ex.Message,
+                                    requestData.ToXML());
+        _success = false;
+      }
+    }
+
+     public EcommDelayedPaymentResponseData(AtlantisException atlantisException)
+    {
+      this._exception = atlantisException;
+    }
+
+    public EcommDelayedPaymentResponseData(RequestData requestData, Exception exception)
+    {
+      this._exception = new AtlantisException(requestData,
+                                   "EcommDelayedPaymentResponseData",
+                                   exception.Message,
+                                   requestData.ToXML());
+    }
+
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      return _resultXML;
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    #endregion
+
+  }
+}
---
+++ Atlantis.Framework.EcommDelayedPayment/Atlantis.Framework.EcommDelayedPayment/Atlantis.Framework.EcommDelayedPayment.Interface/PaymentInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Atlantis.Framework.EcommDelayedPayment.Interface
+{
+  public class PaymentInfo
+  {
+    public string Remote_Addr { get; set; }
+    public string Pathway { get; set; }
+    public string PublisherHash { get; set; }
+    public string TranslationIP { get; set; }
+    public string TranslationLanguage { get; set; }
+    public string CurrencyDisplay { get; set; }
+
+    private void AddAttribute(string attributeName, string value, XmlTextWriter xmlWriter)
+    {
+      if (!string.IsNullOrEmpty(value))
+      {
+        xmlWriter.WriteAttributeString(attributeName, value);
+      }
+    }
+
+    public void ToXML(XmlTextWriter xtwRequest)
+    {
+      xtwRequest.WriteStartElement("PaymentTracking");
+      AddAttribute("remote_addr", Remote_Addr, xtwRequest);
+      AddAttribute("pathway", Pathway, xtwRequest);
+      AddAttribute("publisherHash", PublisherHash, xtwRequest);
+      AddAttribute("translationIP", TranslationIP, xtwRequest);
+      AddAttribute("translationLanguage", TranslationLanguage, xtwRequest);
+      AddAttribute("currencyDisplay", CurrencyDisplay, xtwRequest);
+      xtwRequest.WriteEndElement();
+    }
+  }
+}

ef1010e4ae679bcc601fd5329e71b294b8d6baa5
tbird <tbird@jomax.tfs.local>
2011-12-15 21:35:04 +0000

    Implement Using on service
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C234941

--- Atlantis.Framework.AddBasketShipping/Atlantis.Framework.AddBasketShipping.Impl/AddBasketShippingRequest.cs
+++ Atlantis.Framework.AddBasketShipping/Atlantis.Framework.AddBasketShipping.Impl/AddBasketShippingRequest.cs
@@ -16,10 +16,12 @@ namespace Atlantis.Framework.AddBasketShipping.Impl
       string sResponseXML = string.Empty;
       try
       {
-        WscgdBasket.WscgdBasketService oBasketWS = new WscgdBasket.WscgdBasketService();
-        oBasketWS.Url = ((WsConfigElement)oConfig).WSURL;
-        oBasketWS.Timeout = (int)oRequestData.RequestTimeout.TotalMilliseconds;
-        sResponseXML = oBasketWS.AddShippingToBasket(addShippingRequest.ShopperID, requestXML);
+        using (WscgdBasket.WscgdBasketService oBasketWS = new WscgdBasket.WscgdBasketService())
+        {
+          oBasketWS.Url = ((WsConfigElement)oConfig).WSURL;
+          oBasketWS.Timeout = (int)oRequestData.RequestTimeout.TotalMilliseconds;
+          sResponseXML = oBasketWS.AddShippingToBasket(addShippingRequest.ShopperID, requestXML);
+        }
         if (sResponseXML.IndexOf("<error>", StringComparison.OrdinalIgnoreCase) > -1)
         {
           AtlantisException exAtlantis = new AtlantisException(oRequestData,

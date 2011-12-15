c0ef31f00b427fcb938070d6c84f92b08583b45f
tbird <tbird@jomax.tfs.local>
2011-12-15 21:37:41 +0000

    Implement dispose on service
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C234944

--- Atlantis.Framework.BillingResourceId/Atlantis.Framework.BillingResourceId.Impl/BillingResourceIdRequest.cs
+++ Atlantis.Framework.BillingResourceId/Atlantis.Framework.BillingResourceId.Impl/BillingResourceIdRequest.cs
@@ -13,21 +13,23 @@ namespace Atlantis.Framework.BillingResourceId.Impl
       try
       {
         BillingResourceIdRequestData billingResourceIdRequestData = (BillingResourceIdRequestData)requestData;
-        GDBillingData.WSgdBillingDataService billingDataWS = new GDBillingData.WSgdBillingDataService();
-        billingDataWS.Url = ((WsConfigElement)config).WSURL;
-        billingDataWS.Timeout = (int)billingResourceIdRequestData.RequestTimeout.TotalMilliseconds;
-
-        string error;
-        string result = billingDataWS.GetResourceIDForShopperID(billingResourceIdRequestData.ShopperID, billingResourceIdRequestData.ProductId, out error);
-
-        if (!string.IsNullOrEmpty(error))
+        using (GDBillingData.WSgdBillingDataService billingDataWS = new GDBillingData.WSgdBillingDataService())
         {
-          AtlantisException exception = new AtlantisException(requestData, "BillingResourceIdRequest", error, billingResourceIdRequestData.ProductId.ToString());
-          responseData = new BillingResourceIdResponseData(exception);
-        }
-        else
-        {
-          responseData = new BillingResourceIdResponseData(result);
+          billingDataWS.Url = ((WsConfigElement)config).WSURL;
+          billingDataWS.Timeout = (int)billingResourceIdRequestData.RequestTimeout.TotalMilliseconds;
+
+          string error;
+          string result = billingDataWS.GetResourceIDForShopperID(billingResourceIdRequestData.ShopperID, billingResourceIdRequestData.ProductId, out error);
+
+          if (!string.IsNullOrEmpty(error))
+          {
+            AtlantisException exception = new AtlantisException(requestData, "BillingResourceIdRequest", error, billingResourceIdRequestData.ProductId.ToString());
+            responseData = new BillingResourceIdResponseData(exception);
+          }
+          else
+          {
+            responseData = new BillingResourceIdResponseData(result);
+          }
         }
       }
       catch (AtlantisException atlantisException)

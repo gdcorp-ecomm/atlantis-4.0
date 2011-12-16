59b0e3afd34b46b90f4322e159b948f7014166ff
tbird <tbird@jomax.tfs.local>
2011-12-16 17:31:03 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C235159

--- Atlantis.Framework.QSCThemeImages/Atlantis.Framework.QSCThemeImages.Impl/QSCThemeImagesRequest.cs
+++ Atlantis.Framework.QSCThemeImages/Atlantis.Framework.QSCThemeImages.Impl/QSCThemeImagesRequest.cs
@@ -16,17 +16,19 @@ namespace Atlantis.Framework.QSCThemeImages.Impl
       try
       {
         QSCThemeImagesRequestData cmsRequest = (QSCThemeImagesRequestData)requestData;
-        QSCService.FOSService service = new QSCService.FOSService();
-        service.Url = ((WsConfigElement)config).WSURL;
-        service.Timeout = (int)cmsRequest.RequestTimeout.TotalMilliseconds;
-        QSCService.template[] templates= service.getThemeInformation();
-        List<ThemeData> themes = new List<ThemeData>();
-        foreach (QSCService.template currentTemplate in templates)
+        using (QSCService.FOSService service = new QSCService.FOSService())
         {
-          ThemeData newTheme = new ThemeData(currentTemplate.backgroundID, currentTemplate.src, currentTemplate.thumbnailSrc);
-          themes.Add(newTheme);
+          service.Url = ((WsConfigElement)config).WSURL;
+          service.Timeout = (int)cmsRequest.RequestTimeout.TotalMilliseconds;
+          QSCService.template[] templates = service.getThemeInformation();
+          List<ThemeData> themes = new List<ThemeData>();
+          foreach (QSCService.template currentTemplate in templates)
+          {
+            ThemeData newTheme = new ThemeData(currentTemplate.backgroundID, currentTemplate.src, currentTemplate.thumbnailSrc);
+            themes.Add(newTheme);
+          }
+          responseData = new QSCThemeImagesResponseData(themes);
         }
-        responseData = new QSCThemeImagesResponseData(themes);
       } 
     
       catch (AtlantisException exAtlantis)

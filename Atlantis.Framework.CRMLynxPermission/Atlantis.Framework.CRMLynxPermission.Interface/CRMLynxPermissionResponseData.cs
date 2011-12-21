e217c4c8023ba6018a33219d5d699c6b4567ace1
jzogg:40 <jzogg:40@jomax.tfs.local>
2011-12-21 23:43:58 +0000

    convert CRMLynxPermission to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C236347

---
+++ Atlantis.Framework.CRMLynxPermission/Atlantis.Framework.CRMLynxPermission.Interface/CRMLynxPermissionResponseData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.SessionCache;
+
+namespace Atlantis.Framework.CRMLynxPermission.Interface
+{
+  public class CRMLynxPermissionResponseData : IResponseData, ISessionSerializableResponse
+  {
+    #region Properties
+    private readonly AtlantisException _exAtlantis;
+
+    private bool _userHasAccess;
+    public bool UserHasAccess
+    {
+      get { return _userHasAccess; }
+    }
+
+    public bool IsSuccess { get; private set; }
+
+    #endregion 
+
+    public CRMLynxPermissionResponseData()
+    { }
+
+    public CRMLynxPermissionResponseData(bool userHasAccess)
+    {
+      IsSuccess = true;
+      _userHasAccess = userHasAccess;
+    }
+
+    public CRMLynxPermissionResponseData(AtlantisException atlantisException)
+    {
+      _exAtlantis = atlantisException;
+    }
+
+    public CRMLynxPermissionResponseData(RequestData requestData, Exception exception)
+    {
+      _exAtlantis = new AtlantisException(requestData, "CRMLynxPermissionResponseData", exception.Message, requestData.ToXML());
+    }
+
+
+    #region IResponseData Members
+
+    public string ToXML()
+    {
+      return string.Format("<UserHasAccess>{0}</UserHasAccess>", UserHasAccess);
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exAtlantis;
+    }
+
+    #endregion
+
+    #region ISessionSerializableResponse Members
+
+    public string SerializeSessionData()
+    {
+      return ToXML();
+    }
+
+    public void DeserializeSessionData(string sessionData)
+    {
+      if (string.IsNullOrEmpty(sessionData))
+        return;
+
+      using (XmlReader reader = XmlReader.Create(new StringReader(sessionData)))
+      {
+        if (!reader.Read())
+          return;
+
+        reader.ReadStartElement("UserHasAccess");
+        if (bool.TryParse(reader.Value, out _userHasAccess))
+        {
+          IsSuccess = true;
+        }
+      }
+    }
+
+    #endregion
+  }
+}
---
+++ Atlantis.Framework.CRMLynxPermission/Atlantis.Framework.CRMLynxPermission.Tests/CRMLynxPermissionTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Atlantis.Framework.CRMLynxPermission.Interface;
+using Atlantis.Framework.Testing.MockHttpContext;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.CRMLynxPermission.Tests
+{
+  [TestClass]
+  public class GetCRMLynxPermissionTests
+  {
+
+    private const string _shopperId = "";
+    private const int _mgrUserId = 4692;
+    private const int _requestType = 353;
+
+    public GetCRMLynxPermissionTests()
+    { }
+
+    public TestContext TestContext { get; set; }
+
+    #region Additional test attributes
+
+    #endregion
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void CRMLynxPermissionTest()
+    {
+      var request = new CRMLynxPermissionRequestData(_shopperId
+         , string.Empty
+         , string.Empty
+         , string.Empty
+         , 0
+         , _mgrUserId
+         , "CancelSetupProducts");
+
+      var response = (CRMLynxPermissionResponseData)Engine.Engine.ProcessRequest(request, _requestType);
+
+      Debug.WriteLine(response.ToXML());
+      Assert.IsTrue(response.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void SerializeTest()
+    {
+      MockHttpContext.SetMockHttpContext(string.Empty, "http://localhost", string.Empty);
+
+      var request = new CRMLynxPermissionRequestData(_shopperId
+         , string.Empty
+         , string.Empty
+         , string.Empty
+         , 0
+         , _mgrUserId
+         , "CancelSetupProducts");
+
+      var response = SessionCache.SessionCache.GetProcessRequest<CRMLynxPermissionResponseData>(request, _requestType);
+
+      string xml = response.SerializeSessionData();
+      Debug.WriteLine(xml);
+      Assert.IsTrue(response.IsSuccess);
+    }
+  }
+}

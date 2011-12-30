ea5c408f0877d663c9c609c86cf218f94fcae096
Kent Searle <ksearle@godaddy.com>
2011-12-30 22:04:08 +0000

    Remove fromDate from GetCacheMD5
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C238483

--- Atlantis.Framework.MyaRecentNamespaces/Atlantis.Framework.MyaRecentNamespaces.Interface/MyaRecentNamespacesRequestData.cs
+++ Atlantis.Framework.MyaRecentNamespaces/Atlantis.Framework.MyaRecentNamespaces.Interface/MyaRecentNamespacesRequestData.cs
@@ -21,7 +21,7 @@ namespace Atlantis.Framework.MyaRecentNamespaces.Interface
       MD5 oMD5 = new MD5CryptoServiceProvider();
       oMD5.Initialize();
 
-      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}-{1}", ShopperID, FromDate));
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}", ShopperID));
       byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
       string sValue = BitConverter.ToString(md5Bytes, 0);
       return sValue.Replace("-", "");

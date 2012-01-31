6b334e0a54bcc0a66346e0072b6ba8d051b2fba7
syukna:167 <syukna:167@jomax.tfs.local>
2012-01-31 00:14:39 +0000

    Used to get balances of in store credit and also used to determine if a shopper has any in store credit.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C243688

---
+++ Atlantis.Framework.EcommInstoreBalance/Atlantis.Framework.EcommInstoreBalance.Impl/EcommInstoreBalanceRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Atlantis.Framework.EcommInstoreBalance.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Nimitz;
+
+namespace Atlantis.Framework.EcommInstoreBalance.Impl
+{
+  public class EcommInstoreBalanceRequest: IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      EcommInstoreBalanceResponseData response = null;
+      try
+      {
+        string connectionString = NetConnect.LookupConnectInfo(config);
+        
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+          using (SqlCommand command = new SqlCommand("instore_creditGetBalanceWithCurrencyByShopper_sp", connection))
+          {
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandTimeout = (int)requestData.RequestTimeout.TotalSeconds;
+            command.Parameters.AddWithValue("@s_shopper_id", requestData.ShopperID);
+
+
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            response = new EcommInstoreBalanceResponseData(dt);
+          }
+        }
+      }
+      catch (AtlantisException aex)
+      {
+        response = new EcommInstoreBalanceResponseData(aex);
+      }
+      catch (Exception ex)
+      {
+        response = new EcommInstoreBalanceResponseData(requestData, ex);
+      }
+
+      return response;
+    }
+  }
+}
---
+++ Atlantis.Framework.EcommInstoreBalance/Atlantis.Framework.EcommInstoreBalance.Interface/EcommInstoreBalanceRequestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.EcommInstoreBalance.Interface
+{
+  public class EcommInstoreBalanceRequestData: RequestData
+  {
+    public EcommInstoreBalanceRequestData(string shopperId, string sourceURL, string orderId, string pathway, int pageCount)
+      : base(shopperId, sourceURL, orderId, pathway, pageCount)
+    {     
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

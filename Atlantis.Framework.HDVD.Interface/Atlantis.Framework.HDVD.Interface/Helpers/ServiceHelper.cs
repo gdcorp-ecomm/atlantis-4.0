d2dbe0d5d5e1607ec899611c85c7d113ba31023b
kklink <kklink@jomax.tfs.local>
2012-02-09 18:29:04 +0000

    Correction to typo
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C245959

--- Atlantis.Framework.HDVD.Interface/Atlantis.Framework.HDVD.Interface/Helpers/ServiceHelper.cs
+++ Atlantis.Framework.HDVD.Interface/Atlantis.Framework.HDVD.Interface/Helpers/ServiceHelper.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Atlantis.Framework.HDVD.Interface.Aries;
+﻿using Atlantis.Framework.HDVD.Interface.Aries;
 
 namespace Atlantis.Framework.HDVD.Interface.Helpers
 {
-  public static class SerivceHelper
+  public static class ServiceHelper
   {
     public static HCCAPIServiceAries GetServiceReference(string wsUrl)
     {

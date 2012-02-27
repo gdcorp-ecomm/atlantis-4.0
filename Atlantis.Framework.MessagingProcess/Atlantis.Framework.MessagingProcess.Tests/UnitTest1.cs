4f8bdab76dafae5532b6c8078ff9f542b346cd7e
Gregg Stelmach <gstelmach@godaddy.com>
2012-02-27 23:44:06 +0000

    Added more email unit tests.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C249381

--- Atlantis.Framework.MessagingProcess/Atlantis.Framework.MessagingProcess.Tests/UnitTest1.cs
+++ Atlantis.Framework.MessagingProcess/Atlantis.Framework.MessagingProcess.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Reflection;
 using System.IO;
 using Atlantis.Framework.MessagingProcess.Interface;
@@ -26,17 +27,12 @@ namespace Atlantis.Framework.MessagingProcess.Tests
     ///</summary>
     public TestContext TestContext
     {
-      get
-      {
-        return testContextInstance;
-      }
-      set
-      {
-        testContextInstance = value;
-      }
+      get { return testContextInstance; }
+      set { testContextInstance = value; }
     }
 
     #region Additional test attributes
+
     //
     // You can use the following additional attributes as you write your tests:
     //
@@ -56,6 +52,7 @@ namespace Atlantis.Framework.MessagingProcess.Tests
     // [TestCleanup()]
     // public void MyTestCleanup() { }
     //
+
     #endregion
 
     [TestMethod]
@@ -64,7 +61,7 @@ namespace Atlantis.Framework.MessagingProcess.Tests
     {
       //string xml = TestEmail1Xml;
       MessagingProcessRequestData request = new MessagingProcessRequestData(
-        "840820", string.Empty, string.Empty, string.Empty, 0, 1, "SuperBowlCommercial", "TellAFriend");
+              "840820", string.Empty, string.Empty, string.Empty, 0, 1, "SuperBowlCommercial", "TellAFriend");
 
       ResourceItem resource = new ResourceItem("SuperBowl", "0");
       resource["EMAIL_MESSAGE"] = new AttributeValue("message");
@@ -82,7 +79,87 @@ namespace Atlantis.Framework.MessagingProcess.Tests
       request.AddResource(resource);
 
       MessagingProcessResponseData response =
-        (MessagingProcessResponseData)Engine.Engine.ProcessRequest(request, 66);
+              (MessagingProcessResponseData) Engine.Engine.ProcessRequest(request, 66);
+      Assert.IsTrue(response.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void ResetPasswordEmail()
+    {
+      var UserName = "855307";
+
+      MessagingProcessRequestData request = new MessagingProcessRequestData(UserName, string.Empty, string.Empty, string.Empty, 0, 1,
+                                                                            "PasswordReset", "MYA");
+
+      request.AddDictionaryItem("LocalizationCode", "ES");
+
+      ResourceItem resource = new ResourceItem("Shopper", UserName);
+      resource["AuthToken"] = new AttributeValue(Guid.NewGuid().ToString());
+      resource["Login"] = new AttributeValue(UserName);
+
+      ContactPointItem contactPoint = new ContactPointItem("ShopperContact", "SHOPPER");
+      contactPoint["id"] = UserName;
+      contactPoint["SendEmail"] = "true";
+
+      resource.ContactPoints.Add(contactPoint);
+
+      request.AddResource(resource);
+
+      MessagingProcessResponseData response =
+              (MessagingProcessResponseData) Engine.Engine.ProcessRequest(request, 66);
+      Assert.IsTrue(response.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void WelcomeEmail()
+    {
+      var UserName = "855307";
+
+      MessagingProcessRequestData request = new MessagingProcessRequestData(UserName, string.Empty, string.Empty, string.Empty, 0, 1,
+                                                                            "AccountCreation", "OrderProcessing");
+
+      request.AddDictionaryItem("LocalizationCode", "ES");
+
+      ResourceItem resource = new ResourceItem("Shopper", UserName);
+
+      ContactPointItem contactPoint = new ContactPointItem("ShopperContact", "SHOPPER");
+      contactPoint["id"] = UserName;
+      contactPoint["SendEmail"] = "true";
+
+      resource.ContactPoints.Add(contactPoint);
+
+      request.AddResource(resource);
+
+      MessagingProcessResponseData response =
+              (MessagingProcessResponseData) Engine.Engine.ProcessRequest(request, 66);
+      Assert.IsTrue(response.IsSuccess);
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void RetrieveShopperNum()
+    {
+      var UserName = "855307";
+
+      MessagingProcessRequestData request = new MessagingProcessRequestData(UserName, string.Empty, string.Empty, string.Empty, 0, 1,
+                                                                            "LoginInformation", "MYA");
+
+      request.AddDictionaryItem("LocalizationCode", "ES");
+
+      ResourceItem resource = new ResourceItem("Shopper", UserName);
+      resource["Login"] = new AttributeValue(UserName);
+
+      ContactPointItem contactPoint = new ContactPointItem("ShopperContact", "SHOPPER");
+      contactPoint["id"] = UserName;
+      contactPoint["SendEmail"] = "true";
+
+      resource.ContactPoints.Add(contactPoint);
+
+      request.AddResource(resource);
+
+      MessagingProcessResponseData response = (MessagingProcessResponseData) Engine.Engine.ProcessRequest(request, 66);
       Assert.IsTrue(response.IsSuccess);
     }
   }

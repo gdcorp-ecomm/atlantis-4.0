03ce57774fadfec661f659a12db335aaab337395
trwalker <trwalker@jomax.tfs.local>
2012-03-20 20:55:38 +0000

    Atlantis.Framework.Auth.Interface initial check in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C253796

---
+++ Atlantis.Framework.Auth.Interface/Atlantis.Framework.Auth.Interface/AuthTwoFactorPhoneXml.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Serialization;
+using XmlSerializer = Atlantis.Framework.Auth.Interface.Serializers.XmlSerializer;
+
+namespace Atlantis.Framework.Auth.Interface
+{
+  [XmlRoot(ElementName = "Phone")]
+  public class AuthTwoFactorPhoneXml
+  {
+    [XmlAttribute(AttributeName = "number")]
+    public string PhoneNumber { get; set; }
+
+    [XmlAttribute(AttributeName = "carrier")]
+    public string Carrier { get; set; }
+
+    public AuthTwoFactorPhoneXml()
+    {
+    }
+
+    public AuthTwoFactorPhoneXml(string xml)
+    {
+      if (!string.IsNullOrEmpty(xml))
+      {
+        XmlSerializer xmlSerializer = new XmlSerializer();
+
+        try
+        {
+          AuthTwoFactorPhoneXml authTwoFactorPhoneXml = xmlSerializer.Deserialize<AuthTwoFactorPhoneXml>(xml);
+          PhoneNumber = authTwoFactorPhoneXml.PhoneNumber;
+          Carrier = authTwoFactorPhoneXml.Carrier;
+        }
+        catch
+        {
+          PhoneNumber = string.Empty;
+          Carrier = string.Empty;
+        }
+      }
+    }
+
+    public string ToXml()
+    {
+      string xml;
+      XmlSerializer xmlSerializer = new XmlSerializer();
+
+      try
+      {
+        xml = xmlSerializer.Serialize(this);
+      }
+      catch
+      {
+        xml = string.Empty;
+      }
+
+      return xml;
+    }
+  }
+}

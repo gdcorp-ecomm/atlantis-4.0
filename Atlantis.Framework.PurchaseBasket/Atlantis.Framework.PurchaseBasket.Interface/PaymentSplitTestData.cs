735d63c5e1235a95dc169275c65674f9d5ae5fee
tbird <tbird@jomax.tfs.local>
2012-03-26 22:40:41 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C254920

---
+++ Atlantis.Framework.PurchaseBasket/Atlantis.Framework.PurchaseBasket.Interface/PaymentSplitTestData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.PurchaseBasket.Interface
+{
+  public class PaymentSplitTestData : PaymentElement
+  {
+
+    public override string ElementName
+    {
+      get { return "SplitTestData"; }
+    }
+
+  }
+}

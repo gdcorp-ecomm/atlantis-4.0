b0720d88bd030f211abee538b51b83afa685a280
trwalker <trwalker@jomax.tfs.local>
2012-04-02 16:42:23 +0000

    Atlantis.Framework.ValidateField update to ValidationFailure class and xml so that messages are in a format and you can pass your field name to get a complete error message
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C256247

--- Atlantis.Framework.ValidateField/Atlantis.Framework.ValidateField.Interface/ValidationFailure.cs
+++ Atlantis.Framework.ValidateField/Atlantis.Framework.ValidateField.Interface/ValidationFailure.cs
@@ -3,13 +3,19 @@ namespace Atlantis.Framework.ValidateField.Interface
 {
   public class ValidationFailure
   {
+    private string DescriptionFormat { get; set; }
+
     public int FailureCode { get; private set; }
-    public string Description { get; private set; }
 
     public ValidationFailure(int failureCode, string description)
     {
       FailureCode = failureCode;
-      Description = description;
+      DescriptionFormat = description;
+    }
+
+    public string GetFormattedDescription(string fieldName)
+    {
+      return string.Format(DescriptionFormat, fieldName);
     }
   }
 }

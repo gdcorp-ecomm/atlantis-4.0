4ad010b6f04ae24322434b26386a7539932dedf6
syukna:167 <syukna:167@jomax.tfs.local>
2012-04-03 21:12:06 +0000

    Updated to check for ded_ip and ipaddress.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C256657

--- Atlantis.Framework.OrionGetShopperIdByIP/Atlantis.Framework.OrionGetShopperIdByIP.Impl/OrionGetShopperIdByIPRequest.cs
+++ Atlantis.Framework.OrionGetShopperIdByIP/Atlantis.Framework.OrionGetShopperIdByIP.Impl/OrionGetShopperIdByIPRequest.cs
@@ -12,9 +12,6 @@ namespace Atlantis.Framework.OrionGetShopperIdByIP.Impl
   {
     public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
     {
-      string[] errors = { };
-      string[] returnAttributeList = { };
-      AccountQueryResponse accountList;
       OrionGetShopperIdByIPResponseData response = null;
 
       try
@@ -30,40 +27,31 @@ namespace Atlantis.Framework.OrionGetShopperIdByIP.Impl
             accountServices.Timeout = (int)orionRequestData.RequestTimeout.TotalMilliseconds;
             accountServices.SecureHeaderValue = new SecureHeader { Token = responseSecurityData.AuthToken };
 
-            var query = new AccountQuery();
+            List<Account> accounts = SearchForAccounts(accountServices, "IPADDRESS", orionRequestData.IpToSearch);
 
-            query.ProductName = string.Empty;
-            query.SelectTop = 0;
-            query.StartingId = 0;
-            query.SystemNamespace = string.Empty;
-            query.CustomerNum = string.Empty;
-            query.ResellerId = -1;
-            query.ShowRemoved = 0;
-            query.cdtn = new AccountQueryCondition[] {
-                              new AccountQueryCondition { 
-                                    AttributeName= "server_data", 
-                                    ElementName="DEDICATED_IPADDRESS",
-                                    ElementValue = orionRequestData.IpToSearch }
-                         };
-
-            accountServices.GetAccountListByQuery("1", query, returnAttributeList, out accountList, out errors);
-
-            List<Account> accounts = accountList.AccountList.Where(a => a.Status.ToLower().Equals("setup")).ToList();
-
-            if (accounts.Count == 0)
+            if (accounts.Count != 1)
             {
-              response = new OrionGetShopperIdByIPResponseData(shopperId: null);
-            }
-            else if (accounts.Count == 1)
-            {
-              
-              var shopperId = accounts[0].CustomerNum ?? string.Empty;
-              response = new OrionGetShopperIdByIPResponseData(shopperId);
+              accounts = SearchForAccounts(accountServices, "DEDICATED_IPADDRESS", orionRequestData.IpToSearch);
+
+              if (accounts.Count == 0)
+              {
+                response = new OrionGetShopperIdByIPResponseData(shopperId: null);
+              }
+              else if (accounts.Count == 1)
+              {
+                var shopperId = accounts[0].CustomerNum ?? string.Empty;
+                response = new OrionGetShopperIdByIPResponseData(shopperId);
+              }
+              else
+              {
+                var aex = new AtlantisException(requestData, "OrionGetShopperIdByIPResponse", "More than one account with this dedicated IP address is setup.  Possibly bad data in DB", "Dedicated IP: " + orionRequestData.IpToSearch);
+                response = new OrionGetShopperIdByIPResponseData(aex);
+              }
             }
             else
-            {
-              var aex = new AtlantisException(requestData, "OrionGetShopperIdByIPResponse", "More than one account with this dedicated IP address is setup.  Possibly bad data in DB", "Dedicated IP: " + orionRequestData.IpToSearch);
-              response = new OrionGetShopperIdByIPResponseData(aex);
+            {              
+              var shopperId = accounts[0].CustomerNum ?? string.Empty;
+              response = new OrionGetShopperIdByIPResponseData(shopperId);
             }
           }
         }
@@ -79,6 +67,37 @@ namespace Atlantis.Framework.OrionGetShopperIdByIP.Impl
 
       return response;
     }
+
+    #region Search For Accounts
+    private List<Account> SearchForAccounts(AccountQueries accountServices, string elementType, string ipAddress)
+    {
+      string[] errors = { };
+      string[] returnAttributeList = { };
+      AccountQueryResponse accountList;
+
+      var query = new AccountQuery();
+
+      query.ProductName = string.Empty;
+      query.SelectTop = 0;
+      query.StartingId = 0;
+      query.SystemNamespace = string.Empty;
+      query.CustomerNum = string.Empty;
+      query.ResellerId = -1;
+      query.ShowRemoved = 0;
+      query.cdtn = new AccountQueryCondition[] {
+                              new AccountQueryCondition { 
+                                    AttributeName= "server_data", 
+                                    ElementName= elementType,
+                                    ElementValue = ipAddress }
+                         };
+
+      accountServices.GetAccountListByQuery("1", query, returnAttributeList, out accountList, out errors);
+      List<Account> accounts = accountList.AccountList.Where(a => a.Status.ToLower().Equals("setup")).ToList();
+
+      return accounts;
+    }
+
+    #endregion
 
     #region OrionSecurity
     private static OrionSecurityAuthResponseData GetOrionAuthToken(RequestData accountRequestData)

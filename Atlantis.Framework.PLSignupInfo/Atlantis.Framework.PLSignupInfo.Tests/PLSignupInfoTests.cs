375f051269c5d23f77d0ac50b67b0338a9d31776
Michael Micco <mmicco@godaddy.com>
2012-04-03 17:41:00 +0000

    Atlantis.Framework.PLSignupInfo Triplet (522)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C256563

---
+++ Atlantis.Framework.PLSignupInfo/Atlantis.Framework.PLSignupInfo.Tests/PLSignupInfoTests.cs
@@ -0,0 +1,20 @@
+﻿using Atlantis.Framework.PLSignupInfo.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.PLSignupInfo.Tests
+{
+  [TestClass]
+  public class PLSignupInfoTests
+  {
+    [TestMethod]
+    [DeploymentItem("Interop.gdDataCacheLib.dll")]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("Atlantis.Framework.PLSignupInfo.Impl.dll")]
+    public void BasicSignupInfo()
+    {
+      PLSignupInfoRequestData request = new PLSignupInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 1724);
+      PLSignupInfoResponseData response = (PLSignupInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 522);
+      Assert.IsNotNull(response.DefaultTransactionCurrencyType);
+    }
+  }
+}

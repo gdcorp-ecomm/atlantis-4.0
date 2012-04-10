37a77a02c581cfc81b0bd0a2d34f9589aed58a47
cgregory <cgregory@jomax.tfs.local>
2012-04-10 21:29:27 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C258085

--- Atlantis.Framework.DotTypeCache/Atlantis.Framework.DotTypeCache.DotAt/DotAt.cs
+++ Atlantis.Framework.DotTypeCache/Atlantis.Framework.DotTypeCache.DotAt/DotAt.cs
@@ -53,5 +53,15 @@ namespace Atlantis.Framework.DotTypeCache.DotAt
       get { return 1; }
     }
 
+    public override int MaxRenewalLength
+    {
+      get { return 1; }
+    }
+
+    public override int MaxRenewalMonthsOut
+    {
+      get { return 24; }
+    }
+
   }
 }

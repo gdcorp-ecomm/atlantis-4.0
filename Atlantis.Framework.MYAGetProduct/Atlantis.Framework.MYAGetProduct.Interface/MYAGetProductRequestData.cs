f66f7e50391c16a41b09f21e808422206652ee1c
msavarese <msavarese@jomax.tfs.local>
2012-04-10 20:01:58 +0000

    Conversion to 4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C258028

---
+++ Atlantis.Framework.MYAGetProduct/Atlantis.Framework.MYAGetProduct.Interface/MYAGetProductRequestData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Cryptography;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.MYAGetProduct.Interface.PageHelper;
+
+namespace Atlantis.Framework.MYAGetProduct.Interface
+{
+  public class MYAGetProductRequestData : RequestData
+  {
+    #region Properties
+
+    private PagingInfo _pagingInfo;
+    private int _productTypeId;
+    private static TimeSpan _requestTimeout = TimeSpan.FromSeconds(4);
+
+    public PagingInfo PagingInfo
+    {
+      get { return _pagingInfo; }
+    }
+
+    public int ProductTypeId
+    {
+      get { return _productTypeId; }
+    }
+
+    public TimeSpan RequestTimeout
+    {
+      get { return _requestTimeout; }
+      set { _requestTimeout = value; }
+    }
+    #endregion
+
+    #region Constructor
+    public MYAGetProductRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderId,
+                                  string pathway,
+                                  int pageCount,
+                                  PagingInfo pagingInfo,
+                                  int productTypeId)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      RequestTimeout = _requestTimeout;
+      _pagingInfo = pagingInfo;
+      _productTypeId = productTypeId;
+    }
+    #endregion
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("GetCacheMD5 not implemented in MYAGetProductRequestData");
+    }
+  }
+}
---
+++ Atlantis.Framework.MobileSaveSecurityAgreement/Atlantis.Framework.MobileSaveSecurityAgreement.Interface/MobileSaveSecurityAgreementRequestData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.MobileSaveSecurityAgreement.Interface
+{
+  public enum MobileSaveSecurityAgreementType
+  {
+    Username = 1,
+    UsernameAndPassword = 2,
+  }
+
+  public enum MobileApplicationType
+  {
+    GDShopper = 1,
+    Email = 2,
+    UserPass = 3,
+  }
+
+  public enum AppId
+  {
+    Iphone = 1,
+    Blackberry = 2,
+    Android = 3,
+    Ipad = 4,
+  }
+
+  public class MobileSaveSecurityAgreementRequestData : RequestData
+  {
+    private static TimeSpan _requestTimeout = TimeSpan.FromSeconds(6);
+
+
+    public AppId AppId { get; set; }
+    public string DeviceId { get; set; }
+    public MobileApplicationType MobileAppType { get; set; }
+    public MobileSaveSecurityAgreementType AgreementType { get; set; }
+
+    public MobileSaveSecurityAgreementRequestData(string shopperID,
+                                           string sourceUrl,
+                                           string orderID,
+                                           string pathway,
+                                           int pageCount,
+                                           AppId appId,
+                                           string deviceId,
+                                           MobileSaveSecurityAgreementType agreementType,
+                                           MobileApplicationType mobileAppType)
+      : base(shopperID, sourceUrl, orderID, pathway, pageCount)
+    {
+      RequestTimeout = _requestTimeout;
+      AgreementType = agreementType;
+      AppId = appId;
+      MobileAppType = mobileAppType;
+      DeviceId = deviceId;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException("MobileSaveSecurityAgreementRequestData is not a cacheable request");
+    }
+  }
+}

b5733ec13990772d8ade56e06a4f2e3375cded53
trwalker <trwalker@jomax.tfs.local>
2012-04-11 22:54:40 +0000

    Atlantis.Framework.Interface update to move GetClientCertificate into the WsConfig element
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C258547

--- Atlantis.Framework.Engine/Atlantis.Framework.Interface/WsConfigElement.cs
+++ Atlantis.Framework.Engine/Atlantis.Framework.Interface/WsConfigElement.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Atlantis.Framework.Interface
 {
   public class WsConfigElement : ConfigElement
   {
-    string _webServiceUrl;
+    private string _webServiceUrl;
+    public string WSURL
+    {
+      get { return _webServiceUrl; }
+    }
 
     public WsConfigElement(string progId, string assembly, bool LPC, string webServiceUrl)
       : base(progId, assembly, LPC)
@@ -18,9 +24,57 @@ namespace Atlantis.Framework.Interface
       _webServiceUrl = webServiceUrl;
     }
 
-    public string WSURL
+    private static bool IsCertificateExpired(X509Certificate2 certificate)
     {
-      get { return _webServiceUrl; }
+      bool isExpired = false;
+
+      DateTime expirationDate = DateTime.Parse(certificate.GetExpirationDateString());
+
+      if (expirationDate < DateTime.Now)
+      {
+        isExpired = true;
+      }
+
+      return isExpired;
+    }
+
+    public X509Certificate2 GetClientCertificate()
+    {
+      return GetClientCertificate(GetConfigValue("ClientCertificateName"));
+    }
+
+    public X509Certificate2 GetClientCertificate(string friendlyName)
+    {
+      X509Certificate2 clientCertificate = null;
+      X509Store store = null;
+
+      if (!string.IsNullOrEmpty(friendlyName))
+      {
+        try
+        {
+          store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+          store.Open(OpenFlags.OpenExistingOnly | OpenFlags.ReadOnly);
+
+          foreach (X509Certificate2 certificate in store.Certificates)
+          {
+            if (certificate.FriendlyName.Equals(friendlyName, StringComparison.CurrentCultureIgnoreCase) &&
+                !IsCertificateExpired(certificate))
+            {
+              clientCertificate = certificate;
+              break;
+            }
+          }
+        }
+        finally
+        {
+          if (store != null)
+          {
+            store.Close();
+          }
+        }
+      }
+
+      return clientCertificate;
     }
   }
 }

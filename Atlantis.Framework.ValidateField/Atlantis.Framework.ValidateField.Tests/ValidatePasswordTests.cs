e734fe9063e11ca1e0d0677cde6cf73429a002b8
trwalker <trwalker@jomax.tfs.local>
2012-04-12 12:06:45 +0000

    Atlantis.Framework.ValidateField update to password validation to not require lower case letters (to match ecomm requirements)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C258618

--- Atlantis.Framework.ValidateField/Atlantis.Framework.ValidateField.Tests/ValidatePasswordTests.cs
+++ Atlantis.Framework.ValidateField/Atlantis.Framework.ValidateField.Tests/ValidatePasswordTests.cs
@@ -1,65 +1,12 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Atlantis.Framework.ValidateField.Interface;
 
 namespace Atlantis.Framework.ValidateField.Tests
 {
-  /// <summary>
-  /// Summary description for UnitTest1
-  /// </summary>
   [TestClass]
   public class ValidatePasswordTests
   {
-    public ValidatePasswordTests()
-    {
-      //
-      // TODO: Add constructor logic here
-      //
-    }
-
-    private TestContext testContextInstance;
-
-    /// <summary>
-    ///Gets or sets the test context which provides
-    ///information about and functionality for the current test run.
-    ///</summary>
-    public TestContext TestContext
-    {
-      get
-      {
-        return testContextInstance;
-      }
-      set
-      {
-        testContextInstance = value;
-      }
-    }
-
-    #region Additional test attributes
-    //
-    // You can use the following additional attributes as you write your tests:
-    //
-    // Use ClassInitialize to run code before running the first test in the class
-    // [ClassInitialize()]
-    // public static void MyClassInitialize(TestContext testContext) { }
-    //
-    // Use ClassCleanup to run code after all tests in a class have run
-    // [ClassCleanup()]
-    // public static void MyClassCleanup() { }
-    //
-    // Use TestInitialize to run code before running each test 
-    // [TestInitialize()]
-    // public void MyTestInitialize() { }
-    //
-    // Use TestCleanup to run code after each test has run
-    // [TestCleanup()]
-    // public void MyTestCleanup() { }
-    //
-    #endregion
-
     [TestMethod]
     [DeploymentItem("atlantis.config")]
     [DeploymentItem("Atlantis.Framework.ValidateField.Impl.dll")]
@@ -141,20 +88,6 @@ namespace Atlantis.Framework.ValidateField.Tests
 
       List<ValidationFailure> errors;
       bool isValid = validator.ValidateStringField("#abkd uZeik<k>ej%-_", out errors);
-      Assert.IsFalse(isValid);
-      Assert.IsTrue(HasFailureCode(errors, 10));
-    }
-
-    [TestMethod]
-    [DeploymentItem("atlantis.config")]
-    [DeploymentItem("Atlantis.Framework.ValidateField.Impl.dll")]
-    public void PasswordNoLowerCase()
-    {
-      var request = new ValidateFieldRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, "password");
-      ValidateFieldResponseData validator = (ValidateFieldResponseData)DataCache.DataCache.GetProcessRequest(request, 507);
-
-      List<ValidationFailure> errors;
-      bool isValid = validator.ValidateStringField("#ABKD UZEI9K-_", out errors);
       Assert.IsFalse(isValid);
       Assert.IsTrue(HasFailureCode(errors, 10));
     }

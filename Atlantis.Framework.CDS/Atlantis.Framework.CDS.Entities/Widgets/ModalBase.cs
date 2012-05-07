2cb16b6c79574c57be5156cdcafdcca78e7736bd
dgoldstein <dgoldstein@jomax.tfs.local>
2012-05-07 16:55:44 +0000

    CDS - Remove unused base pages reference
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C263067

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/ModalBase.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/ModalBase.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Text;
 using System.Web.Script.Serialization;
 using System.Web.UI;
-using Atlantis.Framework.BasePages;
 using Atlantis.Framework.Interface;
 using Atlantis.Framework.Providers.Interface.ProviderContainer;
 

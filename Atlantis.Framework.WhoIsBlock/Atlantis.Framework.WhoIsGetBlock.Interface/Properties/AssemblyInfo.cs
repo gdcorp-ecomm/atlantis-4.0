aeff7b3423b72faba2772a484770a04f39e21104
Michael Melamud <mmelamud@godaddy.com>
2012-05-25 15:16:22 +0000

    4.0
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C266820

---
+++ Atlantis.Framework.WhoIsBlock/Atlantis.Framework.WhoIsGetBlock.Interface/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Atlantis.Framework.WhoIsGetBlock.Interface")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+#endif
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GoDaddy.com")]
+[assembly: AssemblyProduct("Atlantis.Framework.WhoIsGetBlock.Interface")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd5b54aa-6165-4635-a19c-f28f36c85434")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.12.5.25")]

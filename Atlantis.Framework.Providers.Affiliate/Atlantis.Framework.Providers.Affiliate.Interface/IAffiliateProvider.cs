fb92345cc4ae2d929f3e8344d9f354e36ae545b1
Kent Searle <ksearle@godaddy.com>
2012-06-02 02:14:24 +0000

    P82201 - Initial check-in
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C268094

---
+++ Atlantis.Framework.Providers.Affiliate/Atlantis.Framework.Providers.Affiliate.Interface/IAffiliateProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atlantis.Framework.Providers.Affiliate.Interface
+{
+  public interface IAffiliateProvider
+  {
+    int? UpdatedAffiliateMetaDataRequest { get; set; }
+
+    bool ProcessAffiliateSourceCode(string isc, out string affiliateType, out DateTime affiliateStartDate);
+    bool IsValidAffiliate(string affiliateType);
+  }
+}

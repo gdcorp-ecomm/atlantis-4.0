ad2e0da83d9639db603d41bc6fe1699d317dfc9a
Kent Searle <ksearle@godaddy.com>
2012-06-07 18:22:23 +0000

    Removing unused interface element
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C269115

--- Atlantis.Framework.Providers.Affiliate/Atlantis.Framework.Providers.Affiliate.Interface/IAffiliateProvider.cs
+++ Atlantis.Framework.Providers.Affiliate/Atlantis.Framework.Providers.Affiliate.Interface/IAffiliateProvider.cs
@@ -4,8 +4,6 @@ namespace Atlantis.Framework.Providers.Affiliate.Interface
 {
   public interface IAffiliateProvider
   {
-    int? UpdatedAffiliateMetaDataRequest { get; set; }
-
     bool ProcessAffiliateSourceCode(string isc, out string affiliateType, out DateTime affiliateStartDate);
     bool IsValidAffiliate(string affiliateType);
   }

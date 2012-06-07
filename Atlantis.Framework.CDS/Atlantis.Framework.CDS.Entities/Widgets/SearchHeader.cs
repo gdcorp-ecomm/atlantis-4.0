c21a70d0301fc7df3aa8e3450090546a0ce1b28e
jwjones <jwjones@jomax.tfs.local>
2012-06-07 17:42:47 +0000

    add social data back to searchheader
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C269091

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SearchHeader.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SearchHeader.cs
@@ -31,6 +31,10 @@ namespace Atlantis.Framework.CDS.Entities.Widgets
       public string ItemType { get; set; }
       public string Title { get; set; }
       public string TweetText { get; set; }
+      public string TweetUrl { get; set; }
+      public string TweetRelated { get; set; }
+      public bool UseFacebook { get; set; }
+      public bool UseTwitter { get; set; }
       public bool UseGooglePlus { get; set; }
     }
 

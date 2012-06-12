0888d9cff8d648ee643007e26975f23d185983f1
dgoldstein <dgoldstein@jomax.tfs.local>
2012-06-12 16:11:47 +0000

    Roll back constructors and remove attribute
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C269876

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesScript.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesScript.cs
@@ -37,13 +37,7 @@ namespace Atlantis.Framework.CDS.Entities.Widgets
 
     public class ModalData
     {
-      public ModalData()
-      {
-        this.TargetDivWidth = 0;
-      }
-
       public string Url { get; set; }
-      [DisplayName("Target Width")]
       public int TargetDivWidth { get; set; }
 
       private string _associatedMktgBtn;
@@ -74,13 +68,10 @@ namespace Atlantis.Framework.CDS.Entities.Widgets
 
     public class VideoData : ModalData
     {
-      public VideoData() : base() { }
     }
 
     public class PopupData : ModalData
     {
-      public PopupData() : base() { }
-
       [DisplayName("Window Name")]
       public string WindowName { get; set; }
       [DisplayName("Window Params")]

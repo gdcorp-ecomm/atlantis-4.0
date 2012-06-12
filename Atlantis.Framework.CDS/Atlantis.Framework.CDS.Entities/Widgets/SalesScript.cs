4b8080256633e47f585253d7f6a7a51b1a3c0415
dgoldstein <dgoldstein@jomax.tfs.local>
2012-06-12 16:17:42 +0000

    Add back attribute and default value attribute
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C269881

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesScript.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesScript.cs
@@ -38,6 +38,8 @@ namespace Atlantis.Framework.CDS.Entities.Widgets
     public class ModalData
     {
       public string Url { get; set; }
+      [DisplayName("Target Width")]
+      [DefaultValue(0)]
       public int TargetDivWidth { get; set; }
 
       private string _associatedMktgBtn;

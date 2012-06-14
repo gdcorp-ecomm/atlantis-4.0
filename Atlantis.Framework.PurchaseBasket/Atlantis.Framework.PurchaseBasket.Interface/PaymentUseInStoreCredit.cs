d422261c0bd519e5b3204a7481897202bf60a92d
pmccormack <pmccormack@jomax.tfs.local>
2012-06-14 23:23:01 +0000

    Added In Store Credit Payment Element class
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C270651

---
+++ Atlantis.Framework.PurchaseBasket/Atlantis.Framework.PurchaseBasket.Interface/PaymentUseInStoreCredit.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Atlantis.Framework.PurchaseBasket.Interface
+{
+  public class PaymentUseInStoreCredit : PaymentElement
+  {
+    public override string ElementName
+    {
+      get { return "ISCPayment"; }
+    }
+    
+    public int Amount
+    {
+      get { return GetIntProperty("amount", 0); }
+      set { this["amount"] = value.ToString(); }
+    }
+  }
+}

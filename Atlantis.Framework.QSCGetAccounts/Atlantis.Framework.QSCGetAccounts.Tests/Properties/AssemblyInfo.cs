977c86658b6bfd7161cd60f6c950bbf7de38ee70
triedy <triedy@jomax.tfs.local>
2012-06-25 19:50:06 +0000

    Update version info and moved QSC.Interface out of QSCGetAccounts folder.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C272107

--- Atlantis.Framework.QSCGetAccounts/Atlantis.Framework.QSCGetAccounts.Tests/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.QSCGetAccounts/Atlantis.Framework.QSCGetAccounts.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Atlantis.Framework.QSCGetAccounts.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("GoDaddy.com, LLC")]
 [assembly: AssemblyProduct("Atlantis.Framework.QSCGetAccounts.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com, LLC 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.12.6.25")]

d68300a5b4b7ea13e42de579ebd54bcb03f494b8
triedy <triedy@jomax.tfs.local>
2012-06-28 20:38:58 +0000

    P40415 - Update AssemblyFileVersion for QSCUpdateContact triplet.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C273019

--- Atlantis.Framework.QSCUpdateContact/Atlantis.Framework.QSCUpdateContact.Tests/QSCUpdateContactTests.cs
+++ Atlantis.Framework.QSCUpdateContact/Atlantis.Framework.QSCUpdateContact.Tests/QSCUpdateContactTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
 using Atlantis.Framework.QSC.Interface.Constants;
 using Atlantis.Framework.QSCUpdateContact.Interface;
 using Microsoft.VisualStudio.TestTools.UnitTesting;

9cd9d7ac705d4c6024eb3d4f9020fa8e3dc1bc1b
dgoldstein <dgoldstein@jomax.tfs.local>
2012-07-10 22:28:47 +0000

    Add model for checked list widget
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C274920

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/CheckedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class CheckedList : IWidgetModel
+  {
+    public CheckedList()
+    {
+      this.Sections = new List<Section>();
+    }
+
+    public List<Section> Sections { get; set; }
+
+    public class Section
+    {
+      public Section()
+      {
+        this.ListItems = new List<ListItem>();
+      }
+
+      public bool Filtered { get; set; }
+      public string Header { get; set; }
+      public List<ListItem> ListItems { get; set; }
+
+      public class ListItem : ElementBase
+      {
+        public string BoldText { get; set; }
+      }
+    }
+  }
+}

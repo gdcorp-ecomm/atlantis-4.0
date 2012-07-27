5db6065da4ef6a7f672f48a1989052de317989b4
dgoldstein <dgoldstein@jomax.tfs.local>
2012-07-27 16:39:19 +0000

    Add tracking pixel widget model
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C278743

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/TrackingPixel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public enum CDSPixelTypes
+  {
+    YahooDomains,
+    YahooHosting,
+    YahooSSL,
+    GoogleAdwords,
+    GoogleAdwordsSearchPage,
+    GoogleAdwordsHomePage,
+    SSLGoogleAdwords,
+    GoogleAdwordsResellerPage,
+    GoogleAdwordsWwdPage,
+    GoogleAdwordsInclude,
+    Bizo,
+    Verisign
+  }
+
+  public class TrackingPixel : IWidgetModel
+  {
+    public List<string> PixelList { get; set; }
+
+    private string _pixelType;
+    public string PixelType
+    {
+      get
+      {
+        return _pixelType;
+      }
+      set
+      {
+        CDSPixelTypes pixelTypes;
+        if (Enum.TryParse<CDSPixelTypes>(value, out pixelTypes))
+        {
+          _pixelType = value;
+        }
+        else
+        {
+          throw new ArgumentException("Invalid pixel type");
+        }
+      }
+    }
+  }
+}

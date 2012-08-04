eeda4898234c8e97d8b0069716a24263291d3698
Sridhar Thota <sthota@godaddy.com>
2012-08-04 18:35:25 +0000

    67189 - MYA-Redesign P2: Order History page and Receipts
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C280377

---
+++ Atlantis.Framework.CommerceOrderXml/Atlantis.Framework.CommerceOrderXml.Interface/CommerceOrderXmlRequestData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.CommerceOrderXml.Interface
+{
+  public class CommerceOrderXmlRequestData : RequestData
+  {
+    #region Properties
+    private string _recentOrderId;
+
+    public string RecentOrderId
+    {
+      get { return _recentOrderId; }
+    }
+
+    #endregion
+    public CommerceOrderXmlRequestData(string shopperId,
+                                  string sourceUrl,
+                                  string orderId,
+                                  string pathway,
+                                  int pageCount,
+                                  string recentOrderId)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      _recentOrderId = recentOrderId;
+    }
+
+    public override string GetCacheMD5()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

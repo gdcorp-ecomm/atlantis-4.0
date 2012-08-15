738e4f40d5fe154959a4a28de2682e7699ff74b7
dgoldstein <dgoldstein@jomax.tfs.local>
2012-08-15 22:01:25 +0000

    Add TldElementBase
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C282734

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/TldElementBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class TldElementBase : ElementBase
+  {
+    public string Tld { get; set; }
+  }
+}

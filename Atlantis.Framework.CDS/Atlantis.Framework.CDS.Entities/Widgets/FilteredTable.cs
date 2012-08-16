a88f8dde288c3bdfa1fb7e108b8f1cc6c01419e2
dgoldstein <dgoldstein@jomax.tfs.local>
2012-08-16 16:07:13 +0000

    Add model for filtered table widget
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C282858

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/FilteredTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class FilteredTable : IWidgetModel
+  {
+    public string Styles { get; set; }
+    public string AboveTableContent { get; set; }
+    public string TableColsMarkup { get; set; }
+    public int NumberOfColumns { get; set; }
+    public List<string> TableHeadRow { get; set; }
+    public List<FilteredTableRow> Rows { get; set; }
+    public bool Filtered { get; set; }
+    public string JavaScript { get; set; }
+    public int? RegistrationType { get; set; }
+
+    public class FilteredTableRow : TldElementBase
+    {
+      public List<string> Row { get; set; }
+    }
+  }
+}

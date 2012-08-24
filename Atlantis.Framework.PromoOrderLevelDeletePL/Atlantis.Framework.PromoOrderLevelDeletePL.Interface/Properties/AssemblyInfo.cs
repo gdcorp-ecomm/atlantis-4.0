3868b1899bb6a54a9f4658e23b718fcb5ab76ec9
nwood <nwood@jomax.tfs.local>
2012-08-24 23:29:35 +0000

    Forgot to update assembly versioning info
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C284975

--- Atlantis.Framework.PromoOrderLevelDeletePL/Atlantis.Framework.PromoOrderLevelDeletePL.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.PromoOrderLevelDeletePL/Atlantis.Framework.PromoOrderLevelDeletePL.Interface/Properties/AssemblyInfo.cs
@@ -6,9 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.PromoOrderLevelDeletePL.Interface")]
-[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+#endif
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("GoDaddy.com, LLC.")]
 [assembly: AssemblyProduct("Atlantis.Framework.PromoOrderLevelDeletePL.Interface")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.12.8.24")]

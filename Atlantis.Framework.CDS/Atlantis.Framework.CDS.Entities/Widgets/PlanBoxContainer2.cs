ac3c0fec15bbafaf86a0eb0b499d1130fe7c96a7
dgoldstein <dgoldstein@jomax.tfs.local>
2012-08-29 20:50:16 +0000

    Update PlanBox Container
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C285737

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/PlanBoxContainer2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class PlanBoxContainer2 : IWidgetModel
+  {
+    public string PlanBoxFormName { get; set; }
+    public string FormActionUrl { get; set; }
+    public string CiCode { get; set; }
+    public string ItemTrackingCode { get; set; }
+    public string ISC { get; set; }
+    public string CrossSellConfig { get; set; }
+    public string Upsell { get; set; }
+    public bool RedirectStraightToCart { get; set; }
+    public bool SkipDomainerCheck { get; set; }
+    public bool UseNewCrossSellPage { get; set; }
+    public bool FromDPP { get; set; }
+    public bool HideInactiveAddOns { get; set; }
+    [JsonIgnore]
+    public List<Widget<IWidgetModel>> Widgets { get; set; }
+  }
+}

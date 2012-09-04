725fd9c3c5d7cdd50185598cc75054cc7aee8c17
jwjones <jwjones@jomax.tfs.local>
2012-09-04 16:47:24 +0000

    Initial HP CDS entities.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C286514

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.HP/Widgets/PanelContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.CDS.HP.Widgets
+{
+  public class PanelContainer
+  {
+    public IList<Panel> PanelList { get; set; }
+    public PanelContainer()
+    {
+      PanelList = new List<Panel>();
+    }
+  }
+
+  public class Panel
+  {
+    public string PanelId { get; set; }
+    public string PanelZoneId { get; set; }
+    public bool IsDefault { get; set; }
+    public string OnLoadQuerystring { get; set; }
+  }
+}

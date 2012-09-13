cd4d5f4c4a044758309bbf35a65459a31899c52d
jxblair <jxblair@jomax.tfs.local>
2012-09-13 21:48:11 +0000

    CDS tokenizer, updated assembly file version
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C288762

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Tokenizer/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Tokenizer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoDaddy.com, Inc.")]
 [assembly: AssemblyProduct("Atlantis.Framework.CDS.Tokenizer")]
-[assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.11.3.11")]
+[assembly: AssemblyFileVersion("4.12.9.13")]

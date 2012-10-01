88b5a752abc05c27b60cd32355753fa9733f23db
jwjones <jwjones@jomax.tfs.local>
2012-10-01 17:10:49 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C292416

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/CustomPod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class CustomPod: IWidgetModel
+  {
+    public string BackgroundColor { get; set; }
+    public string HoverBackgroundColor { get; set; }
+    public string BorderColor { get; set; }
+    public string HoverBorderColor { get; set; }
+    public IList<CustomPodInfo> PodInfoList { get; set; }
+    public CustomPod()
+    {
+      PodInfoList = new List<CustomPodInfo>();
+    }
+  }
+
+  public class CustomPodInfo:IWidgetModel
+  {
+    public string CSS { get; set; }
+    public string Content { get; set; }
+    public string Script { get; set; }
+    public string CountryCodesList { get; set; }
+    public string SplitValue { get; set; }
+  }
+}
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SingleVideoPod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class SingleVideoPod : IWidgetModel
+  {
+    public IList<SingleVideoPodItem> SingleVideoPodItemList { get; set; }
+    public SingleVideoPod()
+    {
+      SingleVideoPodItemList = new List<SingleVideoPodItem>();
+    }
+  }
+
+  public class SingleVideoPodItem : IWidgetModel
+  {
+    public string VideoTitle { get; set; }
+    public string BackgroundImage { get; set; }
+    public string BackgroundColor { get; set; }
+    public string HoverBackgroundColor { get; set; }
+    public string BorderColor { get; set; }
+    public string HoverBorderColor { get; set; }
+    public string VideoMediaId { get; set; }
+    public string VideoLanguage { get; set; }
+    public string CountryCodesList { get; set; }
+    public string SplitValue { get; set; }
+  }
+}

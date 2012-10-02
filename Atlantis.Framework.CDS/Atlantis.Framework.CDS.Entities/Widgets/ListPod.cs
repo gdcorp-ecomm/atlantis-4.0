022eb4cea41d518654d6fe9f82e4e69e88da61ee
prich <prich@jomax.tfs.local>
2012-10-02 16:44:53 +0000

    list pod
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C292735

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/ListPod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class ListPod : IWidgetModel
+  {
+    public string BackgroundColor { get; set; }
+    public string HoverBackgroundColor { get; set; }
+    public string BorderColor { get; set; }
+    public string HoverBorderColor { get; set; }
+    public IList<ListPodInfo> PodInfoList { get; set; }
+    public ListPod()
+    {
+      PodInfoList = new List<ListPodInfo>();
+    }
+  }
+
+  public class ListPodInfo : IWidgetModel
+  {
+    public string PodTitle { get; set; }
+    public IList<ListPodListItem> PodListItems { get; set; }
+    public string CountryCodesList { get; set; }
+    public string SplitValue { get; set; }
+    public string BottomLinkText { get; set; }
+    public string BottomLink { get; set; }
+    public ListPodInfo()
+    {
+      PodListItems = new List<ListPodListItem>();
+    }
+  }
+
+  public class ListPodListItem : IWidgetModel
+  {
+    public string ItemText { get; set; }
+    public string ItemLink { get; set; }
+  }
+
+}

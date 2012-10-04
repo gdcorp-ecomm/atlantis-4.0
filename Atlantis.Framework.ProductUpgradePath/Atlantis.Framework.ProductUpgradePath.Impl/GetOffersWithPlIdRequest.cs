3bb18bdad491b2c65025b8fea65d8d20e21554d8
tbird <tbird@jomax.tfs.local>
2012-10-04 16:38:09 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C293417

---
+++ Atlantis.Framework.ProductUpgradePath/Atlantis.Framework.ProductUpgradePath.Impl/GetOffersWithPlIdRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.ProductUpgradePath.Interface;
+
+namespace Atlantis.Framework.ProductUpgradePath.Impl
+{
+  public class ProductUpgradePathRequest : IRequest
+  {
+   public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      ProductUpgradePathResponseData responseData = null;
+
+      try
+      {
+        
+
+       
+      } 
+    
+      catch (AtlantisException exAtlantis)
+      {
+        responseData = new ProductUpgradePathResponseData(exAtlantis);
+      }
+
+      catch (Exception ex)
+      {
+        responseData = new ProductUpgradePathResponseData(requestData, ex);
+      }
+       
+      return responseData;
+    }
+  }
+}

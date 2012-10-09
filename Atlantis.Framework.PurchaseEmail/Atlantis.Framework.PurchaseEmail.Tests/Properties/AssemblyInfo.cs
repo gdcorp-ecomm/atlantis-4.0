28a33ffce29b65a063adf44f8ea03568481936c0
jojohnson <jojohnson@jomax.tfs.local>
2012-10-09 21:33:06 +0000

    P90396: Update AssemblyInfo of PurchaseEmail triplet
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C294281

--- Atlantis.Framework.PurchaseEmail/Atlantis.Framework.PurchaseEmail.Tests/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.PurchaseEmail/Atlantis.Framework.PurchaseEmail.Tests/Properties/AssemblyInfo.cs
@@ -5,7 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.PurchaseEmail.Tests")]
-[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+#endif
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoDaddy.com, Inc.")]
 [assembly: AssemblyProduct("Atlantis.Framework.PurchaseEmail.Tests")]
@@ -31,4 +35,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.11.10.26")]
+[assembly: AssemblyFileVersion("4.12.10.9")]

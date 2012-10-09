5e87ab43962e02de1fe6c2f8431f19082586290f
dgoldstein <dgoldstein@jomax.tfs.local>
2012-10-09 23:20:56 +0000

    PlanBox3
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C294339

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/PlanBox3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using System.ComponentModel.DataAnnotations;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class PlanBox3 : PlanBox2, IWidgetModel
+  {
+    public string AbovePlanDetailsCustomContent { get; set; }
+
+    [RegularExpression(@"^(\d+,)*\d+$", ErrorMessage="Custom quantities must be a comma-separated list of nonnegative integers")]
+    public string CustomQuantities { get; set; }
+
+    private int[] _customQuantitiesList;
+    public int[] CustomQuantitiesList
+    {
+      get
+      {
+        if (_customQuantitiesList == null)
+        {
+          if (!string.IsNullOrEmpty(CustomQuantities))
+          {
+            string[] temp = CustomQuantities.Split(new char[] { ',' });
+            _customQuantitiesList = Array.ConvertAll(temp, Convert.ToInt32);
+          }
+        }
+        return _customQuantitiesList;
+      }
+    }
+  }
+}

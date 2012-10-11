5caae66e7651d261ccaa19a371b37806da44d24a
jbaziuk <jbaziuk@jomax.tfs.local>
2012-10-11 15:49:49 +0000

    Added a few more entities tests.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C294767

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Tests/EntitiesTest.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Tests/EntitiesTest.cs
@@ -73,18 +73,61 @@ namespace Atlantis.Framework.CDS.Tests
             Assert.AreEqual("test", jsContent.Html,"Html string is null and was not initialized by contructor.");
             Assert.AreEqual("22", jsContent.TargetDivID, "Target div ID is not 22 and was not initialized by contructor.");
         }
-
+        
         /// <summary>
         ///A test for the disclaimer class.
         ///</summary>
         [TestMethod()]
         public void DisclaimerTest()
         {
-            Atlantis.Framework.CDS.Entities.Widgets.Disclaimer.ModalData md = new Atlantis.Framework.CDS.Entities.Widgets.Disclaimer.ModalData();
-            Assert.AreEqual(md.Text, "Click here for product disclaimers and legal policies.", "Default disclaimer text was not correct.");
-          
+            Atlantis.Framework.CDS.Entities.Widgets.Disclaimer md = new Atlantis.Framework.CDS.Entities.Widgets.Disclaimer();
+            Assert.AreEqual(md.CurrentModal.Text, "Click here for product disclaimers and legal policies.", "Default disclaimer text was not correct.");
+
+        }
+        
+          /// <summary>
+        ///A test for the DppFreeExtras class column number.
+        ///</summary>
+        [TestMethod()]
+        public void DppFreeExtrasColumnNumberTest()
+        {
+            Atlantis.Framework.CDS.Entities.Widgets.DPPFreeExtras target = new Atlantis.Framework.CDS.Entities.Widgets.DPPFreeExtras();
+            Assert.AreEqual(target.NumColumns, 2, "Column number is not 2 and it should be.");
         }
 
+        /// <summary>
+        ///A test for List class column number.
+        ///</summary>
+        [TestMethod()]
+        public void ListColumnNumberTest()
+        {
+            Atlantis.Framework.CDS.Entities.Widgets.List target = new Atlantis.Framework.CDS.Entities.Widgets.List();
+            Assert.AreEqual(target.NumColumns, 2, "Column number is not 2 and it should be.");
+        }
+
+        /// <summary>
+        ///A test for the ManageNow class link text and description.
+        ///</summary>
+        [TestMethod()]
+        public void ManageNowTest()
+        {
+            Atlantis.Framework.CDS.Entities.Widgets.ManageNow target = new Atlantis.Framework.CDS.Entities.Widgets.ManageNow();
+            Assert.AreEqual(target.LinkText, "Manage Now", "Link text not correct for the ManageNow class.");
+            Assert.AreEqual(target.Description, "Already own this product?", "Description text not correct for the ManageNow class.");
+        }
+
+        /// <summary>
+        ///A test for the Support class title and community group text.
+        ///</summary>
+        [TestMethod()]
+        public void SupportTest()
+        {
+            Atlantis.Framework.CDS.Entities.Widgets.Support target = new Atlantis.Framework.CDS.Entities.Widgets.Support();
+            Assert.AreEqual(target.Title, "Support", "Title text not correct for Support class.");
+            Assert.AreEqual(target.CommunityGroup, "product group of your choice", "CommunityGroup text not correct for the Support class.");
+        }
+
+     
 
     }
 

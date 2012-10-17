5cef75450cbc73ad5845fabe8e148c69400bc682
dgoldstein <dgoldstein@jomax.tfs.local>
2012-10-17 18:55:31 +0000

    SalesHeader3
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C295792

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesHeader3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class SalesHeader3 : SalesHeader2, IWidgetModel, IContent
+  {
+    public SalesHeader3() { }
+
+    public string Custom99PercentContent { get; set; }
+    public int SubHeadTop { get; set; }
+
+    public List<string> ContentList { get; set; }
+  }
+}

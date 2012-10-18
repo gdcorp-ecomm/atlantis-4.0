8d0799807739c4615cc10a8243b35f4370fed334
jxblair <jxblair@jomax.tfs.local>
2012-10-18 20:49:17 +0000

    89832: CDS Mobile Entities initial checkin
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C296120

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/MobileDomainSearchWidget.cs
@@ -0,0 +1,24 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile
+{
+  class MobileDomainSearchWidget : IWidgetModel
+  {
+    public MobileDomainSearchWidget()
+    {
+      HideBulkDomainSearch = false;
+      SectionGrooveTop = false;
+      ExtraPadding = false;
+      IscCode = string.Empty;
+      SearchActionName = string.Empty;
+      RegistrationLength = 1;
+    }
+
+    public bool HideBulkDomainSearch { get; set; }
+    public bool SectionGrooveTop { get; set; }
+    public bool ExtraPadding { get; set; }
+    public string IscCode { get; set; }
+    public string SearchActionName { get; set; }
+    public int RegistrationLength { get; set; }
+  }
+}
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/MobileSimpleBannerWidget.cs
@@ -0,0 +1,19 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile
+{
+  public class MobileSimpleBannerWidget : IWidgetModel
+  {
+    public MobileSimpleBannerWidget()
+    {
+    }
+
+    public string DefaultBanner { get; set; }
+    public string DefaultBanner { get; set; }
+    public string PromoBanner { get; set; }
+    public string[] PromoBannerCodeList { get; set; }
+
+  }
+}
+
+

095c951c4c33f035695295756bc85717b28a9981
jxblair <jxblair@jomax.tfs.local>
2012-10-19 01:42:47 +0000

    CDS SalesMagicBox added to Entities
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C296218

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/SalesMagicBox.cs
@@ -0,0 +1,16 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  class SalesMagicBox : IWidgetModel
+  {
+    public SalesMagicBox()
+      {
+      }
+
+      public string HeaderText { get; set; }
+      public string CSS { get; set; }
+      public string JavaScript { get; set; }
+      public string Markup { get; set; }
+  }
+}

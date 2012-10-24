2b266bd7bf45e6466e3a1ecdc2524b6221aa052f
caldarondo <caldarondo@jomax.tfs.local>
2012-10-24 22:51:41 +0000

    Add content below plan details box and widgets inside of it
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C297135

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/PlanBox4.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class PlanBox4 : PlanBox3, IWidgetModel
+  {
+    public string BelowPlanDetailsCustomContent { get; set; }
+
+    [JsonIgnore]
+    public List<Widget<IWidgetModel>> Widgets { get; set; }
+  }
+}

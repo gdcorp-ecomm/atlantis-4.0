f5e8a031328aab238c7e066868dd955f87caa73c
caldarondo <caldarondo@jomax.tfs.local>
2012-10-26 22:58:06 +0000

    Project 91761
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C297683

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.SalesTables/Widgets/ComparePlansTable.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.SalesTables/Widgets/ComparePlansTable.cs
@@ -4,8 +4,26 @@ using Atlantis.Framework.CDS.Entities.Widgets;
 
 namespace Atlantis.Framework.CDS.Entities.SalesTables.Widgets
 {
-  public class ComparePlansTable : IWidgetModel
+  public class ComparePlansTable : IWidgetModel, IAddToCartWidgetModel
   {
+
+    public string CiCode { get; set; }
+    public string CrossSellConfig { get; set; }
+    public string FormName { get; set; }
+    public string FormActionUrl { get; set; }
+    public bool FromDPP { get; set; }
+    public string ISC { get; set; }
+    public string ItemTrackingCode { get; set; }
+    public bool RedirectStraightToCart { get; set; }
+    public bool SkipDomainerCheck { get; set; }
+    public string Upsell { get; set; }
+    public bool UseNewCrossSellPage { get; set; }
+    public string XS_AppSettingKey { get; set; }
+    public string XS_ContainerCode { get; set; }
+    public string XS_LaunchCICode { get; set; }
+
+    public int NumberOfColumns { get; set; }
+
     public List<ComparePlansTableColumn> Columns { get; set; }
     public List<ComparePlansTableRow> Rows { get; set; }
     public string JavaScript { get; set; }
@@ -19,8 +37,16 @@ namespace Atlantis.Framework.CDS.Entities.SalesTables.Widgets
 
     public class ComparePlansTableColumn : TldElementBase
     {
+      public ComparePlansTableColumn()
+      {
+        this.ProductOption = new ProductOption();
+      }
+
       public int Pfid { get; set; }
       public bool AddToCartButton { get; set; }
+      public bool OrangeAddToCartButton { get; set; }
+      public ProductOption ProductOption { get; set; }
+
       public string Heading { get; set; }
       public string Styles { get; set; }
     }

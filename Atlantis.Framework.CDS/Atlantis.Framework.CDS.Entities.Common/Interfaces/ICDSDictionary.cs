bc57e688b06afd9030aaef65bdd70fa4aa10ec46
dgoldstein <dgoldstein@jomax.tfs.local>
2012-10-29 19:58:13 +0000

    Add ICDSDictionary
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C297829

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Common/Interfaces/ICDSDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.CDS.Entities.Common.Interfaces
+{
+  public interface ICDSDictionary
+  {
+    Dictionary<string, string> CDSDictionary { get; set; }
+  }
+}

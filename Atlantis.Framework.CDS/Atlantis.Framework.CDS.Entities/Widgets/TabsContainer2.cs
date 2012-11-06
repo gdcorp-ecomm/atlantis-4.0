8337b16e4aefe65faa2051fb0650520ac092931a
dgoldstein <dgoldstein@jomax.tfs.local>
2012-11-06 18:17:11 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299269

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/TabsContainer2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class TabsContainer2 : TabsContainer
+  {
+    public int DefaultTab { get; set; }
+  }
+}

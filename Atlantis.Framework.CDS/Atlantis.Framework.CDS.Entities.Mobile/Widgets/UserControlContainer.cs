71e09d456f9069d73808ed19d226e21c3b750298
Rajendar Vontela <rvontela@godaddy.com>
2012-11-08 17:18:54 +0000

    P93181: MBLE - Added UserControlContainer widger
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299683

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/UserControlContainer.cs
@@ -0,0 +1,9 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class UserControlContainer : IWidgetModel
+  {
+    public string UserControlVirtualPath { get; set; }
+  }
+}

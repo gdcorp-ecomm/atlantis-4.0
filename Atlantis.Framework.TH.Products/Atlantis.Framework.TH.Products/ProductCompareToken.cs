d54760bf6b064fbe9de54214f471d85c0e7a8f7a
caldarondo <caldarondo@jomax.tfs.local>
2012-11-08 19:52:03 +0000

    Base of ProductCompareToken
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299740

---
+++ Atlantis.Framework.TH.Products/Atlantis.Framework.TH.Products/ProductCompareToken.cs
@@ -0,0 +1,13 @@
+﻿using Atlantis.Framework.Tokens.Interface;
+
+namespace Atlantis.Framework.TH.Products
+{
+  class ProductCompareToken : XmlToken
+  {
+
+    public ProductCompareToken(string key, string data, string fullTokenString)
+      : base(key, data, fullTokenString)
+    {
+    }
+  }
+}
---
+++ Atlantis.Framework.TH.Products/Atlantis.Framework.TH.Products/ProductCompareTokenHandler.cs
@@ -0,0 +1,20 @@
+﻿using Atlantis.Framework.Interface;
+using Atlantis.Framework.Tokens.Interface;
+using System.Collections.Generic;
+
+namespace Atlantis.Framework.TH.Products
+{
+  class ProductCompareTokenHandler : XmlTokenHandlerBase
+  {
+    public override string TokenKey
+    {
+      get { return "productcompare"; }
+    }
+    
+    public override TokenEvaluationResult EvaluateTokens(IEnumerable<IToken> tokens, IProviderContainer container)
+    {
+      TokenEvaluationResult result = TokenEvaluationResult.Success;
+      return result;
+    }
+  }
+}

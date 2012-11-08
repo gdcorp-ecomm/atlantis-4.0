d88488d6e284e2dbbdb4fa56e8e87114429b9ef1
caldarondo <caldarondo@jomax.tfs.local>
2012-11-08 20:08:07 +0000

    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299743

--- Atlantis.Framework.TH.Products/Atlantis.Framework.TH.Products/ProductCompareToken.cs
+++ Atlantis.Framework.TH.Products/Atlantis.Framework.TH.Products/ProductCompareToken.cs
@@ -4,10 +4,32 @@ namespace Atlantis.Framework.TH.Products
 {
   class ProductCompareToken : XmlToken
   {
+    public string RenderType
+    {
+      get { return (TokenData != null) ? TokenData.Name.ToString() : string.Empty; }
+    }
+
+    public const string CALCULATE_TIMES = "X";
+    public const string CALCULATE_PERCENT = "%";
+
+    public int PrimaryProductId { get; private set; }
+    public int PrimaryPrice { get; private set; }
+    public int SecondaryProductId { get; private set; }
+    public int SecondaryPrice { get; private set; }
+    public string Calculate { get; private set; }
+    public string Html { get; private set; }
+    public int HideBelow { get; private set; }
 
     public ProductCompareToken(string key, string data, string fullTokenString)
       : base(key, data, fullTokenString)
     {
+      PrimaryProductId = GetAttributeInt("primaryproductid", 0);
+      PrimaryPrice = GetAttributeInt("primaryprice", 0);
+      SecondaryProductId = GetAttributeInt("secondaryproductid", 0);
+      SecondaryPrice = GetAttributeInt("secondaryprice", 0);
+      Calculate = GetAttributeText("calculate", string.Empty);
+      Html = GetAttributeText("html", string.Empty);
+      HideBelow = GetAttributeInt("hidebelow", 0);
     }
   }
 }

651ce83f3fa549adc8d3c529dd5ddf17137e520d
aheuertz <aheuertz@jomax.tfs.local>
2012-11-09 16:52:59 +0000

    Sample shopper validator (only "IsRequired" rules)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299928

--- Atlantis.Framework.RuleEngine/Atlantis.Framework.RuleEngine.Tests/AtlantisRuleEngineTests.cs
+++ Atlantis.Framework.RuleEngine/Atlantis.Framework.RuleEngine.Tests/AtlantisRuleEngineTests.cs
@@ -40,6 +40,75 @@ namespace Atlantis.Framework.RuleEngine.Tests
       
     }
 
+    [TestMethod]
+    [DeploymentItem("ShopperValidation.xml")]
+    public void TestShopperValidation()
+    {
+      var rules = new XmlDocument();
+      Uri pathUri = new Uri(Path.GetDirectoryName(this.GetType().Assembly.CodeBase));
+      var assemblyPath = pathUri.LocalPath;
+      string directory = Path.Combine(assemblyPath, @"ShopperValidation.xml");
+      rules.Load(directory);
+
+      var model = new Dictionary<string, Dictionary<string, string>>();
+
+    //<Fact id="FFirstName" key="txtFirstName" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FLastName" key="txtLastName" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FUserName" key="txtUserName" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FCreatePassword" key="txtCreatePassword" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FCreatePassword2" key="txtCreatePassword2" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FPin" key="txtPin" desc="new value" type="string" modelId="Shopper" />
+    //<Fact id="FEmail" key="txtEmail" desc="new value" type="string" modelId="Shopper" />
+
+      model.Add("Shopper", new Dictionary<string, string>
+                            {
+                              { "txtFirstName", "" },
+                              { "txtLastName", "aasdf" },
+                              { "txtUserName", "asdf" },
+                              { "txtCreatePassword", "asdf" },
+                              { "txtCreatePassword2", "asdf" },
+                              { "txtPin", "asdf" },
+                              { "txtEmail", "asdf" }
+                            });
+
+      var engineResult = RuleEngine.EvaluateRules(model, rules);
+
+      var modelResults = engineResult.ValidationResults;
+      var facts = modelResults.FirstOrDefault(m => m.ModelId == "Shopper");
+
+      Assert.IsTrue(facts != null);
+      Assert.IsTrue(facts.ContainsInvalids);
+
+      //Assert.IsTrue(facts.Facts.ToList().Find(m => m.FactKey == "organization").Status == ValidationResultStatus.InValid);
+
+      foreach (var fact in facts.Facts)
+      {
+
+        switch (fact.FactKey)
+        {
+
+          case "txtFirstName":
+
+            Assert.IsTrue(fact.Status == ValidationResultStatus.InValid);
+
+            Assert.IsTrue(fact.Messages.Count > 0);
+
+            break;
+
+          default:
+
+            Assert.IsTrue(fact.Status == ValidationResultStatus.Valid);
+
+            break;
+
+        }
+
+      }
+
+    }
+
+
+
     //[TestMethod]
     //[DeploymentItem("DotCaRule.xml")]
     //public void TestLegalTypeIsValid()

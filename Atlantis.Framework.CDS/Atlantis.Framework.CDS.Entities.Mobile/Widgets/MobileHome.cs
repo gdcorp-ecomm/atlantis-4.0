291e9cfefb146d33ba5a0e57399c19d7c56a65f6
Rajendar Vontela <rvontela@godaddy.com>
2012-11-09 16:25:27 +0000

    93181: Added mobile home widget model
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C299917

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobileHome.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobileHome : IWidgetModel
+  {
+    public string ButtonText { get; set; }
+    public string ButtonCssClass { get; set; }
+    public List<MobileProductDisclaimerItem> DisclaimerList { get; set; }
+  }
+}

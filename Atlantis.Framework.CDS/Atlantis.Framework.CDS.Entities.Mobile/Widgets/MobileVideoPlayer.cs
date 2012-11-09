d92a3cd23469786b55e8f1e4212878a750651727
Rajendar Vontela <rvontela@godaddy.com>
2012-11-09 22:12:26 +0000

    P93181: Mobile VideoPlayer entity
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300042

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobileVideoPlayer.cs
@@ -0,0 +1,10 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobileVideoPlayer : IWidgetModel
+  {
+    public bool HideVideoHeader { get; set; }
+    public string HeaderContainerId { get; set; }
+  }
+}

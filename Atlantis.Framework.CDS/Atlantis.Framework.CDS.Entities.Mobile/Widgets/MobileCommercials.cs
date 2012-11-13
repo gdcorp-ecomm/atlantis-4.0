44f48aa618526e1fe46bd3623819a5fe2d9617c5
Rajendar Vontela <rvontela@godaddy.com>
2012-11-13 16:05:01 +0000

    P93181: added MobileCommericals entity
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300369

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobileCommercials.cs
@@ -0,0 +1,10 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobileCommercials : IWidgetModel
+  {
+    public bool ShowToolBar { get; set; }
+    public string CustomCommercialList { get; set; }
+  }
+}

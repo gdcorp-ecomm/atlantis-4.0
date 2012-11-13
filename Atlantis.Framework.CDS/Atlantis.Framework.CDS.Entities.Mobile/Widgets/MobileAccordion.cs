ab85aa5974f6b951f29020c2046cc1a8c5eec190
Rajendar Vontela <rvontela@godaddy.com>
2012-11-13 21:25:03 +0000

    P93181: added MobileAccordion entity
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300509

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobileAccordion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Widgets;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobileAccordion : IWidgetModel
+  {
+    public List<MobileAccordDetails> Details { get; set; }
+  }
+
+  public class MobileAccordDetails : ElementBase
+  {
+    //public string Text { get; set; } -> comes from ElementBase
+    public MobileAccordSubDetails SubDetails { get; set; }
+  }
+
+  public class MobileAccordSubDetails : ElementBase
+  {
+    //public string Text { get; set; } -> comes from ElementBase
+    public string Text2 { get; set; }
+  }
+}

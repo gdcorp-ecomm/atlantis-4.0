40e2c9687f72ed595bea0493a8786cf2b4d2343a
Sridhar Thota <sthota@godaddy.com>
2012-11-13 22:36:18 +0000

    91810 - CDS domains sidedoor json Tab content
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300557

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/TabContentJson.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Newtonsoft.Json;
+
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class TabContentJson : IWidgetModel
+  {
+    public TabContentJson()
+    {
+    }
+
+    public string TabHeaderText { get; set; }
+    public string TabSubHeaderText { get; set; }
+    public string TabContainerTemplate { get; set; }
+    public int TabIndex { get; set; }
+    public string TabBottomText { get; set; }
+    public string JsonContentVirtualPath { get; set; }
+    public string KeyValuePairs { get; set; }
+
+    private CurrentTab _tab;
+    public CurrentTab Tab
+    {
+      get
+      {
+        if (_tab == null)
+        {
+          _tab = new CurrentTab();
+        }
+        return _tab;
+      }
+      set { _tab = value; }
+    }
+
+    public class CurrentTab
+    {
+      public string Text { get; set; }
+      public string CiCode { get; set; }
+    }
+  }
+}

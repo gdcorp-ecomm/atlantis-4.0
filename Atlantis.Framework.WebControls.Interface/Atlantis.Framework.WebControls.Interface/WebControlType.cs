81470748119a5d3eac9fad89c5ed4aa83edf1595
pmccormack <pmccormack@jomax.tfs.local>
2012-11-14 22:52:54 +0000

    Initial checkin (support for web control types)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300845

---
+++ Atlantis.Framework.WebControls.Interface/Atlantis.Framework.WebControls.Interface/WebControlType.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Atlantis.Framework.WebControls.Interface
+{
+  public static class WebControlType
+  {
+    public const string INPUT_HIDDEN = "hidden";
+    public const string INPUT_CHECKBOX = "checkbox";
+    public const string INPUT_TEXT = "text";
+    public const string INPUT_BUTTON = "button";
+    public const string INPUT_RADIO = "radio";
+    public const string INPUT_PASSWORD = "password";
+
+    public const string SELECT = "select";
+    public const string TEXT_AREA = "textarea";
+  }
+}

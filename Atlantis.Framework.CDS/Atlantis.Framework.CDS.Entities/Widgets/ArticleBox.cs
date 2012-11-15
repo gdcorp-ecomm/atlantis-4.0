0dd7d3ebb48693a611d7680c8287afab14a6a2da
rflowers <rflowers@jomax.tfs.local>
2012-11-15 22:58:34 +0000

    91810 DPP - CDS Domain Search Side Door
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C301225

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/ArticleBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class ArticleBox : IWidgetModel
+  {
+    public string HeaderText { get; set; }
+    public string ArticleTextBottom { get; set; }
+    public string ArticleTextTop { get; set; }
+    public List<string> CurrentUnorderedList { get; set; }
+    public string JavaScript { get; set; }
+    public string CSS { get; set; }
+    public string Markup { get; set; }
+  }
+}

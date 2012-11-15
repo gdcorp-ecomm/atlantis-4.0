6fe693499b2ff9d224cfe7e02bdc9deb9906f61e
Rajendar Vontela <rvontela@godaddy.com>
2012-11-15 16:24:29 +0000

    Added MobilePromoText entity
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C300967

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobilePromoText.cs
@@ -0,0 +1,10 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobilePromoText : IWidgetModel
+  {
+    public string PromoCodeValue { get; set; }
+    public string PromoTextStyle { get; set; }
+  }
+}

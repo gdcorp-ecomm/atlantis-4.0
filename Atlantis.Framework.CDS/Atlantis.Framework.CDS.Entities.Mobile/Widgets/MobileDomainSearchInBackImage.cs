f743eb71d51cd1a2161c35049eb9b8f53a80c0aa
Rajendar Vontela <rvontela@godaddy.com>
2012-11-15 20:27:58 +0000

    Added MobileDomainSearchInBackimage
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C301108

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Mobile/Widgets/MobileDomainSearchInBackImage.cs
@@ -0,0 +1,11 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Mobile.Widgets
+{
+  public class MobileDomainSearchInBackImage : IWidgetModel
+  {
+    public string BackImageWidth { get; set; }
+    public string BackImageHeight { get; set; }
+    public string DomainSearchMarginTop { get; set; }
+  }
+}

735e7486555f3d4e3bc40bd70bb62537f8b73ca5
caldarondo <caldarondo@jomax.tfs.local>
2012-11-15 21:10:52 +0000

    Project 91384
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C301135

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities/Widgets/ManagerRegionSelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using System.ComponentModel;
+
+namespace Atlantis.Framework.CDS.Entities.Widgets
+{
+  public class ManagerRegionSelector : IWidgetModel
+  {
+    public ManagerRegionSelector()
+    {
+    }
+  }
+}

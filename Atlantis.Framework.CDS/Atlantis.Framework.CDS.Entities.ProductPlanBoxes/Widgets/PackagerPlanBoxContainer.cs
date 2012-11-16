2be075c485590d331f87db0c3b4a23690f639d96
trwalker <trwalker@jomax.tfs.local>
2012-11-16 23:48:05 +0000

    Atlantis.Framework.CDS added new ProductPlanBoxes library and packager plan box container and plan box
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C301574

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/Widgets/PackagerPlanBoxContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes.Widgets
+{
+  public class PackagerPlanBoxContainer : IWidgetModel
+  {
+    public string CiCode { get; set; }
+
+    public string ItemTrackingCode { get; set; }
+
+    public string Isc { get; set; }
+
+    public string CrossSellConfig { get; set; }
+
+    public string Upsell { get; set; }
+
+    public bool RedirectStraightToCart { get; set; }
+
+    public bool SkipDomainerCheck { get; set; }
+
+    public bool UseNewCrossSellPage { get; set; }
+
+    public bool HideInactiveAddOns { get; set; }
+    
+    public string XsellContainerCode { get; set; }
+
+    public string XsellAppSettingKey { get; set; }
+
+    public string XsellLaunchCiCode { get; set; }
+    
+    [JsonIgnore]
+    public List<Widget<IWidgetModel>> Widgets { get; set; }
+  }
+}
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/Widgets/PackagerPlanBoxSimple.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Widgets;
+
+namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes.Widgets
+{
+  public class PackagerPlanBoxSimple : IWidgetModel
+  {
+    public string PackagerProductGroupId { get; set; }
+
+    public string TierAttributeValue { get; set; }
+
+    public int Width { get; set; }
+    
+    public bool DisplayBestValueFlag { get; set; }
+    
+    public bool ShowMonthlyView { get; set; }
+    
+    public string CustomHeaderDurationText { get; set; }
+
+    public string AbovePlanDetailsCustomContent { get; set; }
+
+    public List<PlanBox2.PlanDetail> PlanDetails { get; set; }
+
+    public string AbovePlanFeaturesCustomContent { get; set; }
+
+    public List<PlanBox2.PlanFeature> PlanFeatures { get; set; }
+
+    public string BelowPlanFeaturesCustomContent { get; set; }
+
+    public List<PlanAddOn> PlanAddOns { get; set; }
+
+    public string InputType { get; set; }
+
+    public string BelowPlanBoxCustomContent { get; set; }
+  }
+
+  public class PlanAddOn : ElementBase
+  {
+    public string InputLabel { get; set; }
+
+    public string SelectType { get; set; }
+    
+    public string InputType { get; set; }
+
+    public string AddOnName { get; set; }
+
+    public bool Hide { get; set; }
+  }
+}

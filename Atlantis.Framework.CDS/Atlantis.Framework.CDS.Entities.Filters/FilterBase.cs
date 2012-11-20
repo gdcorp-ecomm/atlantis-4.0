29d5482c53b749f5f79b4b559085588bd96fea3e
dgoldstein <dgoldstein@jomax.tfs.local>
2012-11-20 22:24:31 +0000

    Add filtered quick tour
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C302054

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Filters/FilterBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atlantis.Framework.CDS.Entities.Filters
+{
+    public class FilterBase
+    {
+      public string Filters { get; set; }
+    }
+
+    public class SimpleFilteredItem : FilterBase
+    {
+      public string Text { get; set; }
+    }
+}
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.SalesModals/FilteredQuickTour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Filters;
+
+namespace Atlantis.Framework.CDS.Entities.SalesModals
+{
+    public class FilteredQuickTour : IWidgetModel
+    {
+      public int ProductGroup { get; set; }
+      public string PageTitle { get; set; }
+
+      private string _bodyImageBasePath;
+      public string BodyImageBasePath
+      {
+        get
+        {
+          if (_bodyImageBasePath == default(string))
+          {
+            _bodyImageBasePath = string.Empty;
+          }
+          return _bodyImageBasePath;
+        }
+        set
+        {
+          _bodyImageBasePath = value;
+        }
+      }
+
+      public class QuickTourSlide : FilterBase
+      {
+        public bool HasTextForCenterImage { get; set; }
+        public string HeaderText { get; set; }
+        public SimpleFilteredItem[] CenterTextList { get; set; }
+        public List<BottomContentItem> BottomContentItems { get; set; }
+
+        public class BottomContentItem : SimpleFilteredItem
+        {
+          public string Paragraph { get; set; }
+          public SimpleFilteredItem[] ListItems { get; set; }
+        }
+
+        public List<SimpleFilteredItem> CustomContentItems { get; set; }
+      }
+      public List<QuickTourSlide> Slides { get; set; }
+    }
+}

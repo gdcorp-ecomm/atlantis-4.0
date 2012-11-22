3297ee9f2bec0c6df39f5b01790a4b64a1d52e46
trwalker <trwalker@jomax.tfs.local>
2012-11-22 21:37:07 +0000

    Atlantis.Framework.CDS.Entities.ProductPlanBoxes removed duration from add ons, duration determined by parent product
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C302268

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/PackagerPlanAddOn.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/PackagerPlanAddOn.cs
@@ -1,4 +1,7 @@
-﻿using Atlantis.Framework.CDS.Entities.Widgets;
+﻿using System;
+using System.Collections.Generic;
+using Atlantis.Framework.CDS.Entities.Widgets;
+using Newtonsoft.Json;
 
 namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes
 {
@@ -13,5 +16,33 @@ namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes
     public string AddOnName { get; set; }
 
     public bool Hide { get; set; }
+
+    public int PackagerPlanAddOnType { get; set; }
+
+    public string QuantityValues { get; set; }
+
+    [JsonIgnore]
+    private IList<int> _quantityList;
+    [JsonIgnore]
+    public IList<int> QuantityList
+    {
+      get
+      {
+        if (_quantityList == null)
+        {
+          string[] quantityValuesArray = QuantityValues.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+          _quantityList = new List<int>(quantityValuesArray.Length);
+          foreach (string quantityValue in quantityValuesArray)
+          {
+            int quantity;
+            if (int.TryParse(quantityValue, out quantity))
+            {
+              _quantityList.Add(quantity);
+            }
+          }
+        }
+        return _quantityList;
+      }
+    }
   }
 }
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/PackagerPlanAddOnTypes.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes
+{
+  public static class PackagerPlanAddOnTypes
+  {
+    public const int MultiplePfid = 1;
+    public const int SinglePfidMultipleQuantity = 2;
+  }
+}

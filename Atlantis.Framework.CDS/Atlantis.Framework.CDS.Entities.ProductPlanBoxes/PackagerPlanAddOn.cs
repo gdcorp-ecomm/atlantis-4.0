13f07402b669f6947c95260438a0ebe05ad1b63f
trwalker <trwalker@jomax.tfs.local>
2012-11-23 15:41:09 +0000

    Atlantis.Framework.CDS.Entities.ProductPlanBoxes simplifying packager add on
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C302272

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/PackagerPlanAddOn.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.ProductPlanBoxes/PackagerPlanAddOn.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Atlantis.Framework.CDS.Entities.Widgets;
-using Newtonsoft.Json;
+﻿using Atlantis.Framework.CDS.Entities.Widgets;
 
 namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes
 {
@@ -9,40 +6,12 @@ namespace Atlantis.Framework.CDS.Entities.ProductPlanBoxes
   {
     public string InputLabel { get; set; }
 
-    public string SelectType { get; set; }
-
     public string InputType { get; set; }
+
+    public string SelectType { get; set; }
 
     public string AddOnName { get; set; }
 
     public bool Hide { get; set; }
-
-    public int PackagerPlanAddOnType { get; set; }
-
-    public string QuantityValues { get; set; }
-
-    [JsonIgnore]
-    private IList<int> _quantityList;
-    [JsonIgnore]
-    public IList<int> QuantityList
-    {
-      get
-      {
-        if (_quantityList == null)
-        {
-          string[] quantityValuesArray = QuantityValues.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
-          _quantityList = new List<int>(quantityValuesArray.Length);
-          foreach (string quantityValue in quantityValuesArray)
-          {
-            int quantity;
-            if (int.TryParse(quantityValue, out quantity))
-            {
-              _quantityList.Add(quantity);
-            }
-          }
-        }
-        return _quantityList;
-      }
-    }
   }
 }

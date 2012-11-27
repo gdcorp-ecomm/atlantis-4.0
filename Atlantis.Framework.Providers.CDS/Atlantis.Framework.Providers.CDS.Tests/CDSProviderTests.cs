5c590051ef24d66f81e9493785633206dd7dc98e
prich <prich@jomax.tfs.local>
2012-11-27 21:29:51 +0000

    Added unit test
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C302702

---
+++ Atlantis.Framework.Providers.CDS/Atlantis.Framework.Providers.CDS.Tests/CDSProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Atlantis.Framework.Providers.Interface.CDS;
+using Atlantis.Framework.Providers.Interface.ProviderContainer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atlantis.Framework.CDS.Impl;
+
+namespace Atlantis.Framework.Providers.CDS.Tests
+{
+  [TestClass]
+  public class CDSProviderTests
+  {
+    public CDSProviderTests()
+    {
+      HttpProviderContainer.Instance.RegisterProvider<Atlantis.Framework.Interface.ISiteContext, TestContexts>();
+      HttpProviderContainer.Instance.RegisterProvider<Atlantis.Framework.Interface.IShopperContext, TestContexts>();
+      HttpProviderContainer.Instance.RegisterProvider<ICDSProvider, CDSProvider>();
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    public void GetJson()
+    {
+      CDSRequest request = new CDSRequest();
+      var cdsProvider = HttpProviderContainer.Instance.Resolve<ICDSProvider>();
+      string cdsJson = cdsProvider.GetJSON("content/es/sales/1/homepage/new/default");
+      Assert.IsNotNull(cdsJson);
+    }
+  }
+}

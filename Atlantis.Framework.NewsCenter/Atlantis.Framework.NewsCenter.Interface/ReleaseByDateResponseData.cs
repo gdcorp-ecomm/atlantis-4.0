9b7bf024bca3b463dc432a81a0bd0ee57b062433
Michael Micco <mmicco@godaddy.com>
2012-12-14 20:49:09 +0000

    NewsCenter Framework Item.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C306463

---
+++ Atlantis.Framework.NewsCenter/Atlantis.Framework.NewsCenter.Interface/ReleaseByDateResponseData.cs
@@ -0,0 +1,70 @@
+﻿using Atlantis.Framework.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace Atlantis.Framework.NewsCenter.Interface
+{
+  public class ReleaseByDateResponseData : IResponseData
+  {
+    private AtlantisException _exception = null;
+    private List<NewsRelease> _newsReleases;
+    private Dictionary<string, NewsRelease> _newsReleasesById;
+    private Dictionary<string, NewsRelease> _newsReleasesByUrlPath;
+
+    public ReleaseByDateResponseData(List<NewsRelease> newsReleases)
+    {
+      _newsReleases = newsReleases;
+      _newsReleasesById = new Dictionary<string, NewsRelease>(StringComparer.OrdinalIgnoreCase);
+      _newsReleasesByUrlPath = new Dictionary<string, NewsRelease>(StringComparer.OrdinalIgnoreCase);
+
+      if (newsReleases != null)
+      {
+        foreach (var newsRelease in _newsReleases)
+        {
+          _newsReleasesById[newsRelease.Id] = newsRelease;
+          _newsReleasesByUrlPath[newsRelease.UrlPath] = newsRelease;
+        }
+      }
+    }
+
+    public IEnumerable<NewsRelease> NewsReleases
+    {
+      get { return _newsReleases; }
+    }
+
+    public NewsRelease GetReleaseByUrlPath(string urlPath)
+    {
+      NewsRelease result;
+      if (!_newsReleasesByUrlPath.TryGetValue(urlPath, out result))
+      {
+        result = null;
+      }
+      return result;
+    }
+
+    public NewsRelease GetReleaseById(string id)
+    {
+      NewsRelease result;
+      if (!_newsReleasesById.TryGetValue(id, out result))
+      {
+        result = null;
+      }
+      return result;
+    }
+
+    public ReleaseByDateResponseData(RequestData requestData, Exception ex)
+    {
+      _exception = new AtlantisException(requestData, "ReleaseByDateResponseData.constructor", ex.Message + ex.StackTrace, requestData.ToXML());
+    }
+
+    public string ToXML()
+    {
+      throw new NotImplementedException();
+    }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+  }
+}

9b438d6421b5082000203b55182b49c0c6055a77
Michael Micco <mmicco@godaddy.com>
2012-12-15 02:04:13 +0000

    Added ReleaseByDateAndType request
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C306623

--- Atlantis.Framework.NewsCenter/Atlantis.Framework.NewsCenter.Interface/ReleaseByDateResponseData.cs
+++ Atlantis.Framework.NewsCenter/Atlantis.Framework.NewsCenter.Interface/ReleaseByDateResponseData.cs
@@ -4,67 +4,15 @@ using System.Collections.Generic;
 
 namespace Atlantis.Framework.NewsCenter.Interface
 {
-  public class ReleaseByDateResponseData : IResponseData
+  public class ReleaseByDateResponseData : ReleaseResponseDataBase
   {
-    private AtlantisException _exception = null;
-    private List<NewsRelease> _newsReleases;
-    private Dictionary<string, NewsRelease> _newsReleasesById;
-    private Dictionary<string, NewsRelease> _newsReleasesByUrlPath;
+    public ReleaseByDateResponseData(RequestData requestData, Exception ex)
+      : base(requestData, ex)
+    { }
 
     public ReleaseByDateResponseData(List<NewsRelease> newsReleases)
-    {
-      _newsReleases = newsReleases;
-      _newsReleasesById = new Dictionary<string, NewsRelease>(StringComparer.OrdinalIgnoreCase);
-      _newsReleasesByUrlPath = new Dictionary<string, NewsRelease>(StringComparer.OrdinalIgnoreCase);
+      : base(newsReleases)
+    { }
 
-      if (newsReleases != null)
-      {
-        foreach (var newsRelease in _newsReleases)
-        {
-          _newsReleasesById[newsRelease.Id] = newsRelease;
-          _newsReleasesByUrlPath[newsRelease.UrlPath] = newsRelease;
-        }
-      }
-    }
-
-    public IEnumerable<NewsRelease> NewsReleases
-    {
-      get { return _newsReleases; }
-    }
-
-    public NewsRelease GetReleaseByUrlPath(string urlPath)
-    {
-      NewsRelease result;
-      if (!_newsReleasesByUrlPath.TryGetValue(urlPath, out result))
-      {
-        result = null;
-      }
-      return result;
-    }
-
-    public NewsRelease GetReleaseById(string id)
-    {
-      NewsRelease result;
-      if (!_newsReleasesById.TryGetValue(id, out result))
-      {
-        result = null;
-      }
-      return result;
-    }
-
-    public ReleaseByDateResponseData(RequestData requestData, Exception ex)
-    {
-      _exception = new AtlantisException(requestData, "ReleaseByDateResponseData.constructor", ex.Message + ex.StackTrace, requestData.ToXML());
-    }
-
-    public string ToXML()
-    {
-      throw new NotImplementedException();
-    }
-
-    public AtlantisException GetException()
-    {
-      return _exception;
-    }
   }
 }

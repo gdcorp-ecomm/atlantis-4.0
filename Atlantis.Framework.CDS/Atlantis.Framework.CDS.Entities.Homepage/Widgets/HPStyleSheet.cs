fa1ec515d1495a855035e9d67a7c8625505a628a
prich <prich@jomax.tfs.local>
2012-12-18 19:43:05 +0000

    cds homepage phase 2
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C307165

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Homepage/Widgets/HPStyleSheet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Homepage.Widgets
+{
+  public class HPStyleSheet : IWidgetModel
+  {
+    public string StyleSheetPath { get; set; }
+  }
+}

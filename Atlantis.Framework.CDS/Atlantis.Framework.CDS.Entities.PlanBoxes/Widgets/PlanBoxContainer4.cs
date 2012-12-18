f1105cd6cd42cc08edb17cad2d4ed6b2e234a61f
dgoldstein <dgoldstein@jomax.tfs.local>
2012-12-18 00:10:02 +0000

    PlanBoxContainer4 with Orange Button Text property
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C306975

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.PlanBoxes/Widgets/PlanBoxContainer4.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Newtonsoft.Json;
+
+namespace Atlantis.Framework.CDS.Entities.PlanBoxes.Widgets
+{
+  public class PlanBoxContainer4 : IWidgetModel
+  {
+    public string PlanBoxFormName { get; set; }
+    public string FormActionUrl { get; set; }
+    public string CiCode { get; set; }
+    public string ItemTrackingCode { get; set; }
+    public string ISC { get; set; }
+    public string CrossSellConfig { get; set; }
+    public string Upsell { get; set; }
+    public bool RedirectStraightToCart { get; set; }
+    public bool SkipDomainerCheck { get; set; }
+    public bool UseNewCrossSellPage { get; set; }
+    public bool FromDPP { get; set; }
+    public bool HideInactiveAddOns { get; set; }
+    public string XS_ContainerCode { get; set; }
+    public string XS_AppSettingKey { get; set; }
+    public string XS_LaunchCICode { get; set; }
+
+    private string _orangeButtonText;
+    public string OrangeButtonText
+    {
+      get
+      {
+        if (string.IsNullOrEmpty(_orangeButtonText))
+        {
+          _orangeButtonText = "Add to Cart";
+        }
+        return _orangeButtonText;
+      }
+      set
+      {
+        _orangeButtonText = value;
+      }
+    }
+
+    [JsonIgnore]
+    public List<Widget<IWidgetModel>> Widgets { get; set; }
+  }
+}

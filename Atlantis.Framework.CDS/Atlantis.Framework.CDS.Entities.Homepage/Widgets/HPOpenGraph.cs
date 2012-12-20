5b91cc5702c64bd4968259b5869a35600822352a
prich <prich@jomax.tfs.local>
2012-12-20 19:00:15 +0000

    95463: CDS: Home Page Phase 2: Christmas Magic
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C307759

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Homepage/Widgets/HPOpenGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Homepage.Widgets
+{
+  public class HPOpenGraph : IWidgetModel
+  {
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public string Url { get; set; }
+    public string Image { get; set; }
+    public string SiteName { get; set; }
+    public string Description { get; set; }
+  }
+}

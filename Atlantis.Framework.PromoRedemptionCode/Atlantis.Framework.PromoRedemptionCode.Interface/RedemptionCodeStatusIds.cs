53e31349ac08fd1d772509d91692f7ee7302116c
nthorstrom <nthorstrom@jomax.tfs.local>
2012-12-21 06:09:40 +0000

    85420 - adding status ids
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C307978

---
+++ Atlantis.Framework.PromoRedemptionCode/Atlantis.Framework.PromoRedemptionCode.Interface/RedemptionCodeStatusIds.cs
@@ -0,0 +1,10 @@
+﻿namespace Atlantis.Framework.PromoRedemptionCode.Interface
+{
+  public class RedemptionCodeStatusIds
+  {
+    public const int Available = 1;
+    public const int Redeemed = 2;
+    public const int Revoked = 3;
+    public const int Expired = 4;
+  }
+}

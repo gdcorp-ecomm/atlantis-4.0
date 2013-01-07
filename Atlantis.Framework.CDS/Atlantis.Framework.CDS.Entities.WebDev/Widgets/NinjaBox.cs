9a7925e5999678598db8792319bd022426f5e0ac
jyjones <jyjones@jomax.tfs.local>
2013-01-07 23:13:16 +0000

    Updated NinjaBox to include IWidgetModel
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C310026

--- Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.WebDev/Widgets/NinjaBox.cs
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.WebDev/Widgets/NinjaBox.cs
@@ -1,6 +1,7 @@
-﻿namespace Atlantis.Framework.CDS.Entities.WebDev.Widgets
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+namespace Atlantis.Framework.CDS.Entities.WebDev.Widgets
 {
-  public class NinjaBox
+  public class NinjaBox : IWidgetModel
   {
     public string Content { get; set; }
   }

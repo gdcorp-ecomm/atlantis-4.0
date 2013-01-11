a69e828f4eb72d8a29a8a3f74606d625661f5ac9
dgoldstein <dgoldstein@jomax.tfs.local>
2013-01-11 17:59:33 +0000

    Filtered Magic Box
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C311222

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Content/Widgets/FilteredMagicBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Filters;
+
+namespace Atlantis.Framework.CDS.Entities.Content.Widgets
+{
+  public class FilteredMagicBox : IWidgetModel
+  {
+    public List<SimpleFilteredItem> ContentBlocks { get; set; }
+  }
+}

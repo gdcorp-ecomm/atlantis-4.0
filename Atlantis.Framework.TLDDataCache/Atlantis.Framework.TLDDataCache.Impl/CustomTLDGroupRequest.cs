911d5a414a743ed102b905a3cadeef372dfabee1
Michael Micco <mmicco@godaddy.com>
2013-01-22 02:36:25 +0000

    90577 - Added CustomTLDGroup request and response into TLDDataCache.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C313878

---
+++ Atlantis.Framework.TLDDataCache/Atlantis.Framework.TLDDataCache.Impl/CustomTLDGroupRequest.cs
@@ -0,0 +1,42 @@
+﻿using Atlantis.Framework.Interface;
+using Atlantis.Framework.TLDDataCache.Interface;
+using System;
+
+namespace Atlantis.Framework.TLDDataCache.Impl
+{
+  public class CustomTLDGroupRequest : IRequest
+  {
+    const string _APPSETTINGFORMAT = "CUSTOMTLDGROUP.{0}";
+
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      IResponseData result = null;
+
+      try
+      {
+        CustomTLDGroupRequestData groupRequest = (CustomTLDGroupRequestData)requestData;
+
+        if (string.IsNullOrEmpty(groupRequest.GroupName))
+        {
+          result = CustomTLDGroupResponseData.EmptyGroup;
+        }
+        else
+        {
+          string settingName = string.Format(_APPSETTINGFORMAT, groupRequest.GroupName);
+          string setting = DataCache.DataCache.GetAppSetting(settingName);
+          result = CustomTLDGroupResponseData.FromDelimitedString(setting);
+        }
+      }
+      catch (Exception ex)
+      {
+        string message = ex.Message + ex.StackTrace;
+        string data = requestData.ToXML();
+
+        AtlantisException aex = new AtlantisException(requestData, "CustomTLDGroupRequest.RequestHandler", message, data, ex);
+        result = CustomTLDGroupResponseData.FromException(aex);
+      }
+
+      return result;
+    }
+  }
+}

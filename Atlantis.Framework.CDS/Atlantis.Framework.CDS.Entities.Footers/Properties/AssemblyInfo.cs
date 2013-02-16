04aae8ec0a5d33fe76fa7006582f2056469602b6
agurumurthi <agurumurthi@jomax.tfs.local>
2013-02-16 01:02:22 +0000

    91377 - LP: CDS Accelerate - Reseller Hosting
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C320770

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Footers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Atlantis.Framework.CDS.Entities.Footers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Atlantis.Framework.CDS.Entities.Footers")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c0b95c0f-ad6d-4d6d-8099-4761a04bb841")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("4.13.2.14")]
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Footers/ServerSupport.cs
@@ -0,0 +1,89 @@
+﻿using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlantis.Framework.CDS.Entities.Footers
+{
+  public class ServerSupport : FooterBase, IWidgetModel
+  {
+    private string _title = "Support";
+    public string Title
+    {
+      get { return _title; }
+      set { _title = value; }
+    }
+    public string CustomSupportNumber { get; set; }
+
+    private string _communityGroup;
+    public string CommunityGroup
+    {
+      get
+      {
+        if (string.IsNullOrWhiteSpace(_communityGroup))
+        {
+          _communityGroup = "product group of your choice";
+        }
+        return _communityGroup;
+      }
+      set { _communityGroup = value; }
+    }
+
+    private bool _hideResponseTime;
+    public bool HideResponseTime
+    {
+      get { return _hideResponseTime; }
+      set { _hideResponseTime = value; }
+    }
+
+    private bool _showAdditionalSupportText;
+    public bool ShowAdditionalSupportText
+    {
+      get { return _showAdditionalSupportText; }
+      set { _showAdditionalSupportText = value; }
+    }
+
+    private string _additionalSupportText = string.Empty;
+    public string AdditionalSupportText
+    {
+      get { return _additionalSupportText; }
+      set { _additionalSupportText = value; }
+    }
+
+    public List<Section> Sections { get; set; }
+
+    public class Section : ElementBase
+    {
+      public Section(string title)
+      {
+        Title = title;
+      }
+
+      public List<Column> Columns { get; set; }
+
+      private string _title = string.Empty;
+      public string Title
+      {
+        get { return _title; }
+        set { _title = value; }
+      }
+
+      public class Column
+      {
+        public string ColumnTitle { get; set; }
+        public List<ListItem> ListItems { get; set; }
+      }
+
+      public class ListItem : ElementBase
+      {
+        public ListItem(string text)
+        {
+          Text = text;
+        }
+      }
+    }
+  }
+}

4cb2d9c4ae1a3a038dded91607d26388b41cff56
dgoldstein <dgoldstein@jomax.tfs.local>
2013-02-20 23:14:55 +0000

    Check in missed files
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C321803

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Page/FilteredDisclaimer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Filters;
+
+namespace Atlantis.Framework.CDS.Entities.Page
+{
+  public class FilteredDisclaimer : IWidgetModel
+  {
+    public List<string> Disclaimers { get; set; }
+    public bool HasModal { get; set; }
+
+    private ModalData _currentModal;
+    public ModalData CurrentModal
+    {
+      get
+      {
+        if (_currentModal == null)
+        {
+          _currentModal = new ModalData();
+        }
+        return _currentModal;
+      }
+      set { _currentModal = value; }
+    }
+
+    public class ModalData
+    {
+      public List<SimpleFilteredItem> Symbols { get; set; }
+      public string Url { get; set; }
+
+      private string _text;
+      public string Text
+      {
+        get
+        {
+          if (_text == default(string))
+          {
+            _text = "Click here for product disclaimers and legal policies.";
+          }
+          return _text;
+        }
+        set
+        {
+          _text = value;
+        }
+      }
+
+      public int TargetDivWidth { get; set; }
+    }
+  }
+}
---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.SalesModals/FilteredDisclaimerModal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using Atlantis.Framework.CDS.Entities.Filters;
+
+namespace Atlantis.Framework.CDS.Entities.SalesModals
+{
+  public class FilteredDisclaimerModal : IWidgetModel
+  {
+    public int ProductGroup { get; set; }
+
+    public List<SimpleFilteredItem> DisclaimerItems { get; set; }
+  }
+}

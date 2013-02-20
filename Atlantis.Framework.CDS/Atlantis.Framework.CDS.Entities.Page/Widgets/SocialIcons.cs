d4f98859049d372d73e43c9ef2cdb5fe214648c1
dgoldstein <dgoldstein@jomax.tfs.local>
2013-02-20 22:14:55 +0000

    94407 - Social Icons Widget
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C321774

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Page/Widgets/SocialIcons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+
+namespace Atlantis.Framework.CDS.Entities.Page.Widgets
+{
+  public class SocialIcons : IWidgetModel
+  {
+    public string FacebookUrl { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string ItemType { get; set; }
+    public string Title { get; set; }
+    public string TweetText { get; set; }
+    public string TweetUrl { get; set; }
+    public string TweetRelated { get; set; }
+    public bool UseFacebook { get; set; }
+    public bool UseTwitter { get; set; }
+    public bool UseGooglePlus { get; set; }
+  }
+}

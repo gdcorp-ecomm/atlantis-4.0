d780452f367a1f0badf6ff953307fffae55e3040
Bryan Focht <bxfocht@godaddy.com>
2013-02-25 23:12:34 +0000

    updated AssemblyInfo.cs per atlantis framework standards.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C322764

--- Atlantis.Framework.RCCUpdatePLCategoryData/Atlantis.Framework.RCCUpdatePLCategoryData.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.RCCUpdatePLCategoryData/Atlantis.Framework.RCCUpdatePLCategoryData.Interface/Properties/AssemblyInfo.cs
@@ -6,7 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.RCCUpdatePLCategoryData.Interface")]
-[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+#endif
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Atlantis.Framework.RCCUpdatePLCategoryData.Interface")]
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.13.02.21")]

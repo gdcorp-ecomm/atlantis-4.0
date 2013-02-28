7808134d7ada27d146567e934099867c4729b3e5
Sridhar Thota <sthota@godaddy.com>
2013-02-28 19:01:02 +0000

    Add permissions keys into the framework item itself
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C323666

---
+++ Atlantis.Framework.CRMLynxPermission/Atlantis.Framework.CRMLynxPermission.Interface/CrmLynxPermissionKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlantis.Framework.CRMLynxPermission.Interface
+{
+  public class CrmLynxPermissionKeys
+  {
+    public const string CancelSetupProducts = "CancelSetupProducts";
+    public const string ChangePasswords = "ChangePasswords";
+    public const string RemovePaymentMethods = "RemovePaymentMethods";
+  }
+}

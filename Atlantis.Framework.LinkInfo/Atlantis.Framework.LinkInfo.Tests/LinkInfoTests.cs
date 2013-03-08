4b1590528ed079fde3efaa738b885e46c6bdd1cd
Michael Micco <mmicco@godaddy.com>
2013-03-08 21:53:14 +0000

    Added failing cache-soiling test.  This test will fail until a re-architecture of the linkinfo and linkprovider
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C325786

--- Atlantis.Framework.LinkInfo/Atlantis.Framework.LinkInfo.Tests/LinkInfoTests.cs
+++ Atlantis.Framework.LinkInfo/Atlantis.Framework.LinkInfo.Tests/LinkInfoTests.cs
@@ -4,61 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Atlantis.Framework.LinkInfo.Tests
 {
-  /// <summary>
-  /// Summary description for UnitTest1
-  /// </summary>
   [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("Atlantis.Framework.LinkInfo.Impl.dll")]
   public class LinkInfoTests
   {
-    public LinkInfoTests()
-    {
-      //
-      // TODO: Add constructor logic here
-      //
-    }
-
-    private TestContext testContextInstance;
-
-    /// <summary>
-    ///Gets or sets the test context which provides
-    ///information about and functionality for the current test run.
-    ///</summary>
-    public TestContext TestContext
-    {
-      get
-      {
-        return testContextInstance;
-      }
-      set
-      {
-        testContextInstance = value;
-      }
-    }
-
-    #region Additional test attributes
-    //
-    // You can use the following additional attributes as you write your tests:
-    //
-    // Use ClassInitialize to run code before running the first test in the class
-    // [ClassInitialize()]
-    // public static void MyClassInitialize(TestContext testContext) { }
-    //
-    // Use ClassCleanup to run code after all tests in a class have run
-    // [ClassCleanup()]
-    // public static void MyClassCleanup() { }
-    //
-    // Use TestInitialize to run code before running each test 
-    // [TestInitialize()]
-    // public void MyTestInitialize() { }
-    //
-    // Use TestCleanup to run code after each test has run
-    // [TestCleanup()]
-    // public void MyTestCleanup() { }
-    //
-    #endregion
-
     [TestMethod]
-    [DeploymentItem("atlantis.config")]
     public void LinkInfoBasic()
     {
       LinkInfoRequestData request = new LinkInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 1);
@@ -67,7 +18,6 @@ namespace Atlantis.Framework.LinkInfo.Tests
     }
 
     [TestMethod]
-    [DeploymentItem("atlantis.config")]
     [ExpectedException(typeof(AtlantisException))]
     public void LinkInfoMissingCacheEmpty()
     {
@@ -78,7 +28,6 @@ namespace Atlantis.Framework.LinkInfo.Tests
     }
 
     [TestMethod]
-    [DeploymentItem("atlantis.config")]
     public void LinkInfoMissingCacheEmptyAllowed()
     {
       DataCache.DataCache.ClearInProcessCachedData("GetProcessRequest" + 12.ToString());
@@ -87,6 +36,22 @@ namespace Atlantis.Framework.LinkInfo.Tests
       LinkInfoResponseData response = (LinkInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 12);
       Assert.AreEqual(0, response.Links.Count);
     }
+
+    [TestMethod]
+    public void LinkInfoSoilTheCache()
+    {
+      DataCache.DataCache.ClearInProcessCachedData("GetProcessRequest" + 12.ToString());
+      LinkInfoRequestData request = new LinkInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 1);
+      LinkInfoResponseData response = (LinkInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 12);
+
+      response.Links["SITEURL"] = "www.micco.name";
+
+      LinkInfoRequestData request2 = new LinkInfoRequestData(string.Empty, string.Empty, string.Empty, string.Empty, 0, 1);
+      LinkInfoResponseData response2 = (LinkInfoResponseData)DataCache.DataCache.GetProcessRequest(request, 12);
+
+      Assert.AreNotEqual("www.micco.name", response2.Links["SITEURL"]);
+    }
+
     
 
   }

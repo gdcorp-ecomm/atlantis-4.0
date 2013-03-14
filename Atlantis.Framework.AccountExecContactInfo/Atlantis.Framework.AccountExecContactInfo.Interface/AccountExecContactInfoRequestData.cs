d5345c57e123a3b3fc54ca5b71fcd9114581b854
Kent Searle <ksearle@godaddy.com>
2013-03-14 23:04:55 +0000

    Adding BuildHashFromStrings back
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C327320

--- Atlantis.Framework.AccountExecContactInfo/Atlantis.Framework.AccountExecContactInfo.Interface/AccountExecContactInfoRequestData.cs
+++ Atlantis.Framework.AccountExecContactInfo/Atlantis.Framework.AccountExecContactInfo.Interface/AccountExecContactInfoRequestData.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Security.Cryptography;
 using Atlantis.Framework.Interface;
 
 namespace Atlantis.Framework.AccountExecContactInfo.Interface
@@ -18,13 +17,7 @@ namespace Atlantis.Framework.AccountExecContactInfo.Interface
 
     public override string GetCacheMD5()
     {
-      MD5 oMD5 = new MD5CryptoServiceProvider();
-      oMD5.Initialize();
-
-      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(ShopperID);
-      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
-      string sValue = BitConverter.ToString(md5Bytes, 0);
-      return sValue.Replace("-", "");
+      return BuildHashFromStrings(ShopperID);
     }
   }
 }

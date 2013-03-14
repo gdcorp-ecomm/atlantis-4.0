13862a55de93c338b2c203789763034738a85e36
Kent Searle <ksearle@godaddy.com>
2013-03-14 21:54:48 +0000

    Revert MD5 for PROD
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C327293

--- Atlantis.Framework.AccountExecContactInfo/Atlantis.Framework.AccountExecContactInfo.Interface/AccountExecContactInfoRequestData.cs
+++ Atlantis.Framework.AccountExecContactInfo/Atlantis.Framework.AccountExecContactInfo.Interface/AccountExecContactInfoRequestData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using Atlantis.Framework.Interface;
 
 namespace Atlantis.Framework.AccountExecContactInfo.Interface
@@ -17,7 +18,13 @@ namespace Atlantis.Framework.AccountExecContactInfo.Interface
 
     public override string GetCacheMD5()
     {
-      return BuildHashFromStrings(ShopperID);
+      MD5 oMD5 = new MD5CryptoServiceProvider();
+      oMD5.Initialize();
+
+      byte[] stringBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(ShopperID);
+      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
+      string sValue = BitConverter.ToString(md5Bytes, 0);
+      return sValue.Replace("-", "");
     }
   }
 }

44d4937326d799090864f253007041a141f32006
tbird <tbird@jomax.tfs.local>
2013-03-20 16:43:48 +0000

    Project 99290
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C328498

--- Atlantis.Framework.EcommValidPaymentType/Atlantis.Framework.EcommValidPaymentType.Interface/EcommValidPaymentTypeRequestData.cs
+++ Atlantis.Framework.EcommValidPaymentType/Atlantis.Framework.EcommValidPaymentType.Interface/EcommValidPaymentTypeRequestData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Atlantis.Framework.Interface;
+using System.Xml;
+using System.IO;
 
 namespace Atlantis.Framework.EcommValidPaymentType.Interface
 {
@@ -13,12 +15,47 @@ namespace Atlantis.Framework.EcommValidPaymentType.Interface
     /// This is the transactional currency the cart has in the cart xml
     /// </summary>
     public string TransactionalCurrencyType { get; set; }
+    public string SelectedCountry { get; set; }
+    public string RestrictedPaymentTypes { get; set; }
+    public int ShopperPaymentTypeID { get; set; }
 
     public EcommValidPaymentTypeRequestData(string shopperId, string sourceUrl, string orderId, string pathway, int pageCount, string basketType, string transactionalCurrencyType) : base(shopperId, sourceUrl, orderId, pathway, pageCount)
     {
       BasketType = basketType;
       TransactionalCurrencyType = transactionalCurrencyType;
       RequestTimeout = _defaultRequestTimeout;
+      ShopperPaymentTypeID = -1;
+    }
+
+    public EcommValidPaymentTypeRequestData(string shopperId, string sourceUrl, string orderId, string pathway, int pageCount, string basketType, string transactionalCurrencyType,string selectedCountry)
+      : base(shopperId, sourceUrl, orderId, pathway, pageCount)
+    {
+      BasketType = basketType;
+      TransactionalCurrencyType = transactionalCurrencyType;
+      SelectedCountry = selectedCountry;
+      RequestTimeout = _defaultRequestTimeout;
+      ShopperPaymentTypeID = -1;
+    }
+
+    public override string ToXML()
+    {
+      System.Text.StringBuilder responseXML = new System.Text.StringBuilder(100);
+      XmlTextWriter customXmlWriter = new XmlTextWriter(new StringWriter(responseXML));
+      customXmlWriter.WriteStartElement("GetActivePaymentTypes");
+      customXmlWriter.WriteStartElement("FilterBy");
+      customXmlWriter.WriteAttributeString("currency", TransactionalCurrencyType);
+      customXmlWriter.WriteAttributeString("countryCode", SelectedCountry);
+      if (!string.IsNullOrEmpty(RestrictedPaymentTypes))
+      {
+        customXmlWriter.WriteAttributeString("restrictedPaymentType", RestrictedPaymentTypes);
+      }
+      if (ShopperPaymentTypeID != -1)
+      {
+        customXmlWriter.WriteAttributeString("shopperPaymentTypeID", ShopperPaymentTypeID.ToString());
+      }
+      customXmlWriter.WriteEndElement();
+      customXmlWriter.WriteEndElement();
+      return responseXML.ToString();
     }
 
     public override string GetCacheMD5()

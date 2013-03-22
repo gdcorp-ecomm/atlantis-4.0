e6c42510668f049f629376e00039fdf9f1ed033f
dgoldstein <dgoldstein@jomax.tfs.local>
2013-03-22 14:31:49 +0000

    99600 - CDS - Convert Robots.txt to use the CDSM instead of a flat file
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C329190

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Page/Widgets/Robots.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.CDS.Entities.Common.Interfaces;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace Atlantis.Framework.CDS.Entities.Page.Widgets
+{
+  public class Robots : IWidgetModel
+  {
+    public List<Section> Sections { get; set; }
+  }
+
+  public class Section
+  {
+    [JsonProperty("User-agent")]
+    [BsonElement("User-agent")]
+    public string UserAgent { get; set; }
+    public List<string> Disallow { get; set; }
+  }
+}

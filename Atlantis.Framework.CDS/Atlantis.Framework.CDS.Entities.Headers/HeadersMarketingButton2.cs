217909ff3dbee129cd476eb0c96a0a1f6929272e
Jeffrey H. Hirsch <jhirsch@godaddy.com>
2013-04-24 17:21:13 +0000

    Project 100171
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C336782

---
+++ Atlantis.Framework.CDS/Atlantis.Framework.CDS.Entities.Headers/HeadersMarketingButton2.cs
@@ -0,0 +1,14 @@
+﻿namespace Atlantis.Framework.CDS.Entities.Headers.Widgets
+{
+  public class HeadersMarketingButton2
+  {
+    public string LargeText { get; set; }
+    public string SmallText { get; set; }
+    public bool IsMainButton { get; set; }
+    public bool IsAddToCartButton { get; set; }
+    public bool IsOrange { get; set; }
+    public string CICode { get; set; }
+    public bool RequireLogin { get; set; }
+    public string RedirectModalContent { get; set; }
+  }
+}

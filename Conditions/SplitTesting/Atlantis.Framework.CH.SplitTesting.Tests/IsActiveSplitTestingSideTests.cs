201688a29fa7158bfb5efd419f3a7e1dff665a33
Rajendar Vontela <rvontela@godaddy.com>
2013-04-30 16:05:06 +0000

    ATLANTIS-8 - New Split Provider Framework (Created the condition handler for CDS usage)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C338004

---
+++ Conditions/SplitTesting/Atlantis.Framework.CH.SplitTesting.Tests/IsActiveSplitTestingSideTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.Containers;
+using Atlantis.Framework.Providers.SplitTesting;
+using Atlantis.Framework.Providers.SplitTesting.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.CH.SplitTesting.Tests
+{
+  [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("Atlantis.Framework.SplitTesting.Impl.dll")]
+  public class IsActiveSplitTestingSideTests
+  {
+    private bool _conditionHandlersRegistered;
+
+    [TestInitialize]
+    public void InitializeTests()
+    {
+      if (!_conditionHandlersRegistered)
+      {
+        ConditionHandlerManager.AutoRegisterConditionHandlers(Assembly.GetExecutingAssembly());
+        ConditionHandlerManager.RegisterConditionHandler(new IsActiveSplitTestingSideConditionHandler());
+        _conditionHandlersRegistered = true;
+      }
+    }
+
+    private IProviderContainer _objectProviderContainer;
+    private IProviderContainer ObjectProviderContainer
+    {
+      get
+      {
+        if (_objectProviderContainer == null)
+        {
+          _objectProviderContainer = new ObjectProviderContainer();
+          _objectProviderContainer.RegisterProvider<ISplitTestingProvider, SplitTestingProvider>();
+        }
+
+        return _objectProviderContainer;
+      }
+    }
+
+    [TestMethod]
+    public void EvaluateValidConditionFalse()
+    {
+      Assert.IsFalse(ConditionHandlerManager.EvaluateCondition("isActiveSplitTestingSide", new[] { "9999998989", "A" }, ObjectProviderContainer));
+    }
+
+    [TestMethod]
+    public void EvaluateValidConditionInvalidFalse()
+    {
+      Assert.IsFalse(ConditionHandlerManager.EvaluateCondition("isActiveSplitTestingSide", new[] { "123Abc", "A" }, ObjectProviderContainer));
+    }
+  }
+}
---
+++ Conditions/SplitTesting/Atlantis.Framework.CH.SplitTesting/IsActiveSplitTestingSideConditionHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.SplitTesting.Interface;
+
+namespace Atlantis.Framework.CH.SplitTesting
+{
+  public class IsActiveSplitTestingSideConditionHandler : IConditionHandler
+  {
+    public string ConditionName
+    {
+      get { return "isActiveSplitTestingSide"; }
+    }
+
+    public bool EvaluateCondition(string conditionName, IList<string> parameters, IProviderContainer providerContainer)
+    {
+      var evaluationResult = false;
+
+      if (parameters.Count == 2)
+      {
+        try
+        {
+          var splitTestId = parameters[0];
+          var splitTestSide = parameters[1];
+
+          var splitTestingProvider = providerContainer.Resolve<ISplitTestingProvider>();
+          var side = splitTestingProvider.GetSplitTestingSide(Convert.ToInt32(splitTestId));
+
+          if (!string.IsNullOrEmpty(side))
+          {
+            evaluationResult = string.Equals(splitTestSide, side, StringComparison.OrdinalIgnoreCase);
+          }
+        }
+        catch (Exception ex)
+        {
+          LogError(GetType().Name, ex);
+        }
+      }
+
+      return evaluationResult;
+    }
+
+    private static void LogError(string methodName, Exception ex)
+    {
+      try
+      {
+        if (ex.GetType() != typeof(ThreadAbortException))
+        {
+          var message = ex.Message + Environment.NewLine + ex.StackTrace;
+          var source = methodName;
+          var aex = new AtlantisException(source, "0", message, string.Empty, null, null);
+          Engine.Engine.LogAtlantisException(aex);
+        }
+      }
+      catch (Exception) { }
+    }
+
+  }
+}

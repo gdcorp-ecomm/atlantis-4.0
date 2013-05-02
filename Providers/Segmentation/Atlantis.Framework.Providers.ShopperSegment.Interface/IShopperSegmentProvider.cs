c0d14ffe375037c85e3424a413d548129260f6b7
sywilliams <sywilliams@jomax.tfs.local>
2013-05-02 16:48:54 +0000

    Shopper Segmentation
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C338687

---
+++ Providers/Segmentation/Atlantis.Framework.Providers.ShopperSegment.Interface/IShopperSegmentProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlantis.Framework.Providers.ShopperSegment.Interface
+{
+  public interface IShopperSegmentProvider
+  {
+    IEnumerable<int> GetShopperSegmentIds();
+
+    int GetShopperSegmentId();
+  }
+}
---
+++ Providers/Segmentation/Atlantis.Framework.Providers.ShopperSegment/ShopperSegmentProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.ShopperSegment.Interface;
+
+namespace Atlantis.Framework.Providers.ShopperSegment
+{
+  public class ShopperSegmentProvider : ProviderBase, IShopperSegmentProvider
+  {
+    private readonly ISiteContext _siteContext;
+    private readonly IShopperContext _shopperContext;
+
+    protected ShopperSegmentProvider(IProviderContainer container)
+      : base(container)
+    {
+      _siteContext = Container.Resolve<ISiteContext>();
+      _shopperContext = Container.Resolve<IShopperContext>();
+    }
+
+    private IShopperContext ShopperContext
+    {
+      get { return _shopperContext; }
+    }
+
+    public IEnumerable<int> GetShopperSegmentIds()
+    {
+      // TODO: Get information from the triplet for the shopper segment ids
+      throw new NotImplementedException();
+    }
+
+    public int GetShopperSegmentId()
+    {
+      int returnValue = -1;
+
+      // TODO: Get information from the triplet for the shopper segment id
+      //return QaSpoofable.GetAppSetting(Container.Resolve<ISiteContext>(), "SEGMENTATION_VALUE");
+
+      return returnValue;
+    }
+
+    public static string GetAppSetting(ISiteContext siteContext, string appSettingName)
+    {
+      string returnValue = string.Empty;
+
+      if (siteContext.IsRequestInternal)
+      {
+        string spoofParam = "QA--" + appSettingName;
+        string spoofValue = HttpContext.Current.Request[spoofParam];
+        if (!string.IsNullOrEmpty(spoofValue))
+        {
+          returnValue = spoofValue;
+        }
+      }
+      return returnValue;
+    }
+  }
+}

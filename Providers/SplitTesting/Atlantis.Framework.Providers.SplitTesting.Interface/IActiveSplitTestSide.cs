adfa9e602d988660d4d88b87fa2c2f108e2440df
Rajendar Vontela <rvontela@godaddy.com>
2013-05-06 22:39:34 +0000

    ATLANTIS-8 - New Split Provider Framework (removed unused using stmts)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C339420

--- Providers/SplitTesting/Atlantis.Framework.Providers.SplitTesting.Interface/IActiveSplitTestSide.cs
+++ Providers/SplitTesting/Atlantis.Framework.Providers.SplitTesting.Interface/IActiveSplitTestSide.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Atlantis.Framework.Providers.SplitTesting.Interface
+﻿namespace Atlantis.Framework.Providers.SplitTesting.Interface
 {
   public interface IActiveSplitTestSide
   {

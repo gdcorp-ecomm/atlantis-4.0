045c98b14ba85ccd7f6aad8ecc923cd3e4527eea
sywilliams <sywilliams@jomax.tfs.local>
2013-05-14 22:26:07 +0000

    100986 - Segmentation Test: WSB v7 LP
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C340555

---
+++ Triplets/Segmentation/Atlantis.Framework.Segmentation.Interface/ShopperSegmentResponseData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.SessionCache;
+
+namespace Atlantis.Framework.Segmentation.Interface
+{
+  public class ShopperSegmentResponseData : IResponseData, ISessionSerializableResponse
+  {
+    private AtlantisException _exception = null;
+
+    public ShopperSegmentResponseData()
+    {
+    }
+    
+    public ShopperSegmentResponseData(int segmentId)
+    {
+      this.SegmentId = segmentId;
+    }
+
+    public ShopperSegmentResponseData(AtlantisException atlantisException)
+    {
+      this._exception = atlantisException;
+    }
+
+    public ShopperSegmentResponseData(RequestData requestData, Exception exception)
+    {
+      this._exception = new AtlantisException(requestData, "ShopperSegmentResponseData", exception.Message, requestData.ToXML());
+    }
+
+    public bool IsSuccess
+    {
+      get
+      {
+        return null == _exception;
+      }
+    }
+    
+    public int SegmentId { get; set; }
+
+    public AtlantisException GetException()
+    {
+      return _exception;
+    }
+
+    public string ToXML()
+    {
+      XmlSerializer xmlSerializer = new XmlSerializer(typeof(ShopperSegmentResponseData));
+      StringWriter writer = new StringWriter();
+
+      xmlSerializer.Serialize(writer, this);
+
+      string returnValue = writer.ToString();
+
+      return returnValue;
+    }
+
+
+    public string SerializeSessionData()
+    {
+      return this.ToXML();
+    }
+
+    public void DeserializeSessionData(string sessionData)
+    {
+      if (!string.IsNullOrEmpty(sessionData))
+      {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(ShopperSegmentResponseData));
+        StringReader reader = new StringReader(sessionData);
+        ShopperSegmentResponseData temp = xmlSerializer.Deserialize(reader) as ShopperSegmentResponseData;
+        if (!ReferenceEquals(null, temp))
+        {
+          this.SegmentId = temp.SegmentId;
+        }
+      }
+    }
+  }
+}
---
+++ Triplets/Segmentation/Atlantis.Framework.Segmentation.Test/RequestShopperSegmentTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Web;
+using Atlantis.Framework.Segmentation.Interface;
+using Atlantis.Framework.Testing.MockHttpContext;
+using Atlantis.Framework.Testing.MockProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.ShopperSegment.Test
+{
+  [TestClass]
+  public class RequestShopperSegmentTests
+  {
+    private const int REQUEST_TYPE_ID = 686;
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    [TestMethod]
+    [DeploymentItem("atlantis.config")]
+    [DeploymentItem("Atlantis.Framework.Segmentation.Impl.dll")]
+    public void GetShopperSegmentTest()
+    {
+      ShopperSegmentRequestData request = new ShopperSegmentRequestData("1001206", string.Empty, string.Empty, string.Empty, 0);
+      ShopperSegmentResponseData actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("1001232", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("1000534", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("100075", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("100019", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("1001708", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("100147", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreNotEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("1001909", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreEqual(0, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("100090", string.Empty, string.Empty, string.Empty, 0);
+      actual = (ShopperSegmentResponseData)Engine.Engine.ProcessRequest(request, REQUEST_TYPE_ID);
+      Assert.IsTrue(actual.IsSuccess);
+      Assert.AreEqual(0, actual.SegmentId);
+
+    }
+
+    [TestMethod]
+    public void GetShopperSegmentFromSessionTest()
+    {
+      HttpWorkerRequest mockHttpRequest = new MockHttpRequest("http://www.debug.godaddy-com.ide/");
+      MockHttpContext.SetFromWorkerRequest(mockHttpRequest);
+
+      ShopperSegmentRequestData request = new ShopperSegmentRequestData("1001206", string.Empty, string.Empty, string.Empty, 0);
+      ShopperSegmentResponseData actual = SessionCache.SessionCache.GetProcessRequest<ShopperSegmentResponseData>(request, REQUEST_TYPE_ID, TimeSpan.FromMinutes(5));
+      Assert.AreEqual(2, actual.SegmentId);
+
+      request = new ShopperSegmentRequestData("1001206", string.Empty, string.Empty, string.Empty, 0);
+      actual = SessionCache.SessionCache.GetProcessRequest<ShopperSegmentResponseData>(request, REQUEST_TYPE_ID, TimeSpan.FromMinutes(5));
+      Assert.AreEqual(2, actual.SegmentId);
+
+
+    }
+  }
+}

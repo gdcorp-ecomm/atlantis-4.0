97796dbe26565d13348477d4ae087b20ec2b2b23
Michael Micco <mmicco@godaddy.com>
2013-05-23 16:13:58 +0000

    Initial Products Triplet with functionality needed for DataCache Spring Cleaning 2013
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C342224

---
+++ Triplets/Products/Atlantis.Framework.Products.Impl/NonUnifiedPfidRequest.cs
@@ -0,0 +1,33 @@
+﻿using Atlantis.Framework.DataCacheService;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Products.Interface;
+using System;
+
+namespace Atlantis.Framework.Products.Impl
+{
+  public class NonUnifiedPfidRequest : IRequest
+  {
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      IResponseData result = NonUnifiedPfidResponseData.NotFound;
+
+      try
+      {
+        var nonUnifiedRequest = (NonUnifiedPfidRequestData)requestData;
+        int pfid = 0;
+        using (GdDataCacheOutOfProcess outCache = GdDataCacheOutOfProcess.CreateDisposable())
+        {
+          pfid = outCache.ConvertToPFID(nonUnifiedRequest.UnifiedProductId, nonUnifiedRequest.PrivateLabelId);
+        }
+
+        result = NonUnifiedPfidResponseData.FromNonUnifiedPfid(pfid);
+      }
+      catch (Exception ex)
+      {
+        AtlantisException exception = new AtlantisException(requestData, "NonUnifiedPfidRequest.RequestHandler", ex.Message + ex.StackTrace, requestData.ToXML());
+      }
+
+      return result;
+    }
+  }
+}

7f6a228ff072a25f98a46ba1fb68c92809eb072e
Rajendar Vontela <rvontela@godaddy.com>
2013-05-23 20:00:31 +0000

    Added the tests for AddToShopperWatchList & RemoveFromShopperWatchList
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C342317

---
+++ Triplets/DotTypeEoi/Atlantis.Framework.DotTypeEoi.Tests/AddToShopperWatchListTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Atlantis.Framework.DotTypeEoi.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.DotTypeEoi.Tests
+{
+  [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("Atlantis.Framework.DotTypeEoi.Impl.dll")]
+  public class AddToShopperWatchListTests
+  {
+    [TestMethod]
+    public void AddToShopperWatchListGoodRequest()
+    {
+      var gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new AddToShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, "2013-05-23 10:29:55", gTlds);
+      var response = (AddToShopperWatchListResponseData)Engine.Engine.ProcessRequest(request, 704);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ResponseMessage));
+    }
+
+    [TestMethod]
+    public void AddToShopperWatchListBadRequest()
+    {
+      try
+      {
+        var request = new AddToShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, string.Empty, string.Empty);
+      }
+      catch (Exception)
+      {
+        Assert.AreEqual(1, 1);        
+      }
+    }
+
+    [TestMethod]
+    public void AddToShopperWatchListRequestToXml()
+    {
+      const string gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new AddToShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, "2013-05-23 10:29:55", gTlds);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(request.ToXML()));
+    }
+
+    [TestMethod]
+    public void AddToShopperWatchListResponseToXml()
+    {
+      const string gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new AddToShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, "2013-05-23 10:29:55", gTlds);
+      var response = (AddToShopperWatchListResponseData)Engine.Engine.ProcessRequest(request, 704);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ToXML()));
+    }
+
+  }
+}
---
+++ Triplets/DotTypeEoi/Atlantis.Framework.DotTypeEoi.Tests/RemoveFromShopperWatchListTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Atlantis.Framework.DotTypeEoi.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.DotTypeEoi.Tests
+{
+  [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("Atlantis.Framework.DotTypeEoi.Impl.dll")]
+  public class RemoveFromShopperWatchListTests
+  {
+    [TestMethod]
+    public void RemoveFromShopperWatchListGoodRequest()
+    {
+      var gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new RemoveFromShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, gTlds);
+      var response = (RemoveFromShopperWatchListResponseData)Engine.Engine.ProcessRequest(request, 705);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ResponseMessage));
+    }
+
+    [TestMethod]
+    public void RemoveFromShopperWatchListBadRequest()
+    {
+      try
+      {
+        var request = new RemoveFromShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, string.Empty);
+      }
+      catch (Exception)
+      {
+        Assert.AreEqual(1, 1);
+      }
+    }
+
+    [TestMethod]
+    public void RemoveFromShopperWatchListRequestToXml()
+    {
+      const string gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new RemoveFromShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, gTlds);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(request.ToXML()));
+    }
+
+    [TestMethod]
+    public void RemoveFromShopperWatchListResponseToXml()
+    {
+      const string gTlds = @"<gTlds><gTld id='18' gTldSubCategoryId='234' /></gTlds>";
+      var request = new RemoveFromShopperWatchListRequestData("861126", string.Empty, string.Empty, string.Empty, 0, gTlds);
+      var response = (RemoveFromShopperWatchListResponseData)Engine.Engine.ProcessRequest(request, 705);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ToXML()));
+    }
+  }
+}

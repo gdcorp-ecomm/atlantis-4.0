6063b92adb3e21a9503414157ad0b655ee4d7258
cklosowski:88 <cklosowski:88@jomax.tfs.local>
2013-06-03 19:14:27 +0000

    Udating test cases
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C343768

--- Atlantis.Framework.BazaarVoiceAPI/Atlantis.Framework.BazaarVoiceAPI.Tests/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.BazaarVoiceAPI/Atlantis.Framework.BazaarVoiceAPI.Tests/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.13.1.2")]
-[assembly: AssemblyFileVersion("4.13.1.2")]
+[assembly: AssemblyVersion("4.13.5.31")]
+[assembly: AssemblyFileVersion("4.13.5.31")]
 
 #if DEBUG
 [assembly: AssemblyDescription("Debug Build")]

36663adb8d6db9b101088b24ba0310827f23a542
trwalker <trwalker@jomax.tfs.local>
2013-06-12 22:57:39 +0000

    Atlantis.Framework.Web.CDSContent update to base page controller to simplify injection content wire up
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C345811

--- Web/CDSContent/Atlantis.Framework.Web.CDSContent/CDSContentPageControllerBase.cs
+++ Web/CDSContent/Atlantis.Framework.Web.CDSContent/CDSContentPageControllerBase.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Web.UI;
 using Atlantis.Framework.CDS.Interface;
 using Atlantis.Framework.Interface;
 using Atlantis.Framework.Providers.CDSContent.Interface;
+using Atlantis.Framework.Providers.PlaceHolder.Interface;
+using Atlantis.Framework.Render.ContentInjection;
+using Atlantis.Framework.Render.ContentInjection.RenderHandlers;
 using Atlantis.Framework.Render.Pipeline.Interface;
 using Atlantis.Framework.Web.RenderPipeline;
 
@@ -10,6 +15,8 @@ namespace Atlantis.Framework.Web.CDSContent
 {
   public abstract class CDSContentPageControllerBase : RenderPipelineBasePage
   {
+    private static readonly IList<string> _emptyUserControlPathList = new List<string>(0);
+ 
     private ICDSContentProvider _cdsContentProvider;
     protected ICDSContentProvider CdsContentProvider
     {
@@ -21,6 +28,22 @@ namespace Atlantis.Framework.Web.CDSContent
     protected abstract string ApplicationName { get; }
 
     protected abstract IProviderContainer ProviderContainer { get; }
+
+    protected abstract IList<IRenderHandler> RenderHandlers { get; }
+
+    protected virtual IList<string> HeadBeginUserControlPaths { get { return _emptyUserControlPathList; } }
+
+    protected virtual IList<string> HeadEndUserControlPaths { get { return _emptyUserControlPathList; } }
+
+    protected virtual IList<string> BodyBeginUserControlPaths { get { return _emptyUserControlPathList; } }
+
+    protected virtual IList<string> BodyEndUserControlPaths { get { return _emptyUserControlPathList; } }
+
+    private bool _useInjectionRenderHandler;
+    protected virtual bool UseInjectionRenderHandler
+    {
+      get { return _useInjectionRenderHandler; }
+    }
 
     private void ProcessContent()
     {
@@ -53,7 +76,67 @@ namespace Atlantis.Framework.Web.CDSContent
     private bool WhiteListCheck()
     {
       IWhitelistResult whitelistResult = CdsContentProvider.CheckWhiteList(ApplicationName, DocumentRoute);
+      _useInjectionRenderHandler = whitelistResult.UrlData.Style.Equals("f", StringComparison.OrdinalIgnoreCase);
+
       return HandleWhiteListResult(whitelistResult);
+    }
+
+    private void ConfigureRenderPipeline()
+    {
+      ConfigureContentInjectionRenderHandler();
+      AddRenderHandlers(RenderHandlers);
+    }
+
+    private void ConfigureContentInjectionRenderHandler()
+    {
+      if (UseInjectionRenderHandler)
+      {
+        IPlaceHolderProvider placeHolderProvider = ProviderContainer.Resolve<IPlaceHolderProvider>();
+
+        IList<IContentInjectionItem> contentInjectionItems = new List<IContentInjectionItem>(4);
+
+        if (HeadBeginUserControlPaths != null && HeadBeginUserControlPaths.Count > 0)
+        {
+          string headBeginMarkup = BuildInjectionItemMarkup(placeHolderProvider, HeadBeginUserControlPaths);
+          contentInjectionItems.Add(new HtmlHeadBeginContentInjectionItem(headBeginMarkup));
+        }
+
+        if (HeadEndUserControlPaths != null && HeadEndUserControlPaths.Count > 0)
+        {
+          string headEndMarkup = BuildInjectionItemMarkup(placeHolderProvider, HeadEndUserControlPaths);
+          contentInjectionItems.Add(new HtmlHeadEndContentInjectionItem(headEndMarkup));
+        }
+
+        if (BodyBeginUserControlPaths != null && BodyBeginUserControlPaths.Count > 0)
+        {
+          string bodyBeginMarkup = BuildInjectionItemMarkup(placeHolderProvider, BodyBeginUserControlPaths);
+          contentInjectionItems.Add(new HtmlBodyBeginContentInjectionItem(bodyBeginMarkup));
+        }
+
+        if (BodyEndUserControlPaths != null && BodyEndUserControlPaths.Count > 0)
+        {
+          string bodyEndMarkup = BuildInjectionItemMarkup(placeHolderProvider, BodyEndUserControlPaths);
+          contentInjectionItems.Add(new HtmlBodyEndContentInjectionItem(bodyEndMarkup));
+        }
+
+        if (contentInjectionItems.Count > 0)
+        {
+          IContentInjectionContext context = new ContentInjectionContext(contentInjectionItems);
+          RenderHandlers.Insert(0, new ContentInjectionRenderHandler(context));
+        }
+      }
+    }
+
+    private string BuildInjectionItemMarkup(IPlaceHolderProvider placeHolderProvider, IList<string> userControlPaths)
+    {
+      StringBuilder markupBuilder = new StringBuilder();
+
+      foreach (string userControlPath in userControlPaths)
+      {
+        markupBuilder.Append(placeHolderProvider.GetPlaceHolderMarkup(PlaceHolderTypes.UserControl, userControlPath, null));
+      }
+
+      return markupBuilder.ToString();
     }
 
     protected virtual bool HandleWhiteListResult(IWhitelistResult whiteListResult)
@@ -79,6 +162,13 @@ namespace Atlantis.Framework.Web.CDSContent
       {
         ProcessContent();  
       }
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+
+      ConfigureRenderPipeline();
     }
   }
 }

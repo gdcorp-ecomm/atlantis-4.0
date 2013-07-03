1aa9f643b426e1ac79ec4d34397e3d8098a5a86c
Rajendar Vontela <rvontela@godaddy.com>
2013-07-03 20:33:14 +0000

    ATLANTIS-49: DotTypeRegistration - updated the DotTypeValidationTests
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C349336

--- Triplets/DotTypeRegistration/Atlantis.Framework.DotTypeValidation.Tests/DotTypeValidationTests.cs
+++ Triplets/DotTypeRegistration/Atlantis.Framework.DotTypeValidation.Tests/DotTypeValidationTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Atlantis.Framework.DotTypeValidation.Interface;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Atlantis.Framework.DotTypeValidation.Tests
@@ -9,8 +10,25 @@ namespace Atlantis.Framework.DotTypeValidation.Tests
   public class DotTypeValidationTests
   {
     [TestMethod]
-    public void TestMethod1()
+    public void DotTypeValidationGoodRequest()
     {
+      var fields = new Dictionary<string, string>();
+      fields["legaltype"] = "CCO";
+      var request = new DotTypeValidationRequestData("dpp_absol", "d1wsdv", 1577, "LR", "apptoken", fields);
+      var response = (DotTypeValidationResponseData)Engine.Engine.ProcessRequest(request, 695);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ToXML()));      
+    }
+
+    [TestMethod]
+    public void DotTypeValidationBadRequest()
+    {
+      var fields = new Dictionary<string, string>();
+      fields["legaltype"] = "CCO-AVCSD";
+      var request = new DotTypeValidationRequestData("", "", 1577, "", "apptokenzsxs", fields);
+      var response = (DotTypeValidationResponseData)Engine.Engine.ProcessRequest(request, 695);
+      Assert.AreEqual(true, response.IsSuccess);
+      Assert.AreEqual(true, !string.IsNullOrEmpty(response.ToXML()));
     }
   }
 }

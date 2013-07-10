f5a7305983bb25f6bc1fac410a92a702a681d3cd
pmccormack <pmccormack@jomax.tfs.local>
2013-07-10 18:23:28 +0000

    Initial checkin
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C350068

---
+++ Domains/Atlantis.Framework.Domains.Interface/Domain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Atlantis.Framework.Domains.Interface
+{
+  public class Domain : IDomain
+  {
+    public Domain(string sld, string tld, string name)
+    {
+      _domainName = name ?? string.Empty;
+      _sld = sld ?? string.Empty;
+      _tld = tld ?? string.Empty;
+    }
+
+    private readonly string _domainName;
+    public string DomainName
+    {
+      get { return _domainName; }
+    }
+
+    private readonly string _sld;
+    public string Sld
+    {
+      get { return _sld; }
+    }
+
+    private readonly string _tld;
+    public string Tld
+    {
+      get { return _tld; }
+    }
+
+    public bool HasSubDomains
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public string HtmlFormat
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+  }
+}
---
+++ Domains/Atlantis.Framework.Domains.Interface/IDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace Atlantis.Framework.Domains.Interface
+{
+  public interface IDomain
+  {
+    string DomainName { get; }
+    string Sld { get; }
+    string Tld { get; }
+    bool HasSubDomains { get; }
+    string HtmlFormat { get; }
+  }
+}

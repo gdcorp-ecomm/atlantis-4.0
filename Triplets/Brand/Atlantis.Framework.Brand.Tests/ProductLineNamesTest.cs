f791bef89f2ed4bb06e377eb2e3a907489dc0213
Shaun Stair <sstair@godaddy.com>
2013-08-05 16:02:55 +0000

    FOS-1364
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C355045

---
+++ Triplets/Brand/Atlantis.Framework.Brand.Tests/ProductLineNamesTest.cs
@@ -0,0 +1,24 @@
+﻿using Atlantis.Framework.Brand.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Atlantis.Framework.Brand.Impl;
+
+namespace Atlantis.Framework.Brand.Tests
+{
+  [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("Atlantis.Framework.Brand.Impl.dll")]
+  public class ProductLineNamesTest
+  {
+    [TestMethod]
+    public void SimpleGetProductLineNames()
+    {
+      int GODADDY_CONTEXTID = 1;
+
+      ProductLineNameRequestData request = new ProductLineNameRequestData(GODADDY_CONTEXTID);
+
+      ProductLineNameResponseData response = (ProductLineNameResponseData)Engine.Engine.ProcessRequest(request, 727);
+
+      Assert.IsNotNull(response);
+    }
+  }
+}

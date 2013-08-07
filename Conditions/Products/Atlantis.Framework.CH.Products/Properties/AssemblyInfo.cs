e776d1f467fddd87d0b0836e3df6be28a8a486de
agurumurthi <agurumurthi@jomax.tfs.local>
2013-08-07 00:07:06 +0000

    ATLANTIS-99: Create condition handler productHasSavingsMoreThan
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C355573

---
+++ Conditions/Products/Atlantis.Framework.CH.Products/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+
+[assembly: AssemblyTitle("Atlantis.Framework.CH.Products")]
+[assembly: AssemblyCompany("GoDaddy")]
+[assembly: AssemblyProduct("Atlantis.Framework.CH.Products")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("176548cd-5aaa-4b48-9e47-765dde23aa5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("4.13.8.6")]

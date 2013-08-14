c2f84bc289fab5ca1e411988305a07358d65b132
dymoore <dymoore@jomax.tfs.local>
2013-08-14 00:10:49 +0000

    converted Atlantis references to NuGet
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C357124

--- Atlantis.Framework.PromoToolGetViralPromo/Atlantis.Framework.PromoToolGetViralPromo.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.PromoToolGetViralPromo/Atlantis.Framework.PromoToolGetViralPromo.Impl/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Atlantis.Framework.PromoToolGetViralPromo.Impl")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Atlantis.Framework.PromoToolGetViralPromo.Impl")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTitle("Atlantis.Framework.PromoToolGetViralPromo")]
+[assembly: AssemblyCompany("GoDaddy LLC")]
+[assembly: AssemblyProduct("Atlantis.Framework.PromoToolGetViralPromo")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy LLC 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

3fee99a2e60e0e9d2898ea0830ca9d3dbfe0b33c
trwalker <trwalker@jomax.tfs.local>
2013-08-20 21:55:45 +0000

    Atlantis.Framework.CH.ShopperSegment cleanup
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C358584

--- Conditions/Segmentation/Atlantis.Framework.CH.ShopperSegmentation/ShopperSegmentAnyConditionHandler.cs
+++ Conditions/Segmentation/Atlantis.Framework.CH.ShopperSegmentation/ShopperSegmentAnyConditionHandler.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Atlantis.Framework.Conditions.Interface;
 using Atlantis.Framework.Interface;
 using Atlantis.Framework.Providers.Segmentation.Interface;

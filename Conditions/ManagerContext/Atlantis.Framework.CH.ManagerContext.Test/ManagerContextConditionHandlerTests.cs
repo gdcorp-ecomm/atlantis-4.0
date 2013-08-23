e745114b56e5914c9159836f84e0ee2e85169038
Bryan Focht <bxfocht@godaddy.com>
2013-08-23 05:25:44 +0000

    Atlantis-124 Manager Context Condition for CDS 2.0 - relates to FOS-3879
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C359332

---
+++ Conditions/ManagerContext/Atlantis.Framework.CH.ManagerContext.Test/ManagerContextConditionHandlerTests.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Render.ExpressionParser;
+using Atlantis.Framework.Testing.MockProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Atlantis.Framework.CH.SiteContext.Tests
+{
+  [TestClass]
+  [DeploymentItem("Atlantis.Framework.CH.ManagerContext.dll")]
+  public class SiteContextManagerConditionHandlerTests
+  {
+    private void WriteOutput(string message)
+    {
+#if (DEBUG)
+      Debug.WriteLine(message);
+#else
+      Console.WriteLine(message);
+#endif
+    }
+
+    private ExpressionParserManager GetExpressionParserManager(IProviderContainer container)
+    {
+      var expressionParserManager = new ExpressionParserManager(container);
+      expressionParserManager.EvaluateExpressionHandler += ConditionHandlerManager.EvaluateCondition;
+
+      return expressionParserManager;
+    }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      ConditionHandlerManager.AutoRegisterConditionHandlers(Assembly.GetExecutingAssembly());
+    }
+
+
+    [TestMethod]
+    public void ManagerInvalidArgumentReturnsFalse()
+    {
+      var container = new MockProviderContainer();
+      container.RegisterProvider<IManagerContext, MockManagerContext>();
+      container.SetData(MockManagerContextSettings.IsManager, false);
+
+      var expressionParserManager = GetExpressionParserManager(container);
+
+      string expression = "isManager(false)";
+      bool result = expressionParserManager.EvaluateExpression(expression);
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void CheckReturnsTrueWhenSiteContextIsManagerTrue()
+    {
+      var container = new MockProviderContainer();
+      container.RegisterProvider<IManagerContext, MockManagerContext>();
+      container.SetData(MockManagerContextSettings.IsManager, true);
+
+      var expressionParserManager = GetExpressionParserManager(container);
+
+      string expression = "isManager()";
+      bool result = expressionParserManager.EvaluateExpression(expression);
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void CheckReturnsFalseWhenSiteContextIsManagerFalse()
+    {
+      var container = new MockProviderContainer();
+      container.RegisterProvider<IManagerContext, MockManagerContext>();
+      container.SetData(MockManagerContextSettings.IsManager, false);
+
+      var expressionParserManager = GetExpressionParserManager(container);
+
+      string expression = "isManager()";
+      bool result = expressionParserManager.EvaluateExpression(expression);
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void CheckReturnsFalseWhenManagerContextIsNull()
+    {
+      var container = new MockProviderContainer();
+
+      var expressionParserManager = GetExpressionParserManager(container);
+
+      string expression = "isManager()";
+      bool result = expressionParserManager.EvaluateExpression(expression);
+      Assert.IsFalse(result);
+    }
+  }
+}
---
+++ Conditions/ManagerContext/Atlantis.Framework.CH.ManagerContext/ManagerContextConditionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.CH.ManagerContext
+{
+  public class ManagerContextConditionHandler : IConditionHandler
+  {
+    public string ConditionName
+    {
+      get
+      {
+        return "isManager";
+      }
+    }
+
+    public bool EvaluateCondition(string conditionName, IList<string> parameters, Interface.IProviderContainer providerContainer)
+    {
+      var managerContext = providerContainer.Resolve<IManagerContext>();
+      return managerContext.IsManager;
+    }
+  }
+}

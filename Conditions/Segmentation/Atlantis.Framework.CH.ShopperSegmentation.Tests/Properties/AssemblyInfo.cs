ca52f9e4c54fd346d6d0a8641590bb585ed918bc
Bryan Focht <bxfocht@godaddy.com>
2013-08-30 05:46:03 +0000

    CDS-203 - segmentation condition to use segmentaiton name and not an ID - updated assemblyinfo
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C361064

--- Conditions/Segmentation/Atlantis.Framework.CH.ShopperSegmentation.Tests/Properties/AssemblyInfo.cs
+++ Conditions/Segmentation/Atlantis.Framework.CH.ShopperSegmentation.Tests/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.13.5.15")]
+[assembly: AssemblyFileVersion("4.13.8.29")]

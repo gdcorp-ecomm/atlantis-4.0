5e1d4795c8ab65b29f9fd84546cd74259ecba500
dymoore <dymoore@jomax.tfs.local>
2013-08-30 20:30:43 +0000

    Added NuGet packaging
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C361216

--- Atlantis.Framework.PromoToolGetOrderPromo/Atlantis.Framework.PromoToolGetOrderPromo.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.PromoToolGetOrderPromo/Atlantis.Framework.PromoToolGetOrderPromo.Impl/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Atlantis.Framework.PromoToolGetOrderPromo")]
+[assembly: AssemblyTitle("Atlantis.Framework.PromoToolGetOrderPromo.Impl")]
 [assembly: AssemblyCompany("GoDaddy LLC")]
-[assembly: AssemblyProduct("Atlantis.Framework.PromoToolGetOrderPromo")]
+[assembly: AssemblyProduct("Atlantis.Framework.PromoToolGetOrderPromo.Impl")]
 [assembly: AssemblyCopyright("Copyright © GoDaddy LLC 2012-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

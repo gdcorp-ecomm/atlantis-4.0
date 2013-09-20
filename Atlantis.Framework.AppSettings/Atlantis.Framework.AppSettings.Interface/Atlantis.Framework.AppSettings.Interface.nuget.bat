2834c3b5f4e4d32cce047d7a2472f43de1d67761
dymoore <dymoore@jomax.tfs.local>
2013-09-20 22:16:31 +0000

    added atlantis.config.transform
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C365323

--- Atlantis.Framework.AppSettings/Atlantis.Framework.AppSettings.Interface/Atlantis.Framework.AppSettings.Interface.nuget.bat
+++ Atlantis.Framework.AppSettings/Atlantis.Framework.AppSettings.Interface/Atlantis.Framework.AppSettings.Interface.nuget.bat
@@ -1,3 +1,2 @@
 @ECHO off
 nuget pack Atlantis.Framework.AppSettings.Interface.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\
-PAUSE

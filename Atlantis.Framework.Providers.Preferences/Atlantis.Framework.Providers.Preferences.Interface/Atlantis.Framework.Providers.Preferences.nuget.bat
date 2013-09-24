c47321d5891528db84abb152da2da96d4a7b9777
dymoore <dymoore@jomax.tfs.local>
2013-09-24 19:18:25 +0000

    converted to use and produce NuGet packages
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C366037

--- Atlantis.Framework.Providers.Preferences/Atlantis.Framework.Providers.Preferences.Interface/Atlantis.Framework.Providers.Preferences.nuget.bat
+++
@@ -1,3 +0,0 @@
-﻿@ECHO off
-nuget pack Atlantis.Framework.Providers.Preferences.Interface.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\
-PAUSE

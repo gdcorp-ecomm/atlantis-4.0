10b8d88ac9fbe4b28634b4fd9bd53c0a1583de3e
dymoore <dymoore@jomax.tfs.local>
2013-09-25 17:50:59 +0000

    converted to use and produce NuGet packages
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C366249

---
+++ Triplets/Shopper/Atlantis.Framework.Shopper.Impl/Atlantis.Framework.Shopper.Impl.nuget.bat
@@ -0,0 +1,2 @@
+@ECHO off
+nuget pack Atlantis.Framework.Shopper.Impl.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

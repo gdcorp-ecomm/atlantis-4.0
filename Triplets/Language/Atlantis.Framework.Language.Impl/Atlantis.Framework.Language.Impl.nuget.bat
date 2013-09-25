5cf0943abfda26b90c371ff42df6267f578d1b2b
dymoore <dymoore@jomax.tfs.local>
2013-09-25 20:37:02 +0000

    converted to use and produce NuGet packages
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C366305

---
+++ Triplets/Language/Atlantis.Framework.Language.Impl/Atlantis.Framework.Language.Impl.nuget.bat
@@ -0,0 +1,2 @@
+@ECHO off
+nuget pack Atlantis.Framework.Language.Impl.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

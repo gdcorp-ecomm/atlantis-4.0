17546c42f1a14b22b0b29f025c1ca4ca00f0a5a6
Shaun Stair <sstair@godaddy.com>
2013-09-25 16:39:03 +0000

    Atlantis-156 : tldPhaseActiveAny Condition - Initial checkin
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C366214

---
+++ Conditions/DotTypeCache/Atlantis.Framework.CH.DotTypeCache.Tests/TldPhaseActiveAnyTests.cs
@@ -0,0 +1,160 @@
+﻿using System.Reflection;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.DotTypeCache;
+using Atlantis.Framework.DotTypeCache.Interface;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.Render.ExpressionParser;
+using Atlantis.Framework.Testing.MockProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.CH.DotTypeCache.Tests
+{
+  [TestClass]
+  [DeploymentItem("atlantis.config")]
+  [DeploymentItem("dottypecache.config")]
+  [DeploymentItem("Atlantis.Framework.AppSettings.Impl.dll")]    
+  [DeploymentItem("Atlantis.Framework.CH.DotTypeCache.dll")]
+  [DeploymentItem("Atlantis.Framework.TLDDataCache.Impl.dll")]
+  [DeploymentItem("Atlantis.Framework.DCCDomainsDataCache.Impl.dll")]
+  public class TldPhaseActiveAnyTests
+  {
+    private ExpressionParserManager GetExpressionParserManager(IProviderContainer container)
+    {
+      var expressionParserManager = new ExpressionParserManager(container);
+      expressionParserManager.EvaluateExpressionHandler += ConditionHandlerManager.EvaluateCondition;
+
+      return expressionParserManager;
+    }
+
+    private ExpressionParserManager MockContainerExpressionParserManager()
+    {
+      var container = new MockProviderContainer();
+      container.RegisterProvider<IDotTypeProvider, DotTypeProvider>();
+      container.RegisterProvider<ISiteContext, MockSiteContext>();
+
+      var expressionParserManager = GetExpressionParserManager(container);
+      return expressionParserManager;
+    }
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      ConditionHandlerManager.AutoRegisterConditionHandlers(Assembly.GetExecutingAssembly());
+    }
+
+    [TestMethod]
+    public void InvalidEmptyParameter()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny()";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void InvalidParameter()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny(fghjfytrtyr)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void InvalidMultipleParameters()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny(fhsajfhdsja, fydusaifydsui)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void InvalidTLDWithOneValidParameters()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny(fydusaifydsui, GA)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void ValidParameter()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny('K.BORG', LR)";
+      bool result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void ValidMultipleParameters()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny('k.borg', GA, LR)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void ValidCasingParameters()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny('K.bOrG', lR)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void ValidParametersReturningFalse()
+    {
+      var expressionParserManager = MockContainerExpressionParserManager();
+
+      const string expression = "tldPhaseActiveAny('K.BORG', GA)";
+      var result = expressionParserManager.EvaluateExpression(expression);
+
+      Assert.IsFalse(result);
+    }
+
+    /*********************************************************************************
+     * Commenting these out since we have no punycode/native TLDs to test currently. 
+     * Looks as though the expression parser can handles these though.
+     *********************************************************************************/
+
+    //[TestMethod]
+    //public void PunyCodeParameters()
+    //{
+    //  var expressionParserManager = MockContainerExpressionParserManager();
+
+    //  const string expression = "tldPhaseActiveAny('xn--mgbcd6b8dd', Sr)";
+    //  var result = expressionParserManager.EvaluateExpression(expression);
+
+    //  Assert.IsTrue(result);
+    //}
+
+    //[TestMethod]
+    //public void NativeCharacterParameters()
+    //{
+    //  var expressionParserManager = MockContainerExpressionParserManager();
+
+    //  const string expression = "tldPhaseActiveAny('الشبكة', Sr)";
+    //  var result = expressionParserManager.EvaluateExpression(expression);
+
+    //  Assert.IsTrue(result);
+    //}
+  }
+}
---
+++ Conditions/DotTypeCache/Atlantis.Framework.CH.DotTypeCache/TldPhaseActiveAny.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Atlantis.Framework.Conditions.Interface;
+using Atlantis.Framework.DotTypeCache.Interface;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.CH.DotTypeCache
+{
+  public class TldPhaseActiveAny : IConditionHandler
+  {
+    public string ConditionName { get { return "tldPhaseActiveAny"; } }
+
+    public bool EvaluateCondition(string conditionName, IList<string> parameters, IProviderContainer providerContainer)
+    {
+      bool result = false;
+
+      try
+      {
+        var dotTypeProvider = providerContainer.Resolve<IDotTypeProvider>();
+        var formatDotType = parameters[0].Replace("'", string.Empty).ToUpperInvariant();
+        var dotType = dotTypeProvider.GetDotTypeInfo(formatDotType);
+
+        if (dotType != null)
+        {
+          DotTypeActivePhase = dotType.GetActiveClientRequestPhases();
+
+          foreach (var phases in parameters)
+          {
+            TokenPreReg tokenPhase;
+
+            if (Enum.TryParse(phases.ToUpperInvariant(), out tokenPhase))
+            {
+              switch (tokenPhase)
+              {
+                case TokenPreReg.GA:
+                  result = IsPhaseActive(PreRegPhases.GeneralAvailability);
+                  break;
+                case TokenPreReg.EA:
+                  result = IsPhaseActive(PreRegPhases.Landrush);
+                  break;
+                case TokenPreReg.LR:
+                  result = IsPhaseActive(PreRegPhases.Landrush);
+                  break;
+                case TokenPreReg.SR:
+                  result = IsPhaseActive(PreRegPhases.SunriseA);
+                  result = IsPhaseActive(PreRegPhases.SunriseB);
+                  result = IsPhaseActive(PreRegPhases.SunriseC);
+                  break;
+              }
+            }
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        var aex = new AtlantisException("tldPhaseActiveAny EvaluateCondition", "0", ex.Message + Environment.NewLine + ex.StackTrace, string.Empty, null, null);
+        Engine.Engine.LogAtlantisException(aex);
+      }
+
+      return result;
+    }
+
+    private Dictionary<string, ITLDLaunchPhase> DotTypeActivePhase { get; set; }
+
+    private PreRegPhases GetPreRegPhase(ITLDLaunchPhase tldLaunchPhase)
+    {
+      PreRegPhases result;
+
+      if (Enum.TryParse(tldLaunchPhase.Type, out result))
+      {
+        return result;
+      }
+
+      return PreRegPhases.Invalid;
+    }
+
+    private bool IsPhaseActive(PreRegPhases phase)
+    {
+      var isPhaseActive = false;
+
+      foreach (var tldDict in DotTypeActivePhase)
+      {
+        var preRegPhase = GetPreRegPhase(tldDict.Value);
+
+        switch (preRegPhase)
+        {
+          case PreRegPhases.Landrush:
+            if (preRegPhase == phase)
+            {
+              isPhaseActive = true;
+            }
+            break;
+          case PreRegPhases.SunriseA:
+            if (preRegPhase == phase)
+            {
+              isPhaseActive = true;
+            }
+            break;
+          case PreRegPhases.SunriseB:
+            if (preRegPhase == phase)
+            {
+              isPhaseActive = true;
+            }
+            break;
+          case PreRegPhases.SunriseC:
+            if (preRegPhase == phase)
+            {
+              isPhaseActive = true;
+            }
+            break;
+          case PreRegPhases.GeneralAvailability:
+            if (preRegPhase == phase)
+            {
+              isPhaseActive = true;
+            }
+            break;
+        }
+      }
+      return isPhaseActive;
+    }
+
+    private enum TokenPreReg
+    {
+      SR,
+      EA,
+      LR,
+      GA
+    }
+  }
+}

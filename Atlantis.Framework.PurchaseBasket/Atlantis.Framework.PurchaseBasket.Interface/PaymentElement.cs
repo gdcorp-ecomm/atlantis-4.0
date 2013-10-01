dc91804583c4e04741689258764962565590df22
tbird <tbird@jomax.tfs.local>
2013-10-01 17:21:18 +0000

    FOS-5776 - fix issue with multiple payment elements - cloning wasn't deep
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C367280

--- Atlantis.Framework.PurchaseBasket/Atlantis.Framework.PurchaseBasket.Interface/PaymentElement.cs
+++ Atlantis.Framework.PurchaseBasket/Atlantis.Framework.PurchaseBasket.Interface/PaymentElement.cs
@@ -1,11 +1,20 @@
 ﻿
+using System;
+using System.Collections.Generic;
 namespace Atlantis.Framework.PurchaseBasket.Interface
 {
   public abstract class PaymentElement : ElementBase
   {
     public PaymentElement Clone()
-    {
-      return this.MemberwiseClone() as PaymentElement;
+    {      
+      PaymentElement newElement= this.MemberwiseClone() as PaymentElement;
+      Type cloneType=this.GetType();
+      PaymentElement currentItem = Activator.CreateInstance(this.GetType()) as PaymentElement;
+      foreach (KeyValuePair<string, string> currentValue in newElement)
+      {
+        currentItem[currentValue.Key] = currentValue.Value;
+      }
+      return currentItem;
     }
   }
 }

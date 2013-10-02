6acafe7c8c3acf887566bca5e09d3622ca3bcc09
pmccormack <pmccormack@jomax.tfs.local>
2013-10-02 21:17:56 +0000

    ATLANTIS-196: Initial Attempt NuGet Package
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C367612

---
+++ Domains/Atlantis.Framework.Domains.Interface/IFindResponseDomain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atlantis.Framework.Domains.Interface
+{
+  public interface IFindResponseDomain
+  {
+    bool IsBadWord { get; }
+    bool HasDashes { get; }
+    bool HasNumbers { get; }
+    bool IsAvailable { get; }
+    bool IsIdn { get; }
+    bool IsBackOrderAvailable { get; }
+    bool IsTypo { get; }
+    bool IsDomainUsingSynonym { get; }
+    bool IsInternalTransfer { get; }
+    bool IsPremium { get; }
+    bool IsAuction { get; }
+    bool IsCloseOutAuction { get; }
+
+    DateTime AuctionEndTimeStamp { get; } 
+    DateTime LastUpdateTimeStamp { get; }
+    DateTime WhoIsExpiration { get; }
+
+    int DatabasePercentileRank { get; }
+    int LengthOfSld { get; }
+    int NumberOfKeywordsInDomain { get; }
+    double CommissionPercentage { get; }
+    int Price { get; }
+
+    int VendorId { get; }
+    string AuctionId { get; }
+    string AuctionType { get; }
+    string AuctionTypeId { get; }
+    string Language { get; }
+    string CurrencyType { get; }
+    string AvailCheckTypePerformed { get; }
+    string DomainSearchDataBase { get; }
+    string IdnScript { get; }
+    string IdnScriptId { get; }
+    Dictionary<string, string> CartAttributes { get; }
+    IDomain Domain { get; }
+
+    bool InWatchPhase { get; }
+    bool InSunrisePhase { get; }
+    bool InLandrushPhase { get; }
+    bool HasLeafPage { get; }
+    int VendorCost { get; }
+    int VendorTier { get; }
+    int InternalTier { get; }
+  }
+}

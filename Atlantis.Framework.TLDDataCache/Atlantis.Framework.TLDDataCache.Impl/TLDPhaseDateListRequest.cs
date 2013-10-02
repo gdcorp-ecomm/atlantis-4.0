f214807846a4a41143bf7ade58b212c7d9146888
Rajendar Vontela <rvontela@godaddy.com>
2013-10-02 21:59:07 +0000

    ATLANTIS-147: Dot Type Cache updates for gTLD pre-registration - Added IRequest for TLDPhaseDateList
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C367636

---
+++ Atlantis.Framework.TLDDataCache/Atlantis.Framework.TLDDataCache.Impl/TLDPhaseDateListRequest.cs
@@ -0,0 +1,46 @@
+﻿using Atlantis.Framework.DataCacheService;
+using Atlantis.Framework.Interface;
+using Atlantis.Framework.TLDDataCache.Interface;
+using System;
+using System.Xml.Linq;
+
+namespace Atlantis.Framework.TLDDataCache.Impl
+{
+  public class TLDPhaseDateListRequest : IRequest
+  {
+    private const string REQUESTFORMAT = "<TLDPhaseDateGetList><param name=\"n_tldID\" value=\"{0}\"/></TLDPhaseDateGetList>";
+
+    public IResponseData RequestHandler(RequestData requestData, ConfigElement config)
+    {
+      IResponseData result = null;
+
+      try
+      {
+        int tldId = ((TLDPhaseDateListRequestData)requestData).TldId;
+        if (tldId <= 0)
+        {
+          throw new ArgumentException("TldId must be greater than zero.");
+        }
+
+        string requestXml = string.Format(REQUESTFORMAT, tldId);
+
+        string responseXml = String.Empty;
+        using (var comCache = GdDataCacheOutOfProcess.CreateDisposable())
+        {
+          responseXml = comCache.GetCacheData(requestXml);
+        }
+
+        //responseXml = "<data count=\"1\"><item gdshop_tldPhase=\"LR\" phaseStartDate=\"04/22/2013\" phaseEndDate=\"12/22/2014\" /></data>";
+        XElement listElements = XElement.Parse(responseXml);
+
+        result = TLDPhaseDateListResponseData.FromDataCacheElement(listElements);
+      }
+      catch (Exception ex)
+      {
+        result = TLDPhaseDateListResponseData.FromException(requestData, ex);
+      }
+
+      return result;
+    }
+  }
+}
---
+++ Atlantis.Framework.TLDDataCache/Atlantis.Framework.TLDDataCache.Interface/TLDPhaseDateListRequestData.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Atlantis.Framework.Interface;
+using System.Xml.Linq;
+
+namespace Atlantis.Framework.TLDDataCache.Interface
+{
+  public class TLDPhaseDateListRequestData : RequestData
+  {
+    public int TldId { get; private set; }
+
+    public TLDPhaseDateListRequestData(int tldId)
+    {
+      TldId = tldId;
+    }
+
+    public override string GetCacheMD5()
+    {
+      return TldId.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public override string ToXML()
+    {
+      var element = new XElement("TLDPhaseDateListRequestData");
+      element.Add(new XAttribute("TldId", TldId));
+      return element.ToString();
+    }
+  }
+}

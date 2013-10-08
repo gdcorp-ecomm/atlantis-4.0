ee6db6e47659d94ac9f4923f23973cdb62912c3b
sywilliams <sywilliams@jomax.tfs.local>
2013-10-08 01:01:36 +0000

    FOS-6120 : Create Token for the SPKey
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C368579

--- Tokens/SSO/Atlantis.Framework.TH.SSO/Properties/AssemblyInfo.cs
+++ Tokens/SSO/Atlantis.Framework.TH.SSO/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SSO")]
+[assembly: AssemblyTitle("Atlantis.Framework.TH.Sso")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SSO")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("GoDaddy.com, LLC")]
+[assembly: AssemblyProduct("Atlantis.Framework.TH.Sso")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com, LLC 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.13.10.07")]

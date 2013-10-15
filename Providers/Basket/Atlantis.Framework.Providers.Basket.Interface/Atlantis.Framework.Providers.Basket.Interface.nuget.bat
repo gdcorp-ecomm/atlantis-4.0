c39993687effd8562f048d1a438108fd9cf662e1
Michael Micco <mmicco@godaddy.com>
2013-10-15 04:37:16 +0000

    ATLANTIS-215 Fixed nuget batch file for ANSI encoding
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C369930

--- Providers/Basket/Atlantis.Framework.Providers.Basket.Interface/Atlantis.Framework.Providers.Basket.Interface.nuget.bat
+++ Providers/Basket/Atlantis.Framework.Providers.Basket.Interface/Atlantis.Framework.Providers.Basket.Interface.nuget.bat
@@ -1,1 +1,1 @@
-﻿nuget pack Atlantis.Framework.Providers.Basket.Interface.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\ 
+nuget pack Atlantis.Framework.Providers.Basket.Interface.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

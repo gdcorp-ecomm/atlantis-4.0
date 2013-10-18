b14a8193d99721dc7a3b7d1416b34d20c7a1c8f6
pmccormack <pmccormack@jomax.tfs.local>
2013-10-18 18:52:32 +0000

    Removed unneccessary references
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C370849

--- Providers/Domains/DomainProductPackage/Atlantis.Framework.Providers.DomainProductPackage/DomainProductPackage.cs
+++ Providers/Domains/DomainProductPackage/Atlantis.Framework.Providers.DomainProductPackage/DomainProductPackage.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Atlantis.Framework.Domains.Interface;
-using Atlantis.Framework.DotTypeCache;
-using Atlantis.Framework.DotTypeCache.Interface;
 using Atlantis.Framework.Interface;
-using Atlantis.Framework.Providers.DomainProductPackage.PackageItems;
 using Atlantis.Framework.Providers.Interface.Currency;
 using Atlantis.Framework.Providers.Interface.Products;
 

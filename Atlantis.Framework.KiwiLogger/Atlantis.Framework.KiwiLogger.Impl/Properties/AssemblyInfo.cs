f91c20fccab3d7311b47a75181b8229616b11e7e
caldarondo <caldarondo@jomax.tfs.local>
2013-10-25 17:10:31 +0000

    FOS-7019
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C372015

--- Atlantis.Framework.KiwiLogger/Atlantis.Framework.KiwiLogger.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.KiwiLogger/Atlantis.Framework.KiwiLogger.Impl/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.KiwiLogger.Impl")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Release Build")]
+[assembly: AssemblyConfiguration("Release Build")]
 [assembly: AssemblyCompany("GoDaddy.com, Inc.")]
 [assembly: AssemblyProduct("Atlantis.Framework.KiwiLogger.Impl")]
 [assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2009-2011")]

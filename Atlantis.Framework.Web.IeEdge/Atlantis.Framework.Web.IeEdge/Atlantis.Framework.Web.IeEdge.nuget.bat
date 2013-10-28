7c961596665dc7292fbbc0cd4f9e54dd46401a68
caldarondo <caldarondo@jomax.tfs.local>
2013-10-28 15:36:09 +0000

    FOS-7019
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C372198

---
+++ Atlantis.Framework.Web.IeEdge/Atlantis.Framework.Web.IeEdge/Atlantis.Framework.Web.IeEdge.nuget.bat
@@ -0,0 +1,2 @@
+﻿@ECHO off
+nuget pack Atlantis.Framework.Web.IeEdge.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

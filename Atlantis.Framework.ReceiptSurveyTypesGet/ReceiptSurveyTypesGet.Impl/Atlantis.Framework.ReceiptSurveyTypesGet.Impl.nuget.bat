e32227b7382a384ec3e90cc9b09793ed54dd77de
caldarondo <caldarondo@jomax.tfs.local>
2013-10-28 19:13:10 +0000

    FOS-7019
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C372287

---
+++ Atlantis.Framework.ReceiptSurveyTypesGet/ReceiptSurveyTypesGet.Impl/Atlantis.Framework.ReceiptSurveyTypesGet.Impl.nuget.bat
@@ -0,0 +1,2 @@
+﻿@ECHO off
+nuget pack Atlantis.Framework.ReceiptSurveyTypesGet.Impl.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

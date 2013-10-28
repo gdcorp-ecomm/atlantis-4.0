9f5868d31b98a0b9ff4ca5d594315887dd698038
caldarondo <caldarondo@jomax.tfs.local>
2013-10-28 18:11:23 +0000

    FOS-7019
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C372264

--- Atlantis.Framework.RegGetDotTypeProductIdList/Atlantis.Framework.RegGetDotTypeProductIdList.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.RegGetDotTypeProductIdList/Atlantis.Framework.RegGetDotTypeProductIdList.Impl/Properties/AssemblyInfo.cs
@@ -5,8 +5,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.RegGetDotTypeProductIdList.Impl")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
 [assembly: AssemblyCompany("GoDaddy.com, Inc.")]
 [assembly: AssemblyProduct("Atlantis.Framework.RegGetDotTypeProductIdList.Impl")]
 [assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2010-2011")]

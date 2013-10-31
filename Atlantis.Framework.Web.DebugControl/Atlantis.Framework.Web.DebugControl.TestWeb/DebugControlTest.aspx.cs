4c1e37a924b3080c0ea416f5dab1417a2bf2a9d0
Scott M. Creighton <screighton@godaddy.com>
2013-10-31 20:21:11 +0000

    initial checkin
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C373168

---
+++ Atlantis.Framework.Web.DebugControl/Atlantis.Framework.Web.DebugControl.TestWeb/DebugControlTest.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.Interface.ProviderContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Atlantis.Framework.Web.DebugControl.TestWeb
+{
+  public partial class DebugControlTest : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Session["debugControlTest"] = "test";
+    }
+  }
+
+  
+}
---
+++ Atlantis.Framework.Web.DebugControl/Atlantis.Framework.Web.DebugControl.TestWeb/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.Interface.ProviderContainer;
+using Atlantis.Framework.Testing.MockProviders;
+using System;
+using Atlantis.Framework.BasePages.Providers;
+using Atlantis.Framework.Providers.Interface.Links;
+using Atlantis.Framework.Providers.Links;
+
+namespace Atlantis.Framework.Web.DebugControl.TestWeb
+{
+  public class Global : System.Web.HttpApplication
+  {
+
+    protected void Application_Start(object sender, EventArgs e)
+    {
+      HttpProviderContainer.Instance.RegisterProvider<ISiteContext, MockSiteContext>();
+      HttpProviderContainer.Instance.RegisterProvider<IManagerContext, MockNoManagerContext>();
+      HttpProviderContainer.Instance.RegisterProvider<IShopperContext, MockShopperContext>();
+      HttpProviderContainer.Instance.RegisterProvider<IDebugContext, DebugProvider>();
+      HttpProviderContainer.Instance.RegisterProvider<ILinkProvider, LinkProvider>();
+    }
+
+    protected void Session_Start(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Application_BeginRequest(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Application_AuthenticateRequest(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Application_Error(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Session_End(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Application_End(object sender, EventArgs e)
+    {
+
+    }
+  }
+}

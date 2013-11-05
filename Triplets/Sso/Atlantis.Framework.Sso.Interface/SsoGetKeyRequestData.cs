62f88b1cceaa5adf11e83a791616348f4ebcae22
syukna:167 <syukna:167@jomax.tfs.local>
2013-11-05 20:12:46 +0000

    Updated to follow best practice for cache key.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C374045

--- Triplets/Sso/Atlantis.Framework.Sso.Interface/SsoGetKeyRequestData.cs
+++ Triplets/Sso/Atlantis.Framework.Sso.Interface/SsoGetKeyRequestData.cs
@@ -23,14 +23,7 @@ namespace Atlantis.Framework.Sso.Interface
     
     public override string GetCacheMD5()
     {
-      MD5 oMD5 = new MD5CryptoServiceProvider();
-      oMD5.Initialize();
-
-      byte[] stringBytes = System.Text.Encoding.ASCII.GetBytes(Token.Header.kid);
-      byte[] md5Bytes = oMD5.ComputeHash(stringBytes);
-      string sValue = BitConverter.ToString(md5Bytes, 0);
-      return sValue.Replace("-", "");
+      return Token.Header.kid;
     }
-
   }
 }

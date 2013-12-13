b58e08f9fe1b161bd860e8776a9960b43443ab77
jojohnson <jojohnson@jomax.tfs.local>
2013-12-13 17:04:11 +0000

    ATLANTIS-303 Add WebstashRenderHandler
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C382974

---
+++ Web/RenderPipeline/Atlantis.Framework.Web.RenderPipeline/WebStashRenderHandler.cs
@@ -0,0 +1,15 @@
+﻿using Atlantis.Framework.Interface;
+using Atlantis.Framework.Providers.RenderPipeline.Interface;
+
+namespace Atlantis.Framework.Web.RenderPipeline
+{
+  public class WebStashRenderHandler : IRenderHandler
+  {
+    public void ProcessContent(IProcessedRenderContent processRenderContent, IProviderContainer providerContainer)
+    {
+      string modifiedContent = WebStashManager.ProcessScript(processRenderContent.Content);
+
+      processRenderContent.OverWriteContent(modifiedContent);
+    }
+  }
+}

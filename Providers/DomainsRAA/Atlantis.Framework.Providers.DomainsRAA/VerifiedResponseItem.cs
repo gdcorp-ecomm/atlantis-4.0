4f4638e514f24a03cbae6692d6f41563e374a706
pmccormack <pmccormack@jomax.tfs.local>
2013-12-16 22:57:22 +0000

    A-256 - Added file manually to SC
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C383338

---
+++ Providers/DomainsRAA/Atlantis.Framework.Providers.DomainsRAA/VerifiedResponseItem.cs
@@ -0,0 +1,28 @@
+﻿using Atlantis.Framework.Providers.DomainsRAA.Interface;
+using Atlantis.Framework.Providers.DomainsRAA.Interface.Items;
+
+namespace Atlantis.Framework.Providers.DomainsRAA
+{
+  public class VerifiedResponseItem : IVerifiedResponseItem
+
+{
+  public string ItemType { get; private set; }
+  public string ItemTypeValue { get; private set; }
+  public string ItemValidationGuid { get; private set; }
+
+  public DomainsRAAVerifyCode ItemVerifiedCode { get; private set; }
+
+  internal static VerifiedResponseItem Create(string itemType, string itemTypeValue, DomainsRAAVerifyCode itemVerifiedCode, string itemValidationGuid)
+  {
+    var verifyItem = new VerifiedResponseItem
+    {
+      ItemType = itemType,
+      ItemTypeValue = itemTypeValue,
+      ItemValidationGuid = itemValidationGuid,
+      ItemVerifiedCode = itemVerifiedCode
+    };
+
+    return verifyItem;
+  }
+}
+}

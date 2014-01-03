eb23c5775450d7c3bda99d267b9760c6c9551d61
Jeffrey H. Hirsch <jhirsch@godaddy.com>
2014-01-03 00:01:10 +0000

    Moved Brand to Obsolete folder
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C385768

--- Providers/Brand/Atlantis.Framework.Providers.Brand/BrandEngineRequests.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Atlantis.Framework.Providers.Brand
-{
-  public static class BrandEngineRequests
-  {
-    public static int CompanyNameRequestId { get { return 726; } }
-    public static int ProductLineNameRequestId { get { return 727; } }
-  }
-}

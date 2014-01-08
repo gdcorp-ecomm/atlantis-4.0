df442c21ac831beaca6fe1a62cbbc881ea118587
agurumurthi <agurumurthi@jomax.tfs.local>
2014-01-08 08:23:57 +0000

    CDS-486 - As a TMS Stakeholder, I would like to see some new features added to the TMS Placeholder and personalization provider
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C386510

--- Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder/Properties/AssemblyInfo.cs
+++ Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.13.1.6")]
+[assembly: AssemblyFileVersion("4.14.1.6")]
 [assembly: InternalsVisibleTo("Atlantis.Framework.Providers.PlaceHolder.Tests")]

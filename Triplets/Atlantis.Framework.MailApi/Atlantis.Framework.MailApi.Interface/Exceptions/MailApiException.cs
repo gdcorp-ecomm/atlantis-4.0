72e0022d183a866f4a6ced4eb777f9187bed746d
kklink <kklink@jomax.tfs.local>
2014-02-18 22:43:50 +0000

    Add custom exception to standardize mail api errors
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C393831

---
+++ Triplets/Atlantis.Framework.MailApi/Atlantis.Framework.MailApi.Interface/Exceptions/MailApiException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Atlantis.Framework.MailApi.Interface.Exceptions
+{
+  public class MailApiException : Exception
+  {
+    public string EmailAddress { get; set; }
+    public string MailPod { get; set; }
+    public string RequestJson { get; set; }
+    public string ResponseJson { get; set; }
+
+    public MailApiException(string message,  string emailAddress, string mailPod, string requestJson, string responseJson) : base(message)
+    {
+      EmailAddress = emailAddress;
+      MailPod = mailPod;
+      RequestJson = requestJson;
+      ResponseJson = responseJson;
+    }
+
+    public MailApiException(string message,  string emailAddress, string mailPod, string requestJson, string responseJson, Exception innerException)
+      : base(message,innerException)
+    {
+      EmailAddress = emailAddress;
+      MailPod = mailPod;
+      RequestJson = requestJson;
+      ResponseJson = responseJson;
+    }
+  }
+}

d9d316134af29291565d0e671366b66a4da68bcc
Michael Micco <mmicco@godaddy.com>
2014-02-20 10:34:38 +0000

    Moved Product Provider to GitHub
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C394075

--- Atlantis.Framework.Providers.Products/Atlantis.Framework.Providers.Products/Atlantis.Framework.Providers.Products.nuget.bat
+++
@@ -1,2 +0,0 @@
-@ECHO off
-nuget pack Atlantis.Framework.Providers.Products.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\
--- Atlantis.Framework.Providers.Products/Atlantis.Framework.Providers.Products/Handlers/IProductGroupOfferedHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using Atlantis.Framework.Products.Interface;
-
-namespace Atlantis.Framework.Providers.Products.Handlers
-{
-  internal interface IProductGroupOfferedHandler
-  {
-    bool IsProductGroupOffered(int productGroupType, ProductGroupsOfferedResponseData responseData);
-  }
-}

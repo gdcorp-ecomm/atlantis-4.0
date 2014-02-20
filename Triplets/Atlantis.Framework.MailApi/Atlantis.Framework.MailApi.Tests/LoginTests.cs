9f9f81cc2eb9f7508d296beab51566d42c942434
asmentkowski <asmentkowski@jomax.tfs.local>
2014-02-20 22:04:24 +0000

    - added test to get login data
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C394227

--- Triplets/Atlantis.Framework.MailApi/Atlantis.Framework.MailApi.Tests/LoginTests.cs
+++ Triplets/Atlantis.Framework.MailApi/Atlantis.Framework.MailApi.Tests/LoginTests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Runtime.Versioning;
 using Atlantis.Framework.MailApi.Interface;
+using Atlantis.Framework.MailApi.Tests.Properties;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Atlantis.Framework.MailApi.Tests
 {
   [TestClass]
+  [DeploymentItem("Atlantis.Framework.MailApi.Impl.dll")]
+  [DeploymentItem("atlantis.config")]
   public class LoginTests
   {
 
@@ -24,5 +28,23 @@ namespace Atlantis.Framework.MailApi.Tests
       var request = new LoginRequestData("username", "password", ANDROID_APP_KEY);
       Assert.AreEqual("password", request.Password);
     }
+
+    [TestMethod]
+    public void AppKeyProperty()
+    {
+      var request = new LoginRequestData("username", "password", ANDROID_APP_KEY);
+      Assert.AreEqual(ANDROID_APP_KEY, request.Appkey);
+    }
+
+    [TestMethod]
+    public void GetLoginData()
+    {
+
+      var request = new LoginRequestData("tester@qa-emailpod04.com", "Godaddy25", ANDROID_APP_KEY);
+
+      var response = (LoginResponseData)Engine.Engine.ProcessRequest(request, 798);
+      Assert.IsNotNull(response);
+    }
+
   }
 }

04e3ec0ff44a18fa9b01fd0fe66120cafcc7f2ab
kklink <kklink@jomax.tfs.local>
2014-02-25 17:43:05 +0000

    initial checkin for provider solution for mail api
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C394812

---
+++ Providers/MailApi/Atlantis.Framework.Providers.MailApi.Tests/LibraryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Atlantis.Framework.Providers.MailApi.Tests
+{
+  [TestClass]
+  public class LibraryTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}

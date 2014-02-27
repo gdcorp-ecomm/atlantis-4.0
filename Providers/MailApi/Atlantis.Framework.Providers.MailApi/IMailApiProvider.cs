f457f06fda584bdbdd2654498f99de57a018b66f
Mark J. Weaver <mweaver@godaddy.com>
2014-02-27 19:32:42 +0000

    (MOBIAPPS-2482) impl GetFolderList in MailApiProvider, some method sig changes
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C395240

--- Providers/MailApi/Atlantis.Framework.Providers.MailApi/IMailApiProvider.cs
+++ Providers/MailApi/Atlantis.Framework.Providers.MailApi/IMailApiProvider.cs
@@ -10,8 +10,8 @@ namespace Atlantis.Framework.Providers.MailApi
 
     object Login(string username, string password);  //object should be replaced with the DTO defined for composite call
     CompositeLoginResponse LoginAndFetchFoldersAndInbox(string username, string password, string appKey);
-    MailFolder GetFolder(string folderNumber);
-    object GetMessageList(string folderNumber, int offset, int count, string filter);
-    object GetFolderList();
+    MailFolder GetFolder(string sessionHash, string folderNumber);
+    object GetMessageList(string sessionHash, string folderNumber, int offset, int count, string filter);
+    GetFolderListResponseData GetFolderList(string sessionHash, string appKey, string baseUrl);
   }
 }
--- Providers/MailApi/Atlantis.Framework.Providers.MailApi/MailApiProvider.cs
+++ Providers/MailApi/Atlantis.Framework.Providers.MailApi/MailApiProvider.cs
@@ -1,6 +1,8 @@
 ﻿using Atlantis.Framework.Interface;
 using Atlantis.Framework.MailApi.Interface;
 using Atlantis.Framework.Providers.MailApi.DTOs;
+using System;
+using System.Text;
 
 namespace Atlantis.Framework.Providers.MailApi
 {
@@ -11,9 +13,9 @@ namespace Atlantis.Framework.Providers.MailApi
     const string count = "20";
     const string filter = ""; // not sure what this is yet
 
-    public string RestrictedKey { get; set; }
-    public string AppKey { get; set; }
-    public string MailHash { get; set; }
+    //public string RestrictedKey { get; set; }
+    //public string AppKey { get; set; }
+    //public string MailHash { get; set; }
 
     public MailApiProvider(IProviderContainer container)
       : base(container)
@@ -22,25 +24,36 @@ namespace Atlantis.Framework.Providers.MailApi
 
     public MailApiProvider(IProviderContainer container, string mailHash, string appKey, string restrictedKey) : base(container)
     {
-      MailHash = mailHash;
-      AppKey = appKey;
-      RestrictedKey = restrictedKey;
+      //MailHash = mailHash;
+      //AppKey = appKey;
+      //RestrictedKey = restrictedKey;
     }
 
     public CompositeLoginResponse LoginAndFetchFoldersAndInbox(string username, string password, string appKey)
     {
-      LoginRequestData loginReqData = new LoginRequestData(username, password, appKey);
-      LoginResponseData loginResponseData =(LoginResponseData)Engine.Engine.ProcessRequest(loginReqData, EngineRequests.MAILAPI_LOGIN);
+      LoginResponseData loginResponseData = null;
+      GetFolderListResponseData getFolderListResponseData;
+      GetMessageListResponseData getMessageListResponseData;
 
+      try
+      {
+        LoginRequestData loginReqData = new LoginRequestData(username, password, appKey);
+        loginResponseData = (LoginResponseData)Engine.Engine.ProcessRequest(loginReqData, EngineRequests.MAILAPI_LOGIN);
+      }
+      catch (Exception ex)
+      {
+        var exception = new AtlantisException("IrisProvider.AddIncidentNote", "0", ex.Message, string.Empty, null, null);
+        Engine.Engine.LogAtlantisException(exception);
+      }
       // check for login failure here
 
-      GetFolderListRequestData getFolderListReqData = new GetFolderListRequestData(loginResponseData.LoginData.Hash, appKey, string.Empty, loginResponseData.LoginData.BaseUrl);
-      GetFolderListResponseData getFolderListResponseData = (GetFolderListResponseData)Engine.Engine.ProcessRequest(getFolderListReqData, EngineRequests.MAILAPI_FOLDER_LIST);
+      //GetFolderListRequestData getFolderListReqData = new GetFolderListRequestData(loginResponseData.LoginData.Hash, appKey, string.Empty, loginResponseData.LoginData.BaseUrl);
+      getFolderListResponseData = GetFolderList(loginResponseData.LoginData.Hash, appKey, loginResponseData.LoginData.BaseUrl);
 
       // check for folder list failure here
 
       GetMessageListRequestData getMessageListReqData = new GetMessageListRequestData(DetermineInboxFolderNum(getFolderListResponseData).ToString(), offset, count, filter, getFolderListResponseData.State.Session, loginResponseData.LoginData.BaseUrl, appKey);
-      GetMessageListResponseData getMessageListResponseData = (GetMessageListResponseData)Engine.Engine.ProcessRequest(getMessageListReqData, EngineRequests.MAILAPI_MSG_LIST);
+      getMessageListResponseData = (GetMessageListResponseData)Engine.Engine.ProcessRequest(getMessageListReqData, EngineRequests.MAILAPI_MSG_LIST);
 
       CompositeLoginResponse response = new CompositeLoginResponse(getMessageListResponseData.State, loginResponseData.LoginData, getFolderListResponseData.MailFolders, getMessageListResponseData.MessageListData);
       
@@ -60,7 +73,7 @@ namespace Atlantis.Framework.Providers.MailApi
       }
       if (inboxFolderNum == -1)
       {
-        throw new System.Exception("Could not determine INBOX FolderNum after login");
+        throw new Exception("Could not determine INBOX FolderNum after login");
       }
       return inboxFolderNum;
     }
@@ -71,19 +84,42 @@ namespace Atlantis.Framework.Providers.MailApi
     }
 
 
-    public MailFolder GetFolder(string folderNumber)
+    public MailFolder GetFolder(string sessionHash, string folderNumber)
     {
       throw new System.NotImplementedException();
     }
 
-    public object GetMessageList(string folderNumber, int offset, int count, string filter)
+    public object GetMessageList(string sessionHash, string folderNumber, int offset, int count, string filter)
     {
       throw new System.NotImplementedException();
     }
 
-    public object GetFolderList()
+    public GetFolderListResponseData GetFolderList(string sessionHash, string appKey, string baseUrl)
     {
-      throw new System.NotImplementedException();
+      GetFolderListRequestData getFolderListReqData = null;
+      GetFolderListResponseData getFolderListResponseData = null;
+      try
+      {
+        getFolderListReqData = new GetFolderListRequestData(sessionHash, appKey, string.Empty, baseUrl);
+        getFolderListResponseData = (GetFolderListResponseData)Engine.Engine.ProcessRequest(getFolderListReqData, EngineRequests.MAILAPI_FOLDER_LIST);
+      }
+      catch (Exception ex)
+      {
+        var exception = new AtlantisException("MailApiProvider.GetFolderList", "0", ex.Message, getFolderListExceptionData(getFolderListReqData),  null, null);
+        Engine.Engine.LogAtlantisException(exception);
+      }
+      return getFolderListResponseData;
+    }
+
+    private static string getFolderListExceptionData(GetFolderListRequestData getFolderListReqData)
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.Append("(request: mailApi url: ");
+      sb.Append(getFolderListReqData.MailBaseUrl);
+      sb.Append(" session: ");
+      sb.Append(getFolderListReqData.Session);
+      sb.Append(")");
+      return sb.ToString();
     }
   }
 }

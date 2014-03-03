75e44f8ba64729eeba7cd02d100581d63d4cb474
asmentkowski <asmentkowski@jomax.tfs.local>
2014-03-03 22:20:15 +0000

    - fixed informationalversion
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C395728

--- Triplets/Atlantis.Framework.MailApi/Atlantis.Framework.MailApi.Interface/Properties/AssemblyInfo.cs
+++ Triplets/Atlantis.Framework.MailApi/Atlantis.Framework.MailApi.Interface/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.13.0.0")]
-[assembly: AssemblyInformationalVersion("14.2.20.1")]
+[assembly: AssemblyInformationalVersion("14.3.3.1")]
 [assembly: InternalsVisibleTo("Atlantis.Framework.MailApi.Tests")]

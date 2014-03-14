13cae7430afca0d6d049c9fd48db25598a313741
Michael Micco <mmicco@godaddy.com>
2014-03-14 16:23:35 +0000

    moved providers.containers to git
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C397433

--- Atlantis.Framework.Providers.Containers/Atlantis.Framework.Providers.Containers.Tests/TestProviders/INameProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Atlantis.Framework.Interface;
-
-namespace Atlantis.Framework.Providers.Containers.Tests
-{
-  public interface INameProvider : IProviderContainer
-  {
-    string FirstName { get; set; }
-  }
-}

6f2ffd41a1454595d2d6e2cea2939071357eda9b
asunkenapally <asunkenapally@jomax.tfs.local>
2014-03-25 19:57:14 +0000

    FOS-16759 - Anusha - Updating AssemblyInfo file for AssemblyInformationalVersion tag
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C398815

--- Atlantis.Framework.RegDotTypeProductIds/Atlantis.Framework.RegDotTypeProductIds.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.RegDotTypeProductIds/Atlantis.Framework.RegDotTypeProductIds.Interface/Properties/AssemblyInfo.cs
@@ -39,3 +39,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.13.6.19")]
+[assembly: AssemblyInformationalVersion("14.3.25.1")]

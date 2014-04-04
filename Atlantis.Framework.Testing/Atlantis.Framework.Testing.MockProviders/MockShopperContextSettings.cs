612e908c0e207cbbf36a8bd993c375612d19dda6
Michael Micco <mmicco@godaddy.com>
2014-04-04 22:00:45 +0000

    Moved to github
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C400674

--- Atlantis.Framework.Testing/Atlantis.Framework.Testing.MockProviders/MockShopperContextSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Atlantis.Framework.Testing.MockProviders
-{
-  public static class MockShopperContextSettings
-  {
-    public const string PriceType = "MockShoppperContextSettings.ShopperPriceType";
-  }
-}

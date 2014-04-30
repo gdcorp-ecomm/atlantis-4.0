8c0da467c874dc49a03623e983d35c80517b6ceb
Federico Raggi <fraggi@godaddy.com>
2014-04-30 22:37:27 +0000

    Moved to Github
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C404972

--- Atlantis.Framework.Providers.Links/Atlantis.Framework.Providers.Links.Interface/QueryParamMode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atlantis.Framework.Providers.Interface.Links
-{
-  public enum QueryParamMode
-  {
-    CommonParameters = 0,
-    ExplicitParameters = 1,
-    ExplicitWithLocalizationParameters = 2
-  }
-}

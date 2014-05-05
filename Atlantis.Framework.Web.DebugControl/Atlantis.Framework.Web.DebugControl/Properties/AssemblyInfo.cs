dcfffd629d8e478954e72f5070cde6c5b31e5b54
caldarondo <caldarondo@jomax.tfs.local>
2014-05-05 16:30:07 +0000

    Initial Nuget Package
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C405445

--- Atlantis.Framework.Web.DebugControl/Atlantis.Framework.Web.DebugControl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.Web.DebugControl/Atlantis.Framework.Web.DebugControl/Properties/AssemblyInfo.cs
@@ -6,8 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Atlantis.Framework.Web.DebugControl")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyDescription("Debug Build")]
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyDescription("Release Build")]
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
 [assembly: AssemblyCompany("GoDaddy.com, LLC")]
 [assembly: AssemblyProduct("Atlantis.Framework.Web.DebugControl")]
 [assembly: AssemblyCopyright("Copyright © GoDaddy.com, LLC 2013")]
@@ -32,5 +37,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("14.0.0.0")]

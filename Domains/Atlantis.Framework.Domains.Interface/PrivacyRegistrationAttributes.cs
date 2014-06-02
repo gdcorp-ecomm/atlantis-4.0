c0d51e687445a24f09274f896eef9df15ab59903
Michael Micco <mmicco@godaddy.com>
2014-06-02 22:42:10 +0000

    Moved to Github
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C408122

--- Domains/Atlantis.Framework.Domains.Interface/PrivacyRegistrationAttributes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Atlantis.Framework.Domains.Interface
-{
-  public class PrivacyRegistrationAttributes
-  {
-    public double Duration { get; set; }
-    public int RegistrationType { get; set; }
-    public string DomainByProxyAccount { get; set; }
-  }
-}

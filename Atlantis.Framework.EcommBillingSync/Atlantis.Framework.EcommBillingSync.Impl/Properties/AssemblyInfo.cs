01411fb9b533f305956aee8995f21cc7c35e0e74
Sridhar Thota <sthota@godaddy.com>
2014-06-11 06:06:05 +0000

    update assembly
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C408953

--- Atlantis.Framework.EcommBillingSync/Atlantis.Framework.EcommBillingSync.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.EcommBillingSync/Atlantis.Framework.EcommBillingSync.Impl/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.13.3.20")]
+[assembly: AssemblyFileVersion("4.14.6.10")]
--- Atlantis.Framework.EcommBillingSync/Atlantis.Framework.EcommBillingSync.Tests/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.EcommBillingSync/Atlantis.Framework.EcommBillingSync.Tests/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.13.3.20")]
+[assembly: AssemblyFileVersion("4.14.6.10")]

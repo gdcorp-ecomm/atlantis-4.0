7c8a4ac12b14947d58484d756c76a3fc63662b3e
jparmenter <jparmenter@jomax.tfs.local>
2014-07-25 20:36:35 +0000

    Update to add formFields to the IDotTypeFormFieldsByDomain. Adjusted project files to build properly
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C413119

--- Providers/DotTypeRegistration/Atlantis.Framework.Providers.DotTypeRegistration.Interface/IDotTypeFormFieldsByDomain.cs
+++ Providers/DotTypeRegistration/Atlantis.Framework.Providers.DotTypeRegistration.Interface/IDotTypeFormFieldsByDomain.cs
@@ -5,6 +5,7 @@ namespace Atlantis.Framework.Providers.DotTypeRegistration.Interface
   public interface IDotTypeFormFieldsByDomain
   {
     IDictionary<string, IList<IList<IFormField>>> FormFieldsByDomain { get; set; }
+    IDictionary<string, string> FormItems { get; }
     string ToJson { get; }
   }
 }

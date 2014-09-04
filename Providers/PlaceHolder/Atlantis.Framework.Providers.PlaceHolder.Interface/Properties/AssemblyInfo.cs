9b2f23200e4b763e593a2167b3e8fb9baa64e6fa
Darrick Belcher <dbelcher@godaddy.com>
2014-09-04 19:46:24 +0000

    TMSContent - v14.9.4.4 - Added epoch time.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C416775

--- Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder.Interface/Properties/AssemblyInfo.cs
+++ Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder.Interface/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("14.9.4.3")]
+[assembly: AssemblyInformationalVersion("14.9.4.4")]

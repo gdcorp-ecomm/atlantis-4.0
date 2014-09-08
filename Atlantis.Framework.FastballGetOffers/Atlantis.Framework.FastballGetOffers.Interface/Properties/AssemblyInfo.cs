51cfcfd52107de5548e47adbe6e928e3ade30453
Sridhar Thota <sthota@godaddy.com>
2014-09-08 19:44:18 +0000

    update assembly file
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C416947

--- Atlantis.Framework.FastballGetOffers/Atlantis.Framework.FastballGetOffers.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.FastballGetOffers/Atlantis.Framework.FastballGetOffers.Interface/Properties/AssemblyInfo.cs
@@ -38,4 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.14.9.8")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyInformationalVersion("4.14.9.8")]

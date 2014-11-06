023e1eeb9d86e29dbabb7eedeee1c9490a0b9f78
Karen Van Reede <kvanreede@godaddy.com>
2014-11-06 23:01:01 +0000

    Changed  references to nuget for framework.interface and engine
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C422004

--- Atlantis.Framework.PrivacyPlusBundleXML/Atlantis.Framework.PrivacyPlusBundleXML.Tests/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.PrivacyPlusBundleXML/Atlantis.Framework.PrivacyPlusBundleXML.Tests/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.11.9.27")]
+[assembly: AssemblyFileVersion("4.14.11.6")]

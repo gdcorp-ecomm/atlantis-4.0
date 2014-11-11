16de67cc2479a27e7541b85a843f011f0edfc809
Michael Micco <mmicco@godaddy.com>
2014-11-11 17:13:09 +0000

    Move to github
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C422237

--- Providers/Localization/Atlantis.Framework.Providers.Localization/EngineRequests.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Atlantis.Framework.Providers.Localization
-{
-  public static class EngineRequests
-  {
-    public const int CountrySitesActiveRequest = 725;
-    public const int MarketsActiveRequest = 729;
-  }
-}

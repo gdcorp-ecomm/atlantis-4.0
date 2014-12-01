86f349ab31e72a0469132e36b13fb3aa05c8ea8d
Darrick Belcher <dbelcher@godaddy.com>
2014-12-01 21:02:19 +0000

    Update TMS Provider/Triplet dependency version.
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C423337

--- Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder/Properties/AssemblyInfo.cs
+++ Providers/PlaceHolder/Atlantis.Framework.Providers.PlaceHolder/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("14.11.25.1")]
+[assembly: AssemblyInformationalVersion("14.12.01.1")]
 [assembly: InternalsVisibleTo("Atlantis.Framework.Providers.PlaceHolder.Tests")]

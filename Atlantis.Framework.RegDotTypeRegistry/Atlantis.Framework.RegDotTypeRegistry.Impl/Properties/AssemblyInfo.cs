0fbe69bfa4d25122c4383dda85052fb753d6e756
Carina Shipley <cshipley@godaddy.com>
2015-05-12 21:40:37 +0000

    MCX-29984: Issues with .移动 Chinese for Mobile
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C432780

--- Atlantis.Framework.RegDotTypeRegistry/Atlantis.Framework.RegDotTypeRegistry.Impl/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.RegDotTypeRegistry/Atlantis.Framework.RegDotTypeRegistry.Impl/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -28,15 +27,14 @@ using System.Runtime.InteropServices;
 [assembly: Guid("11e80aee-cfc2-49b9-bf3b-c0b828af43bc")]
 
 // Version information for an assembly consists of the following four values:
+// Versions must be set so the AssemblyVersion and AssemblyFileVersion does not change with revisions,
+// but the InformationalVersion does.
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// use 4.0.0.0 for the Assembly Version and Assembly File Version
+// use YY.M.D.# for the InformationalVersion.  Do not use leading zeros.
+// example 14.6.4.1 for Jun 4, 2014, build 1
+// the fourth number is the build number which would increment only if you released multiple times in a day
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("15.5.8.1")]
+[assembly: AssemblyInformationalVersion("15.5.12.1")]
--- Atlantis.Framework.RegDotTypeRegistry/Atlantis.Framework.RegDotTypeRegistry.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.RegDotTypeRegistry/Atlantis.Framework.RegDotTypeRegistry.Interface/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,12 +7,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Atlantis.Framework.RegDotTypeRegistry.Interface")]
 #if DEBUG
 [assembly: AssemblyDescription("Debug Build")]
-[assembly: AssemblyConfiguration("Debug Build")]
+[assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyDescription("Release Build")]
-[assembly: AssemblyConfiguration("Release Build")]
+[assembly: AssemblyConfiguration("Release")]
 #endif
-[assembly: AssemblyCompany("GoDaddy.com")]
+[assembly: AssemblyCompany("GoDaddy")]
 [assembly: AssemblyProduct("Atlantis.Framework.RegDotTypeRegistry.Interface")]
 [assembly: AssemblyCopyright("Copyright © GoDaddy.com 2013")]
 [assembly: AssemblyTrademark("")]
@@ -28,15 +27,14 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c5731519-40e0-4552-bf9c-78780c8b68d6")]
 
 // Version information for an assembly consists of the following four values:
+// Versions must be set so the AssemblyVersion and AssemblyFileVersion does not change with revisions,
+// but the InformationalVersion does.
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// use 4.0.0.0 for the Assembly Version and Assembly File Version
+// use YY.M.D.# for the InformationalVersion.  Do not use leading zeros.
+// example 14.6.4.1 for Jun 4, 2014, build 1
+// the fourth number is the build number which would increment only if you released multiple times in a day
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("15.5.8.1")]
+[assembly: AssemblyInformationalVersion("15.5.12.1")]

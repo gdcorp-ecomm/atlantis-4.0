16c3732edbbd263019be1f350453ded14bfcbeed
sshao <sshao@jomax.tfs.local>
2015-06-05 18:40:15 +0000

    Changed Default_Duration value to 1
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C434008

--- Atlantis.Framework.ProductUpgradePath/Atlantis.Framework.ProductUpgradePath.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.ProductUpgradePath/Atlantis.Framework.ProductUpgradePath.Interface/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoDaddy.com, Inc.")]
 [assembly: AssemblyProduct("Atlantis.Framework.ProductUpgradePath.Interface")]
-[assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2012")]
+[assembly: AssemblyCopyright("Copyright © GoDaddy.com, Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.13.6.26")]
+[assembly: AssemblyFileVersion("15.6.5.2")]

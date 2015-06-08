cfc8e3b69f58c23c0fafc56ef508d9cf02ba68cc
Michael Micco <mmicco@godaddy.com>
2015-06-08 04:50:02 +0000

    MockEngine moved to Github. Other two libraries should not be used (use Moq instead to mock provider interfaces)
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C434060

--- Testing/Atlantis.Framework.Testing.MockLocalization/Atlantis.Framework.Testing.MockLocalization.nuget.bat
+++
@@ -1,2 +0,0 @@
-@ECHO off
-nuget pack Atlantis.Framework.Testing.MockLocalization.csproj -IncludeReferencedProjects -Build -Prop Configuration=Release -OutputDirectory \\g1dwdevmgmt001\webcontent\nuget.packages\

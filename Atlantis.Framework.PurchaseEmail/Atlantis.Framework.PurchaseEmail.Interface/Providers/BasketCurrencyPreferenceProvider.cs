36e9303a963c5f449fb3d46e37053324d4dcf137
caldarondo <caldarondo@jomax.tfs.local>
2015-07-30 22:51:54 +0000

    Fix Build
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C436582

---
+++ Atlantis.Framework.PurchaseEmail/Atlantis.Framework.PurchaseEmail.Interface/Providers/BasketCurrencyPreferenceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atlantis.Framework.Providers.Currency.Interface;
+
+namespace Atlantis.Framework.PurchaseEmail.Interface.Providers
+{
+  public class BasketCurrencyPreferenceProvider : ICurrencyPreferenceProvider
+  {
+    public string CurrencyPreference { get; set; }
+  }
+}

6f0aa27e8a0886984956ee6a0a5744aabb0b0699
trwalker <trwalker@jomax.tfs.local>
2015-11-30 17:36:55 +0000

    A.F.Tokens adding back obsolete TokenManager for backwards compatability
    git-tfs-id: [http://tfs2010:8080/tfs/gd]$/Delta/Services/AtlantisFramework/4.0;C441961

--- Atlantis.Framework.Tokens/Atlantis.Framework.Tokens.Interface/Properties/AssemblyInfo.cs
+++ Atlantis.Framework.Tokens/Atlantis.Framework.Tokens.Interface/Properties/AssemblyInfo.cs
@@ -41,4 +41,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("15.8.31.1")]
+[assembly: AssemblyInformationalVersion("15.11.24.1")]
---
+++ Atlantis.Framework.Tokens/Atlantis.Framework.Tokens.Interface/TokenManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Atlantis.Framework.Interface;
+
+namespace Atlantis.Framework.Tokens.Interface
+{
+  [Obsolete("TokenManger exists only for backwards compatability, use ITokenProvider instead")]
+  public static class TokenManager
+  {
+    public static void AutoRegisterTokenHandlers()
+    {
+      AutoRegisterTokenHandlers(null);
+    }
+
+    public static void AutoRegisterTokenHandlers(params Assembly[] additionalAssemblies)
+    {
+      TokenProvider.AutoRegisterTokenHandlers(additionalAssemblies);
+    }
+
+    public static void RegisterTokenHandler(ITokenHandler tokenHandler)
+    {
+      TokenProvider.RegisterTokenHandler(tokenHandler);
+    }
+
+    public static void RegisterTokenExpression(Regex tokenExpression)
+    {
+      TokenProvider.RegisterTokenExpression(tokenExpression);
+    }
+
+    public static IList<ITokenHandler> GetRegisteredTokenHandlers()
+    {
+      return TokenProvider.GetRegisteredTokenHandlers();
+    }
+
+    public static void ReplaceTokens(string inputText, IProviderContainer container, out string resultText)
+    {
+      ReplaceTokens(inputText, container, null, out resultText);
+    }
+
+    public static void ReplaceTokens(string inputText, IProviderContainer container, ITokenEncoding tokenDataEncoding, out string resultText)
+    {
+      var tokenProvider = container.Resolve<ITokenProvider>();
+      tokenProvider.ReplaceTokens(inputText, tokenDataEncoding, out resultText);
+    }
+  }
+}
